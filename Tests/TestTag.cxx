9dc6112d42a5c4f3bea6a89f0c6b51e1f7fa164f
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-12-02 18:18:11 +0000

    ENH: Since some people like to make my life miserable, add some test to make code more self explicit

--- Tests/TestTag.cxx
+++ Tests/TestTag.cxx
@@ -61,5 +61,35 @@ int TestTag(int , char * [])
   std::cout << "T3: " << t3 << std::endl;
   std::cout << "T4: " << t4 << std::endl;
 
+  // Test ordering:
+  gdcm::Tag o1(0,0);
+  gdcm::Tag o2(10,0);
+  gdcm::Tag o3(0,10);
+  gdcm::Tag o4(10,10);
+  // Clearly order should be o1 < o3 < o2 < 04
+  // Test o1
+  if( !(o1 < o3)
+    ||!(o1 < o2) 
+    ||!(o1 < o4) )
+    return 1;
+
+  // Test o2
+  if( !(o2 < o4)
+    ||!(o1 < o2) 
+    ||!(o3 < o2) )
+    return 1;
+
+  // Test o3
+  if( !(o3 < o2)
+    ||!(o3 < o4) 
+    ||!(o1 < o3) )
+    return 1;
+
+  // Test o4 (I know this duplicate some tests, but we don't need to optimize a test!)
+  if( !(o1 < o4)
+    ||!(o2 < o4) 
+    ||!(o3 < o4) )
+    return 1;
+
   return 0;
 }
