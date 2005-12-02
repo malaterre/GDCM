b7c8c760d888edcc5ad6137b490a3469891cae78
Jean-Pierre Roux <jpr@creatis.insa-lyon.fr>
2005-12-02 08:37:32 +0000

    Make the output (a little bit) more human readable

--- Tests/TestAllPrint.cxx
+++ Tests/TestAllPrint.cxx
@@ -15,12 +15,13 @@ void PrintDataElements(gdcm::DICOMIStream &is)
   while( !is.eof() && is >> de_tag )
     {
     is >> de;
-    //std::cout << de << std::endl;
+    std::cout << de << std::endl;
     }
 }
 
 void TestPrint(const char *filename)
 {
+  std ::cerr << "===============================================" << std::endl;
   std::cerr << filename << std::endl;
   gdcm::DICOMIStream Is;
   Is.SetFileName(filename);
@@ -29,16 +30,19 @@ void TestPrint(const char *filename)
 
   if( Is.GetNegociatedTS() == gdcm::DICOMIStream::Explicit )
     {
+    std ::cerr << "==gdcm::DICOMIStream::Explicit==" << std::endl;
     PrintDataElements<gdcm::ExplicitDataElement>(Is);
     }
   else
     {
+    std ::cerr << "==gdcm::DICOMIStream::Implicit==" << std::endl;
     PrintDataElements<gdcm::ImplicitDataElement>(Is);
     }
 }
 
 int TestAllPrint(int argc, char *argv[])
 {
+  std ::cerr << std::endl;
   if( argc == 2 )
     {
     const char *filename = argv[1];
