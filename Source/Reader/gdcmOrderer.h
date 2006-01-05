f9c2d1c287739b8b1fc9014def813ae3a664ab11
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-05 01:11:30 +0000

    ENH: Adding some class for reference

---
+++ Source/Reader/gdcmOrderer.h
@@ -0,0 +1,20 @@
+#ifndef __gdcmOrderer_h
+#define __gdcmOrderer_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Class for ordering according to paramater a set of DICOM file
+ * \note 
+ * will use the gdcm::Directory
+ */
+class GDCM_EXPORT Orderer
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmOrderer_h
