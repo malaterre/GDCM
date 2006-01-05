26d6dadbef08681a16a87e3806e027603ba6265a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-05 05:02:17 +0000

    ENH: Move RGBA from Parser

---
+++ Source/Objects/gdcmRGBAReader.h
@@ -0,0 +1,22 @@
+
+#ifndef __gdcmRGBAReader_h
+#define __gdcmRGBAReader_h
+
+#include "gdcmType.h"
+#include <fstream>
+
+namespace gdcm
+{
+/**
+ * \brief Read can convert to RGBA from any DICOM image
+ * \note bla
+ */
+class GDCM_EXPORT RGBAReader
+{
+public:
+private:
+};
+
+}
+
+#endif //__gdcmRGBAReader_h
