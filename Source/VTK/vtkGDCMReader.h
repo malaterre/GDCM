69a35eff83fbb9f1794beaba8562d94c04b881b6
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-05 04:44:41 +0000

    ENH: Move Dicts into Source

---
+++ Source/VTK/vtkGDCMReader.h
@@ -0,0 +1,6 @@
+#ifndef __vtkGDCMReader_h
+#define __vtkGDCMReader_h
+
+
+
+#endif //__vtkGDCMReader_h
---
+++ Source/VTK/vtkGDCMWriter.h
@@ -0,0 +1,5 @@
+#ifndef __vtkGDCMWriter_h
+#define __vtkGDCMWriter_h
+
+
+#endif //__vtkGDCMWriter_h
