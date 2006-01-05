b6275e6c10b2b0bd98dc89badbc348c0153995a7
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-05 04:29:51 +0000

    ENH: Rename Reader into Objects... faute de mieux

---
+++ Source/Objects/gdcmCompressor.h
@@ -0,0 +1,19 @@
+#ifndef __gdcmCompressor_h
+#define __gdcmCompressor_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Class for reprensenting a Compressor
+ * \note bla
+ */
+class GDCM_EXPORT Compressor
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmCompressor_h
