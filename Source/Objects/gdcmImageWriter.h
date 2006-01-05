0a31bd02930ea1b91e9f44854b7972a2d42281c0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-05 04:50:19 +0000

    ENH: Reserve the name

---
+++ Source/Objects/gdcmImageWriter.h
@@ -0,0 +1,20 @@
+#ifndef __gdcmImageWriter_h
+#define __gdcmImageWriter_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Class for that only deal with DICOM Image (precondition)
+ * This one is nasty to do...
+ * \note
+ */
+class GDCM_EXPORT ImageWriter
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmImageWriter_h
