0c264bc969649bed6eb266bca83d91e0ece2109d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-13 17:24:20 +0000

    ENH: Add Dart stuff, and start gdcmLoader/Dumper

---
+++ Source/Objects/gdcmDecompressor.h
@@ -0,0 +1,19 @@
+#ifndef __gdcmDecompressor_h
+#define __gdcmDecompressor_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Class for reprensenting a Decompressor
+ * \note bla
+ */
+class GDCM_EXPORT Decompressor
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmDecompressor_h
