b9b611317da8d84ee767761d948e3ead43c4c1a2
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-16 03:57:39 +0000

    ENH: Adding the Specific Character Set class

---
+++ Source/Objects/gdcmCharacterSet.h
@@ -0,0 +1,23 @@
+#ifndef __gdcmCharacterSet_h
+#define __gdcmCharacterSet_h
+
+namespace gdcm
+{
+/**
+ * \brief Class for reprensenting a CharacterSet
+ * \note bla
+ * Encapsulate the functionality defined by the DICOM Specific Character 
+ * Set attribute. should be able to
+ * - parse the string values of the attribute,
+ * - apply appropriate character conversions from c-string into 
+ *   Unicode (?? to be defined later)
+ */
+class GDCM_EXPORT CharacterSet
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmCharacterSet_h
