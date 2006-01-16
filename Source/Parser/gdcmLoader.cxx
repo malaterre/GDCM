2008176fc74e2d289a565146f6d9544919aba1ed
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-16 05:05:51 +0000

    ENH: Add draft for data loader

---
+++ Source/Parser/gdcmLoader.cxx
@@ -0,0 +1,9 @@
+#include "gdcmLoader.h"
+
+namespace gdcm
+{
+void Loader::Read()
+{
+}
+
+} // end namespace gdcm
---
+++ Source/Parser/gdcmLoader.h
@@ -0,0 +1,27 @@
+
+#ifndef __gdcmLoader_h
+#define __gdcmLoader_h
+
+#include "gdcmDICOMIStream.h"
+
+namespace gdcm
+{
+/**
+ * \brief Actually Load data instead of just parsing
+ *
+ * Detailled description here
+ * \note bla
+ */
+class GDCM_EXPORT Loader : public DICOMIStream
+{
+public:
+  Loader() {}
+  ~Loader() {};
+  void Read();
+
+protected:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmLoader_h
