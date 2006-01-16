e9b7190a5767446860289a6f1294e7a22c2ebcf6
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-16 20:20:33 +0000

    ENH: Maybe I'll a toplevel object to have a centralized place for everything that is common

---
+++ Source/Objects/gdcmObject.h
@@ -0,0 +1,19 @@
+#ifndef __gdcmObject_h
+#define __gdcmObject_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Class for reprensenting a Object
+ * \note bla
+ */
+class GDCM_EXPORT Object
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmObject_h
