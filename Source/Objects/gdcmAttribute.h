5657d857d625829c52eb6fe12ae84a5b2583a6a5
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-16 04:14:35 +0000

    ENH: Adding abstract classes for later concrete subclasses (hopefully generated)

---
+++ Source/Objects/gdcmAttribute.h
@@ -0,0 +1,19 @@
+#ifndef __gdcmAttribute_h
+#define __gdcmAttribute_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Abstract Class for representing an Attribute (PN: Person Name, UL: Unsigned Long...)
+ * \note see concrete sub classed
+ */
+class GDCM_EXPORT Attribute
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmAttribute_h
