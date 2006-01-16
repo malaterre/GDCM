6c84266d5a2415daac3d766265d1c2f8081c4ce7
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-16 04:20:10 +0000

    ENH: Dono yet what this is but all DICOM lib has it

---
+++ Source/Objects/gdcmStructuredReport.h
@@ -0,0 +1,19 @@
+#ifndef __gdcmStructuredReport_h
+#define __gdcmStructuredReport_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Class for reprensenting a StructuredReport
+ * \note bla
+ */
+class GDCM_EXPORT StructuredReport
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmStructuredReport_h
