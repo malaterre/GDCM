8255d9252f3dd013f43c1c9e7e86b1ddc4d7e646
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-02-20 04:34:34 +0000

    STYLE: Minor

--- Source/Parser/gdcmPrinter.cxx
+++ Source/Parser/gdcmPrinter.cxx
@@ -7,15 +7,18 @@
 
 namespace gdcm
 {
+//-----------------------------------------------------------------------------
 Printer::Printer()
 {
   PrintVR = false;
 }
+//-----------------------------------------------------------------------------
 Printer::~Printer()
 {
   PrintVR = false;
 }
 
+//-----------------------------------------------------------------------------
 void PrintImplicitDataElements(gdcm::DICOMIStream &is)
 {
   gdcm::ImplicitDataElement de;
@@ -30,43 +33,43 @@ void PrintImplicitDataElements(gdcm::DICOMIStream &is)
       {
       is.Read(de);
       const gdcm::DictEntry &entry = d.GetDictEntry(de.GetTag());
-//      if( de.GetTag().GetElement() == 0x0 )
-//        {
-//        std::cout << de << "\t # (" << gd.GetName(de.GetTag().GetGroup() )
-//          << ") " << entry.GetName() << std::endl;
-//        }
-//      else
+      //      if( de.GetTag().GetElement() == 0x0 )
+      //        {
+      //        std::cout << de << "\t # (" << gd.GetName(de.GetTag().GetGroup() )
+      //          << ") " << entry.GetName() << std::endl;
+      //        }
+      //      else
         {
         // Use VR from dictionary
         const VR::VRType vr = entry.GetVR();
         if( VR::IsString( vr ) || VR::IsBinary(vr) )
           {
-        std::cout << de << "\t # (" << gd.GetName(de.GetTag().GetGroup() )
-          << ") " << entry.GetName() << std::endl;
+          _os << de << "\t # (" << gd.GetName(de.GetTag().GetGroup() )
+            << ") " << entry.GetName() << std::endl;
           }
-          else
+        else
           {
-        const VM::VMType vm = entry.GetVM();
-        const Value& val = de.GetValue();
-        //std::cout << de << "\t # " << entry.GetName() << std::endl;
-        _os << de.GetTag();
-        if ( false ) // PrintVR
-          {
-          _os << " VR(?)=" << vr;
-          }
-        _os << ",VL=" << std::dec << de.GetValueLength()
-          << " ValueField=[";
+          const VM::VMType vm = entry.GetVM();
+          const Value& val = de.GetValue();
+          //std::cout << de << "\t # " << entry.GetName() << std::endl;
+          _os << de.GetTag();
+          if ( false ) // PrintVR
+            {
+            _os << " VR(?)=" << vr;
+            }
+          _os << ",VL=" << std::dec << de.GetValueLength()
+            << " ValueField=[";
 
-        // Use super class of the template stuff
-        gdcm::Attribute af;
-        af.SetVR(vr);
-        af.SetVM(vm);
-        af.SetLength( val.GetLength() );
-        std::istringstream is;
-        is.str( std::string( val.GetPointer(), val.GetLength() ) );
-        af.Read( is );
-        af.Print( _os );
-        _os << "]\n";
+          // Use super class of the template stuff
+          gdcm::Attribute af;
+          af.SetVR(vr);
+          af.SetVM(vm);
+          af.SetLength( val.GetLength() );
+          std::istringstream is;
+          is.str( std::string( val.GetPointer(), val.GetLength() ) );
+          af.Read( is );
+          af.Print( _os );
+          _os << "]\n";
           }
         }
       }
@@ -77,6 +80,7 @@ void PrintImplicitDataElements(gdcm::DICOMIStream &is)
     }
 }
 
+//-----------------------------------------------------------------------------
 void PrintExplicitDataElements(gdcm::DICOMIStream &is)
 {
   gdcm::ExplicitDataElement de;
@@ -109,24 +113,17 @@ void PrintExplicitDataElements(gdcm::DICOMIStream &is)
 }
 
 
-//template<class DEType>
-//void PrintDataElements(Printer &is)
-//{
-//  PrintDataElement<DEType>(is);
-//}
-
+//-----------------------------------------------------------------------------
 void Printer::Initialize()
 {
   DICOMIStream::Initialize();
 
   if( NegociatedTS == Explicit )
     {
-    //PrintDataElements<gdcm::ExplicitDataElement>(*this);
     PrintExplicitDataElements(*this);
     }
   else
     {
-    //PrintDataElements<gdcm::ImplicitDataElement>(*this);
     PrintImplicitDataElements(*this);
     }
   // FIXME a file that reach eof is not valid...
