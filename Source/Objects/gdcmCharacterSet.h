3a814536a904f51dd873aadba76e06389c97af46
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-03-08 14:38:43 +0000

    ENH: Add coments by JPR

--- Source/Objects/gdcmCharacterSet.h
+++ Source/Objects/gdcmCharacterSet.h
@@ -26,6 +26,35 @@ namespace gdcm
  * f) Graphic Character
  * g) To Invoke
  */
+  
+
+  /*
+   * Comments by JPR:
+    We have to check :
+    0008|0005 [Specific Character Set]
+
+    ASCII         "ISO_IR 6"    (UTF-8)
+    UTF-8         "ISO_IR 192"  (UTF-8)
+    ISO Latin 1   "ISO_IR 100"  (ISO-8859-1)
+    ISO Latin 2   "ISO_IR 101"  (ISO-8859-2)
+    ISO Latin 3   "ISO_IR 109"  (ISO-8859-3)
+    ISO Latin 4   "ISO_IR 110"  (ISO-8859-4)
+    ISO Latin 5   "ISO_IR 148"  (ISO-8859-9)
+    Cyrillic      "ISO_IR 144"  (ISO-8859-5)
+    Arabic        "ISO_IR 127"  (ISO-8859-6)
+    Greek         "ISO_IR 126"  (ISO-8859-7)
+    Hebrew        "ISO_IR 138"  (ISO-8859-8)
+    Japanese
+    ISO_IR 13
+    ISO-IR 13
+    94
+    G1
+    JIS X 0201: Katakana
+    ISO-IR 14
+    94
+    G0
+    JIS X 0201: Romaji 
+    */
 class GDCM_EXPORT CharacterSet
 {
 public:
