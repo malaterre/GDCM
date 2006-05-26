4ff9f243754bd3c657032b8f5b4cbca90a2dad6d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-05-26 03:52:21 +0000

    ENH: valgrind is really smart...

--- Testing/Source/DataStructureAndEncodingDefinition/TestElement.cxx
+++ Testing/Source/DataStructureAndEncodingDefinition/TestElement.cxx
@@ -21,8 +21,8 @@ int TestFD()
   gdcm::Element<gdcm::VR::FD, gdcm::VM::VM1> a = { TPI };
   std::ostringstream os;
   a.Print( os );
-  const char *s = os.str().c_str();
-  std::cout << s << std::endl;
+  std::string st = os.str(); // important
+  const char *s = st.c_str();
   //double t = *reinterpret_cast<const double*>(*s);
   //std::cout << t << std::endl;
 
