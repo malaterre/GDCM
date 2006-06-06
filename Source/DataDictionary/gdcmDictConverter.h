ed41036b1c13946a2cb061a3f7b8f35b6dc3d076
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-06-06 03:37:09 +0000

    ENH: Adding the dict processor

---
+++ Source/DataDictionary/gdcmDictConverter.h
@@ -0,0 +1,67 @@
+
+#ifndef __gdcmDictConverter_h
+#define __gdcmDictConverter_h
+
+#include "gdcmVR.h"
+#include "gdcmVM.h"
+
+namespace gdcm
+{
+
+/**
+ * \brief Class to convert a .dic file into something else:
+ *  - CXX code : embeded dict into shared lib (DICT_DEFAULT)
+ *  - Debug mode (DICT_DEBUG)
+ *  - XML dict (DICT_XML)
+ * \note
+ */
+class DictConverterInternal;
+class GDCM_EXPORT DictConverter
+{
+public:
+  DictConverter();
+  ~DictConverter();
+  void SetInputFileName(const char* filename);
+  const std::string &GetInputFilename() const;
+  void SetOutputFileName(const char* filename);
+  const std::string &GetOutputFilename() const;
+
+  int GetOutputType() const {
+    return OutputType;
+  }
+  void SetOutputType(int type) {
+    OutputType = type;
+  }
+  const std::string &GetDictName() const;
+  void SetDictName(const char *name);
+
+  void Convert();
+
+  // Leaving them public for now. Not really user oriented but may be 
+  // usefull
+  static bool ReadVR(const char *raw, VR::VRType &type);
+  static bool ReadVM(const char *raw, VM::VMType &type);
+  static bool Readuint16(const char *raw, uint16_t &ov);
+
+  enum OutputTypes {
+    DICT_DEFAULT = 0,
+    DICT_DEBUG,
+    DICT_XML
+  };
+
+protected:
+  void WriteHeader();
+  void WriteFooter();
+  bool ConvertToXML(const char *raw, std::string &cxx);
+  bool ConvertToCXX(const char *raw, std::string &cxx);
+  void AddGroupLength();
+
+private:
+  DictConverterInternal *Internal;
+
+  int OutputType;
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmDictConverter_h
--- Testing/Source/DataDictionary/TestDict.cxx
+++ Testing/Source/DataDictionary/TestDict.cxx
@@ -2,5 +2,6 @@
 
 int TestDict(int, char *[])
 {
+  gdcm::Dict d;
   return 0;
 }
