5f21b40be1f9ebf0198d0e73624d6a6fc1b71d85
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-06-17 14:10:13 +0000

    ENH: Do some more template stuff

--- Source/DataStructureAndEncodingDefinition/gdcmImageReader.cxx
+++ Source/DataStructureAndEncodingDefinition/gdcmImageReader.cxx
@@ -13,6 +13,24 @@ ImageReader::~ImageReader()
 {
 }
 
+template <class DEType>
+const char *TemplateGetPointer(DataElement const &de)
+{
+  const DEType &xde =
+    dynamic_cast<const DEType&>(de);
+  const Value &v = xde.GetValue();
+  // C++ would throw an exception when dynamic_cast to a reference
+  // since you could not construct a ref from NULL
+  // using good ol' pointer instead
+  const ByteValue *bv = dynamic_cast<const ByteValue*>(&v);
+  if(bv)
+    {
+    return bv->GetPointer();
+    }
+
+  return 0;
+}
+
 const char *ImageReader::GetPointer() const
 {
   const Tag pixeldata = gdcm::Tag(0x7fe0, 0x0010);
@@ -23,28 +41,11 @@ const char *ImageReader::GetPointer() const
     TS::NegociatedType type = ds.GetNegociatedType();
     if( type == TS::Explicit )
       {
-      const ExplicitDataElement &xde =
-        dynamic_cast<const ExplicitDataElement&>(de);
-      const Value &v = xde.GetValue();
-      // C++ would throw an exception when dynamic_cast to a reference
-      // since you could not construct a ref from NULL
-      // using good ol' pointer instead
-      const ByteValue *bv = dynamic_cast<const ByteValue*>(&v);
-      if(bv)
-        return bv->GetPointer();
-      else
-        return 0;
+      return TemplateGetPointer<ExplicitDataElement>(de);
       }
     else if( type == TS::Implicit )
       {
-      const ImplicitDataElement &xde =
-        dynamic_cast<const ImplicitDataElement&>(de);
-      const Value &v = xde.GetValue();
-      const ByteValue *bv = dynamic_cast<const ByteValue*>(&v);
-      if(bv)
-        return bv->GetPointer();
-      else
-        return 0;
+      return TemplateGetPointer<ImplicitDataElement>(de);
       }
     else
       {
@@ -55,6 +56,7 @@ const char *ImageReader::GetPointer() const
     {
     gdcmWarningMacro( "No Pixel Data Found" );
     }
+
   return 0;
 }
 
