fe643d56435bed3cf24326a0e8f86239df6fb33a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-06-25 21:40:18 +0000

    ENH: Update with a couple of low level Module Attributes tables

---
+++ Source/InformationObjectDefinition/gdcmModule.h
@@ -0,0 +1,20 @@
+#ifndef __gdcmModule_h
+#define __gdcmModule_h
+
+
+namespace gdcm
+{
+/**
+ * \brief Class for representing a Module
+ * \note bla
+ * Module: A set of Attributes within an Information Entity or Normalized IOD which are logically related to each other.
+ */
+class GDCM_EXPORT Module
+{
+public:
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmModule_h
---
+++ Source/InformationObjectDefinition/gdcmTableReader.cxx
@@ -0,0 +1,61 @@
+#include "gdcmTableReader.h"
+#include <iostream>
+#include <fstream>
+#include "expat/lib/expat.h"
+
+namespace gdcm
+{
+#ifdef XML_LARGE_SIZE
+#if defined(XML_USE_MSC_EXTENSIONS) && _MSC_VER < 1400
+#define XML_FMT_INT_MOD "I64"
+#else
+#define XML_FMT_INT_MOD "ll"
+#endif
+#else
+#define XML_FMT_INT_MOD "l"
+#endif
+
+static void XMLCALL startElement(void *userData, const char *name, const char **atts)
+{
+  int i;
+  int *depthPtr = (int *)userData;
+  for (i = 0; i < *depthPtr; i++)
+    putchar('\t');
+  puts(name);
+  *depthPtr += 1;
+}
+
+static void XMLCALL endElement(void *userData, const char *name)
+{
+  int *depthPtr = (int *)userData;
+  *depthPtr -= 1;
+}
+
+int TableReader::Read()
+{
+  std::ifstream is( Filename.c_str() );
+
+  char buf[BUFSIZ];
+  XML_Parser parser = XML_ParserCreate(NULL);
+  int done;
+  int depth = 0;
+  XML_SetUserData(parser, &depth);
+  XML_SetElementHandler(parser, startElement, endElement);
+  do {
+    is.read(buf, sizeof(buf));
+    size_t len = is.gcount();
+    done = len < sizeof(buf);
+    if (XML_Parse(parser, buf, len, done) == XML_STATUS_ERROR) {
+      fprintf(stderr,
+        "%s at line %" XML_FMT_INT_MOD "u\n",
+        XML_ErrorString(XML_GetErrorCode(parser)),
+        XML_GetCurrentLineNumber(parser));
+      return 1;
+    }
+  } while (!done);
+  XML_ParserFree(parser);
+  is.close();
+  return 0;
+}
+
+} // end namespace gdcm
