60e386e1c54f4d1c2a2c9aeedc4fa5f0d4452c2a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-06-25 22:16:07 +0000

    ENH: Quick fix to make it compile on linux

--- Utilities/expat/lib/xmlparse.c
+++ Utilities/expat/lib/xmlparse.c
@@ -8,15 +8,7 @@
 
 #define XML_BUILDING_EXPAT 1
 
-#ifdef COMPILED_FROM_DSP
-#include "winconfig.h"
-#elif defined(MACOS_CLASSIC)
-#include "macconfig.h"
-#elif defined(__amigaos4__)
-#include "amigaconfig.h"
-#elif defined(HAVE_EXPAT_CONFIG_H)
-#include <expat_config.h>
-#endif /* ndef COMPILED_FROM_DSP */
+#include "expat_config.h"
 
 #include "expat.h"
 
