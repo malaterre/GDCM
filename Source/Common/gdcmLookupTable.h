ab5a26b9b3264243512ed8dc8cb8192eb478d5f2
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-06-29 16:11:26 +0000

    ENH: adding an empty lookup table

---
+++ Source/Common/gdcmLookupTable.h
@@ -0,0 +1,19 @@
+
+#ifndef __gdcmLookupTable_h
+#define __gdcmLookupTable_h
+
+#include "gdcmTypes.h"
+
+namespace gdcm
+{
+  
+class LookupTable
+{
+public:
+  LookupTable() {}
+  ~LookupTable() {}
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmLookupTable_h
