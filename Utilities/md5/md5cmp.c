edec3894b3a9c7841b41e4ed9377adae6d13cab3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-07-12 21:30:36 +0000

    ENH: Small executable to compare two files

--- Utilities/md5/md5cmp.c
+++ Utilities/md5/md5cmp.c
@@ -1,35 +1,54 @@
 #include "md5.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+void process_file(const char *filename, md5_byte_t *digest)
+{
+  int di;
+  size_t file_size, read;
+  void *buffer;
+  md5_state_t state;
+  FILE *file = fopen(filename, "rb");
+
+  /* go to the end */
+  /*int*/ fseek(file, 0, SEEK_END);
+  file_size = ftell(file);
+  /*int*/ fseek(file, 0, SEEK_SET);
+  buffer = malloc(file_size);
+  read = fread(buffer, 1, file_size, file);
+  assert( read == file_size );
+
+  md5_init(&state);
+  md5_append(&state, (const md5_byte_t *)buffer, file_size);
+  md5_finish(&state, digest);
+  /*printf("MD5 (\"%s\") = ", test[i]); */
+  for (di = 0; di < 16; ++di)
+  {
+    printf("%02x", digest[di]);
+  }
+  printf("\t%s\n", filename);
+  free(buffer);
+  fclose(file);
+}
+
 int main(int argc, char *argv[])
 {
-
+  md5_byte_t digest1[16];
+  md5_byte_t digest2[16];
   if( argc < 3 )
   {
     return 1;
   }
-  md5_state_t state;
-  md5_byte_t digest[16];
 
-  const char *filename1 = argv[1];
-  const char *filename2 = argv[2];
-  FILE *file1 = fopen(filename1, "rb");
-  FILE *file2 = fopen(filename2, "rb");
+  /* Do file1 */
+  process_file(argv[1], digest1);
 
-  // go to the end
-  /*int*/ fseek(file1, 0, SEEK_END);
-  long file1_size = ftell(file1);
-  /*int*/ fseek(file1, 0, SEEK_SET);
-  void *buffer = malloc(file1_size);
-  /*size_t*/ fread(buffer, 1, file1_size, file1);
+  /* Do file2 */
+  process_file(argv[2], digest2);
 
-  md5_init(&state);
-  md5_append(&state, (const md5_byte_t *)buffer, file1_size);
-  md5_finish(&state, digest);
-  //printf("MD5 (\"%s\") = ", test[i]);
-  for (di = 0; di < 16; ++di)
-  printf("%02x", digest[di]);
-  printf("\n");
-
-  return 0;
+  return memcmp(digest1, digest2, 16);
 }
 
