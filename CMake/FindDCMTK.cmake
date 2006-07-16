480efdd8fbf9cab2084a1e58704f7bd33c561d7c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-07-16 02:29:47 +0000

    ENH: Adding DCMTK tests (commented out for now)

---
+++ CMake/FindDCMTK.cmake
@@ -0,0 +1,157 @@
+# - find DCMTK libraries and applications
+#
+
+#  DCMTK_INCLUDE_DIR   - Directories to include to use DCMTK
+#  DCMTK_LIBRARIES     - Files to link against to use DCMTK
+#  DCMTK_FOUND         - If false, don't try to use DCMTK
+#  DCMTK_DIR           - (optional) Source directory for DCMTK
+#
+# DCMTK_DIR can be used to make it simpler to find the various include
+# directories and compiled libraries if you've just compiled it in the
+# source tree. Just set it to the root of the tree where you extracted
+# the source.
+#
+# Written for VXL by Amitha Perera.
+# Upgraded for GDCM by Mathieu Malaterre.
+# 
+# For debian you need:
+# sudo apt-get install libdcmtk1-dev dcmtk
+
+IF( NOT DCMTK_FOUND )
+  SET( DCMTK_DIR "/usr/include/dcmtk/"
+    CACHE PATH "Root of DCMTK source tree (optional)." )
+  MARK_AS_ADVANCED( DCMTK_DIR )
+ENDIF( NOT DCMTK_FOUND )
+
+
+FIND_PATH( DCMTK_config_INCLUDE_DIR osconfig.h
+  ${DCMTK_DIR}/config/include
+  ${DCMTK_DIR}/config
+  ${DCMTK_DIR}/include
+)
+
+FIND_PATH( DCMTK_ofstd_INCLUDE_DIR ofstdinc.h
+  ${DCMTK_DIR}/ofstd/include
+  ${DCMTK_DIR}/ofstd
+  ${DCMTK_DIR}/include/ofstd
+)
+
+FIND_LIBRARY( DCMTK_ofstd_LIBRARY ofstd
+  ${DCMTK_DIR}/ofstd/libsrc
+  ${DCMTK_DIR}/ofstd/libsrc/Release
+  ${DCMTK_DIR}/ofstd/libsrc/Debug
+  ${DCMTK_DIR}/ofstd/Release
+  ${DCMTK_DIR}/ofstd/Debug
+  ${DCMTK_DIR}/lib
+)
+
+
+FIND_PATH( DCMTK_dcmdata_INCLUDE_DIR dctypes.h
+  ${DCMTK_DIR}/include/dcmdata
+  ${DCMTK_DIR}/dcmdata
+  ${DCMTK_DIR}/dcmdata/include
+)
+
+FIND_LIBRARY( DCMTK_dcmdata_LIBRARY dcmdata
+  ${DCMTK_DIR}/dcmdata/libsrc
+  ${DCMTK_DIR}/dcmdata/libsrc/Release
+  ${DCMTK_DIR}/dcmdata/libsrc/Debug
+  ${DCMTK_DIR}/dcmdata/Release
+  ${DCMTK_DIR}/dcmdata/Debug
+  ${DCMTK_DIR}/lib
+)
+
+
+FIND_PATH( DCMTK_dcmimgle_INCLUDE_DIR dcmimage.h
+  ${DCMTK_DIR}/dcmimgle/include
+  ${DCMTK_DIR}/dcmimgle
+  ${DCMTK_DIR}/include/dcmimgle
+)
+
+FIND_LIBRARY( DCMTK_dcmimgle_LIBRARY dcmimgle
+  ${DCMTK_DIR}/dcmimgle/libsrc
+  ${DCMTK_DIR}/dcmimgle/libsrc/Release
+  ${DCMTK_DIR}/dcmimgle/libsrc/Debug
+  ${DCMTK_DIR}/dcmimgle/Release
+  ${DCMTK_DIR}/dcmimgle/Debug
+  ${DCMTK_DIR}/lib
+)
+
+#FIND_LIBRARY(DCMTK_imagedb_LIBRARY imagedb 
+#  ${DCMTK_DIR}/imagectn/libsrc/Release
+#  ${DCMTK_DIR}/imagectn/libsrc/
+#  ${DCMTK_DIR}/imagectn/libsrc/Debug
+#  )
+
+FIND_LIBRARY(DCMTK_dcmnet_LIBRARY dcmnet 
+  ${DCMTK_DIR}/dcmnet/libsrc/Release
+  ${DCMTK_DIR}/dcmnet/libsrc/Debug
+  ${DCMTK_DIR}/dcmnet/libsrc/
+  )
+
+
+IF( DCMTK_config_INCLUDE_DIR )
+IF( DCMTK_ofstd_INCLUDE_DIR )
+IF( DCMTK_ofstd_LIBRARY )
+IF( DCMTK_dcmdata_INCLUDE_DIR )
+IF( DCMTK_dcmdata_LIBRARY )
+IF( DCMTK_dcmimgle_INCLUDE_DIR )
+IF( DCMTK_dcmimgle_LIBRARY )
+
+  SET( DCMTK_FOUND "YES" )
+  SET( DCMTK_INCLUDE_DIR
+    ${DCMTK_config_INCLUDE_DIR}
+    ${DCMTK_ofstd_INCLUDE_DIR}
+    ${DCMTK_dcmdata_INCLUDE_DIR}
+    ${DCMTK_dcmimgle_INCLUDE_DIR}
+  )
+
+  SET( DCMTK_LIBRARIES
+    ${DCMTK_dcmimgle_LIBRARY}
+    ${DCMTK_dcmdata_LIBRARY}
+    ${DCMTK_ofstd_LIBRARY}
+    ${DCMTK_config_LIBRARY}
+  )
+
+  IF(DCMTK_imagedb_LIBRARY)
+   SET( DCMTK_LIBRARIES
+   ${DCMTK_LIBRARIES}
+   ${DCMTK_imagedb_LIBRARY}
+   )
+  ENDIF(DCMTK_imagedb_LIBRARY)
+
+  IF(DCMTK_dcmnet_LIBRARY)
+   SET( DCMTK_LIBRARIES
+   ${DCMTK_LIBRARIES}
+   ${DCMTK_dcmnet_LIBRARY}
+   )
+  ENDIF(DCMTK_dcmnet_LIBRARY)
+
+  IF( WIN32 )
+    SET( DCMTK_LIBRARIES ${DCMTK_LIBRARIES} netapi32 )
+  ENDIF( WIN32 )
+
+ENDIF( DCMTK_dcmimgle_LIBRARY )
+ENDIF( DCMTK_dcmimgle_INCLUDE_DIR )
+ENDIF( DCMTK_dcmdata_LIBRARY )
+ENDIF( DCMTK_dcmdata_INCLUDE_DIR )
+ENDIF( DCMTK_ofstd_LIBRARY )
+ENDIF( DCMTK_ofstd_INCLUDE_DIR )
+ENDIF( DCMTK_config_INCLUDE_DIR )
+
+FIND_PROGRAM(DCMTK_DCMDUMP dcmdump
+  ${DCMTK_DIR}/bin
+  )
+
+MARK_AS_ADVANCED(
+  DCMTK_DCMDUMP
+  DCMTK_config_INCLUDE_DIR
+  DCMTK_dcmdata_INCLUDE_DIR
+  DCMTK_dcmdata_LIBRARY
+  DCMTK_dcmimgle_INCLUDE_DIR
+  DCMTK_dcmimgle_LIBRARY
+  DCMTK_dcmnet_LIBRARY
+  DCMTK_ofstd_INCLUDE_DIR
+  DCMTK_ofstd_LIBRARY
+  )
+
