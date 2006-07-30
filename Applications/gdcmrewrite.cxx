c4b928e1815ac610e8a4f31cb64124c19157b5af
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-07-30 01:37:09 +0000

    ENH: Adding a template app

---
+++ Applications/gdcmrewrite.cxx
@@ -0,0 +1,132 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/*
+ * Tools to rewrite. Goals being to 'purify' a DICOM file, eg:
+ * - Rewrite PMS SQ into DICOM SQ
+ * - Rewrite Implicit SQ with defined length as undefined length
+ * - Set appropriate VR from DICOM dict
+ *
+ *
+ * - Later on, it should run through a Validator
+ *   which will make sure all field 1, 1C are present and those only
+ * - In a perfect world I should remove private tags and transform them into
+ *   public fields.
+ */
+#include "gdcmReader.h"
+#include "gdcmFileMetaInformation.h"
+#include "gdcmDataSet.h"
+
+#include <string>
+#include <iostream>
+
+#include <stdio.h>     /* for printf */
+#include <stdlib.h>    /* for exit */
+#include <getopt.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+  int c;
+  //int digit_optind = 0;
+
+  std::string filename;
+  while (1) {
+    //int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"input", 1, 0, 0},
+        {"output", 1, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    c = getopt_long (argc, argv, "i:o:",
+      long_options, &option_index);
+    if (c == -1)
+      {
+      break;
+      }
+
+    switch (c)
+      {
+    case 0:
+        {
+        const char *s = long_options[option_index].name;
+        printf ("option %s", s);
+        if (optarg)
+          {
+          if( option_index == 0 ) /* input */
+            {
+            assert( strcmp(s, "input") == 0 );
+            assert( filename.empty() );
+            filename = optarg;
+            }
+          printf (" with arg %s", optarg);
+          }
+        printf ("\n");
+        }
+      break;
+
+    case 'i':
+      printf ("option i with value ’%s’\n", optarg);
+      assert( filename.empty() );
+      filename = optarg;
+      break;
+
+    case 'o':
+      printf ("option o with value ’%s’\n", optarg);
+      break;
+
+    case '?':
+      break;
+
+    default:
+      printf ("?? getopt returned character code 0%o ??\n", c);
+      }
+  }
+
+  if (optind < argc)
+    {
+    printf ("non-option ARGV-elements: ");
+    while (optind < argc)
+      {
+      printf ("%s ", argv[optind++]);
+      }
+    printf ("\n");
+    }
+
+  if( filename.empty() )
+    {
+    return 1;
+    }
+  // else
+  //std::cout << "Filename: " << filename << std::endl;
+  gdcm::Reader reader;
+  reader.SetFileName( filename.c_str() );
+  if( !reader.Read() )
+    {
+    std::cerr << "Failed to read: " << filename << std::endl;
+    return 1;
+    }
+
+  const gdcm::FileMetaInformation &h = reader.GetHeader();
+  std::cout << h << std::endl;
+
+  const gdcm::DataSet &ds = reader.GetDataSet();
+  std::cout << ds << std::endl;
+
+  return 0;
+}
+
