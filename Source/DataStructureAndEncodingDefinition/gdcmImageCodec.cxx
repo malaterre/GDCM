38c85879da779f8ee02adc0c0b1e11a36a7e1615
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-08-08 06:45:23 +0000

    STYLE: indent

--- Source/DataStructureAndEncodingDefinition/gdcmImageCodec.cxx
+++ Source/DataStructureAndEncodingDefinition/gdcmImageCodec.cxx
@@ -101,42 +101,44 @@ bool ImageCodec::DoYBR(IStream &is, OStream &os)
   is.Seekg(start, std::ios::beg); // reset
   SwapCode sc = is.GetSwapCode();
 
-    assert( !(buf_size % 3) );
-    unsigned long size = buf_size/3;
-    unsigned char *copy = new unsigned char[ buf_size ];
-    memmove( copy, dummy_buffer, buf_size);
+  // Code is coming from:
+  // http://lestourtereaux.free.fr/papers/data/yuvrgb.pdf
+  assert( !(buf_size % 3) );
+  unsigned long size = buf_size/3;
+  unsigned char *copy = new unsigned char[ buf_size ];
+  memmove( copy, dummy_buffer, buf_size);
 
-    const unsigned char *a = copy + 0;
-    const unsigned char *b = copy + size;
-    const unsigned char *c = copy + size + size;
-    int R, G, B;
+  const unsigned char *a = copy + 0;
+  const unsigned char *b = copy + size;
+  const unsigned char *c = copy + size + size;
+  int R, G, B;
 
-    unsigned char *p = (unsigned char*)dummy_buffer;
-    for (unsigned long j = 0; j < size; ++j)
-      {
-      R = 38142 *(*a-16) + 52298 *(*c -128);
-      G = 38142 *(*a-16) - 26640 *(*c -128) - 12845 *(*b -128);
-      B = 38142 *(*a-16) + 66093 *(*b -128);
+  unsigned char *p = (unsigned char*)dummy_buffer;
+  for (unsigned long j = 0; j < size; ++j)
+    {
+    R = 38142 *(*a-16) + 52298 *(*c -128);
+    G = 38142 *(*a-16) - 26640 *(*c -128) - 12845 *(*b -128);
+    B = 38142 *(*a-16) + 66093 *(*b -128);
 
-      R = (R+16384)>>15;
-      G = (G+16384)>>15;
-      B = (B+16384)>>15;
+    R = (R+16384)>>15;
+    G = (G+16384)>>15;
+    B = (B+16384)>>15;
 
-      if (R < 0)   R = 0;
-      if (G < 0)   G = 0;
-      if (B < 0)   B = 0;
-      if (R > 255) R = 255;
-      if (G > 255) G = 255;
-      if (B > 255) B = 255;
+    if (R < 0)   R = 0;
+    if (G < 0)   G = 0;
+    if (B < 0)   B = 0;
+    if (R > 255) R = 255;
+    if (G > 255) G = 255;
+    if (B > 255) B = 255;
 
-      *(p++) = (unsigned char)R;
-      *(p++) = (unsigned char)G;
-      *(p++) = (unsigned char)B;
-      a++;
-      b++;
-      c++;
-      }
-    delete[] copy;
+    *(p++) = (unsigned char)R;
+    *(p++) = (unsigned char)G;
+    *(p++) = (unsigned char)B;
+    a++;
+    b++;
+    c++;
+    }
+  delete[] copy;
 
   os.Write(dummy_buffer, buf_size);
   return true;
