92e42812e44f772fb8ec0a4129225e5b582496e1
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-08-18 19:05:59 +0000

    ENH: Improve coverage

--- Testing/Source/Common/TestFilename.cxx
+++ Testing/Source/Common/TestFilename.cxx
@@ -1,12 +1,51 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "gdcmFilename.h"
+#include "gdcmSystem.h"
 
 #include <iostream>
 
-int TestFilename(int, char *[])
+int TestFilename(int argc, char *argv[])
 {
+  (void)argc;
   gdcm::Filename f("/tmp/debug.dcm");
   std::cout << f.GetPath() << std::endl;
   std::cout << f.GetName() << std::endl;
   
+  const char current[] = "/home/mathieu/Creatis/gdcmData/test.acr";
+  if( !gdcm::System::FileExists( current ) )
+    {
+    return 1;
+    }
+  std::cerr << "Current:" << current << std::endl;
+  gdcm::Filename fn(current);
+  std::cerr << fn.GetPath() << std::endl;
+  std::string current2 = fn.GetPath();
+  current2 += "/./";
+  current2 += fn.GetName();
+  std::cerr << current2 << std::endl;
+  if( current2 == current )
+    {
+    return 1;
+    }
+  gdcm::Filename fn2(current2.c_str());
+  if( !fn.IsIdentical(fn2))
+    {
+    return 1;
+    }
+
   return 0;
 }
