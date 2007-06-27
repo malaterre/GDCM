ca997b98b748c5175c78ee14b0e679ee37cb2338
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-06-27 15:36:38 +0000

    STYLE: fix comment

--- Source/MediaStorageAndFileFormat/gdcmValidate.h
+++ Source/MediaStorageAndFileFormat/gdcmValidate.h
@@ -16,13 +16,6 @@
 #ifndef __gdcmValidate_h
 #define __gdcmValidate_h
 
-// TODO Class to implement printing
-// Since DICOM does printing ?
-// Also I would like to encapsulate the IsCharacterPrintable thing 
-// (to avoid printing \0 and other weird characters)
-// \todo I still need to implement skiping of group (shadow)
-// need to implement longer field to read
-
 #include "gdcmFile.h"
 
 namespace gdcm
