7a2217b61bbba7aaf7fdfa5dca982fce8888ae96
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-08-24 18:15:34 +0000

    ENH: update from laptop

--- Testing/Source/InformationObjectDefinition/TestModule.cxx
+++ Testing/Source/InformationObjectDefinition/TestModule.cxx
@@ -13,29 +13,12 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
-#include "gdcmElement.h"
-#include "gdcmVR.h"
+#include "gdcmModule.h"
 
-struct dummy
+int TestModule(int argc, char *argv[])
 {
-  unsigned short I[3];
-};
-//<entry group="0018" element="1624" retired="false" version="3">
-//      <representation vr="US" vm="3"/>
-
-template<int,int> struct TagToElement;
-template<> struct TagToElement<0x0018,0x1624> {
-  typedef gdcm::Element<gdcm::VR::US,gdcm::VM::VM3> Type;
-};
-
-int TestElement2(int, char *[])
-{
-  gdcm::Element<gdcm::VR::US, gdcm::VM::VM3> ref = {0,1,2};
-  dummy d = {0,1,2};
-
-  TagToElement<0x0018,0x1624>::Type t = {1,2,3};
-  t.Print( std::cout );
-  std::cout << std::endl;
+  gdcm::Module module;
 
   return 0;
 }
+
