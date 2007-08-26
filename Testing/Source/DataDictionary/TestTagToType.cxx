f61e49b82913b97d8ddec374c9c5c8d28615d32a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-08-26 16:00:37 +0000

    ENH: save from ibm

--- Testing/Source/DataDictionary/TestTagToType.cxx
+++ Testing/Source/DataDictionary/TestTagToType.cxx
@@ -11,12 +11,13 @@ int TestTagToType(int, char *[])
   gdcm::TagToType<0x0028,0x0011>::Type v3 = 0x8000;
   std::cout << v3 << std::endl;
 
-  // Make sure the group length wors too:
+  // Make sure the group length works too:
   gdcm::TagToType<0x0009,0x0000>::Type l1 = 0xF0000000;
   std::cout << l1 << std::endl;
   gdcm::TagToType<0x0010,0x0000>::Type l2 = 0xF0000000;
   std::cout << l2 << std::endl;
   gdcm::TagToType<0x1234,0x0000>::Type l3 = 0xF0000000;
   std::cout << l3 << std::endl;
+
   return 0;
 }
