6ee28aeee08950f94313d5a0940922a43ffbb977
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-12-04 17:42:24 +0000

    ENH: add test for DICOMDIR

---
+++ Testing/Source/MediaStorageAndFileFormat/TestDICOMDIR.cxx
@@ -0,0 +1,35 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2007 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmDICOMDIR.h"
+#include "gdcmFileSet.h"
+
+int TestDICOMDIR(int argc, char *argv[])
+{
+  (void)argc;
+  (void)argv;
+  gdcm::DICOMDIR dd;
+
+  gdcm::FileSet fs;
+  gdcm::File f1;
+  gdcm::File f2;
+  fs.AddFile( f1 );
+  fs.AddFile( f2 );
+
+  gdcm::DICOMDIR dd2(fs);
+
+  return 0;
+}
+
