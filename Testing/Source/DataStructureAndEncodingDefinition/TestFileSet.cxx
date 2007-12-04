4d9cab3d083c130e0bdeaabfa9dcc2adf87b5c74
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-12-04 17:38:05 +0000

    ENH: add tests

---
+++ Testing/Source/DataStructureAndEncodingDefinition/TestFileSet.cxx
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2007 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmFileSet.h"
+#include "gdcmFile.h"
+
+int TestFileSet(int argc, char *argv[])
+{
+  (void)argc;
+  (void)argv;
+  gdcm::FileSet fs;
+  gdcm::File f1;
+  gdcm::File f2;
+  fs.AddFile( f1 );
+  fs.AddFile( f2 );
+
+  return 0;
+}
