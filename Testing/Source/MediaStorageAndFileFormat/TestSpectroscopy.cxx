3278d01527118eca4762d22cfa16ba626bb6942e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-12-05 13:23:17 +0000

    ENH: add test for new objects

---
+++ Testing/Source/MediaStorageAndFileFormat/TestSpectroscopy.cxx
@@ -0,0 +1,25 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2007 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmSpectroscopy.h"
+
+int TestSpectroscopy(int argc, char *argv[])
+{
+  (void)argc;
+  (void)argv;
+  gdcm::Spectroscopy w;
+
+  return 0;
+}
