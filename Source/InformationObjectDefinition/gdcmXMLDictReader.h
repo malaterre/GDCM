41f5c5c985796a9f63eccb793f2db954befb674b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-12-11 16:14:13 +0000

    ENH: add XLM dict reader

---
+++ Source/InformationObjectDefinition/gdcmXMLDictReader.h
@@ -0,0 +1,56 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2007 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef __gdcmXMLDictReader_h
+#define __gdcmXMLDictReader_h
+
+#include "gdcmTableReader.h"
+#include "gdcmDict.h"
+#include "gdcmDictEntry.h"
+#include "gdcmTag.h"
+
+namespace gdcm
+{
+/**
+ * \brief Class for representing a XMLDictReader
+ * \note bla
+ */
+class GDCM_EXPORT XMLDictReader : public TableReader
+{
+public:
+  XMLDictReader();
+  ~XMLDictReader() {}
+
+  void StartElement(const char *name, const char **atts);
+  void EndElement(const char *name);
+  void CharacterDataHandler(const char *data, int length);
+
+  const Dict & GetDict() { return DICOMDict; }
+
+protected:
+  void HandleEntry(const char **atts);
+  void HandleDescription(const char **atts);
+
+private:
+  Dict DICOMDict;
+  Tag CurrentTag;
+  DictEntry CurrentDE;
+  bool ParsingDescription;
+  std::string Description;
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmXMLDictReader_h
