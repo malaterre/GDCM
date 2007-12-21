0038a55a6eb8bfcd3e68ed627a2e0e3ea5b75c8e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2007-12-21 13:59:15 +0000

    ENH: update license

--- Utilities/VTK/vtkGDCMReader.h
+++ Utilities/VTK/vtkGDCMReader.h
@@ -1,11 +1,12 @@
 /*=========================================================================
 
-  Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkGDCMReader.h,v $
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
 
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  Copyright (c) 2006-2007 Mathieu Malaterre
+  Copyright (c) 1993-2005 CREATIS
   All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
 
      This software is distributed WITHOUT ANY WARRANTY; without even
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
