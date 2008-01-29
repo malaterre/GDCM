2ab08d7790b37580d99433e4eaf276282a7ce57d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-01-29 14:48:17 +0000

    ENH: add test

---
+++ Testing/Source/MediaStorageAndFileFormat/Python/TestUIDGenerator.py
@@ -0,0 +1,40 @@
+############################################################################
+#
+#  Program: GDCM (Grass Root DICOM). A DICOM library
+#  Module:  $URL$
+#
+#  Copyright (c) 2006-2008 Mathieu Malaterre
+#  All rights reserved.
+#  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+#
+#     This software is distributed WITHOUT ANY WARRANTY; without even
+#     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#     PURPOSE.  See the above copyright notice for more information.
+#
+############################################################################
+
+import gdcm
+import os,sys
+
+try:
+  filename = os.sys.argv[1]
+except:
+  # failure
+  print "Need a filename"
+  sys.exit(1)
+
+uid = gdcm.UIDGenerator()
+for i in range(0,100):
+  print uid.Generate()
+
+MY_ROOT = "1.2.3.4"
+
+# static function:
+gdcm.UIDGenerator_SetRoot( MY_ROOT )
+for i in range(0,100):
+  print uid.Generate()
+
+
+# Test succeed ?
+sys.exit(0)
+
