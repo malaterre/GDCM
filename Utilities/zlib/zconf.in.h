5cb4743940fe6048788f4a6038645fb64ae5ae5a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-01-29 13:52:42 +0000

    ENH: try to do zlib correctly

--- Utilities/zlib/zconf.in.h
+++ Utilities/zlib/zconf.in.h
@@ -8,6 +8,8 @@
 #ifndef ZCONF_H
 #define ZCONF_H
 
+#cmakedefine ZLIB_DLL
+
 /*
  * If you *really* need a unique prefix for all types and library functions,
  * compile with -DZ_PREFIX. The "standard" zlib should be compiled without it.
