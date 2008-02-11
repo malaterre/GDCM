b1255e5eeb91719b1985509e0cb0fa7414c0f4a9
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-02-11 16:00:41 +0000

    ENH: Found a trick to avoid the static data array initialization problem :)

--- Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestAttribute5.cxx
+++ Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestAttribute5.cxx
@@ -1,9 +1,25 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "gdcmAttribute.h"
 
 int main()
 {
   gdcm::Attribute<0x0018,0x1182, gdcm::VR::IS, gdcm::VM::VM1> fd = {0};
+  fd.Print( std::cout );
 
+/*
   bool b = gdcm::VR::OB & gdcm::VR::UL;
   std::cout << b << std::endl;
 
@@ -21,6 +37,6 @@ int main()
 
   // Let's if we can construct a private element:
   gdcm::Attribute<0x1233,0x5678, gdcm::VR::IS, gdcm::VM::VM1> fd2 = {0};
-
+*/
   return 0;
 }
