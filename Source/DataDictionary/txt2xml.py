d757937a6174e868f75592559d0dc8870ba12a2e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-02-12 17:48:50 +0000

    ENH: more work

--- Source/DataDictionary/txt2xml.py
+++ Source/DataDictionary/txt2xml.py
@@ -18,7 +18,7 @@ class TextParser:
     for line in infile.readlines():
       patt = re.compile("^\s*([A-Za-z0-9 «»%;#()./,_:>-]+)\s+\(?([0-9A-Fn]+),\s?([0-9A-FxX]+)\)?\s+([A-Z][A-Z])\s+([0-9Nn-]+)\s*$")
       patt1 = re.compile("^\s*([A-Za-z0-9 %#()./,_:>-]+)\s+\(([0-9A-F]+),\s?([0-9A-FxX]+)\)\s+([1-3C]+)\s+([A-Z][A-Z])\s+([0-9Nn-]+)\s*$")
-      patt2 = re.compile( "^\s*([Table ]*[A-Z1-9.-]+)\s+([A-Za-z -]+)\s+\(([A-Z0-9_]+)\)\s*$")
+      patt2 = re.compile( "^\s*([Table ]*[A-Z1-9.:-]+)\s+([A-Za-z -]+)\s+\(([A-Z0-9_]+)\)\s*$")
       #patt3 = re.compile( '^\s*Private Creator Identification\s*\((["A-Za-z0-9() ./])\)\s*$' )
       patt3 = re.compile( '^\s*Private Creator Identification\s*\("?(.*)"?\)\)?\s*$' )
       patt4 = re.compile( '^\s*Private Creator Identification\s*([A-Z0-9_]+)\s*$' )
@@ -42,15 +42,15 @@ class TextParser:
         outLines.append( dicom )
       elif m2:
         # <dict edition="2007" url="http://??" ref="Table A-16" name="Private Creator Identification - Xeleris" owner="GEMS_GENIE_1">
-        s = "<dict ref=\"%s\" name=\"%s\" owner=\"%s\">"%(m2.group(1),m2.group(2).rstrip(),m2.group(3))
+        s = "</dict><dict ref=\"%s\" name=\"%s\" owner=\"%s\">"%(m2.group(1),m2.group(2).rstrip(),m2.group(3))
         s += '\n'
         outLines.append( s )
       elif m3:
-        s = "<dict ref=\"%s\" name=\"%s\" owner=\"%s\">"%("??","??",m3.group(1))
+        s = "</dict><dict ref=\"%s\" name=\"%s\" owner=\"%s\">"%("??","??",m3.group(1))
         s += '\n'
         outLines.append( s )
       elif m4:
-        s = "<dict ref=\"%s\" name=\"%s\" owner=\"%s\">"%("??","??",m4.group(1))
+        s = "</dict><dict ref=\"%s\" name=\"%s\" owner=\"%s\">"%("??","??",m4.group(1))
         s += '\n'
         outLines.append( s )
       else:
@@ -111,7 +111,7 @@ class TextParser3:
     infile = file(inputfilename, 'r')
     outLines = []
     for line in infile.readlines():
-      patt = re.compile("^\s*\(([0-9A-Z]+),([0-9A-Zx]+)\)\s+([A-Z0-9._ -]+)\s\s+([A-Za-z0-9 ()._,/#>-]+)\s+([A-Z][A-Z]_?O?W?)\s+([0-9n-]+)\s*$")
+      patt = re.compile("^\s*\(([0-9A-Z]+),([0-9A-Zx]+)\)\s+([A-Za-z0-9./:_ -]+)\s\s+([A-Za-z0-9 ()._,/#>-]+)\s+([A-Z][A-Z]_?O?W?)\s+([0-9n-]+)\s*$")
       patt2 = re.compile( "^\s*([A-Z1-9.-]+)\s*([A-Za-z -]+)\s*$")
       m = patt.match(line)
       m2 = patt2.match(line)
@@ -193,7 +193,7 @@ class TextParser5:
       m = patt.match(line)
       if m:
         # <entry group="0001" element="0001" vr="LO" vm="1" owner="Private Creator"/>
-        dicom = "<entry group=\"%s\" element=\"%s\" vr=\"%s\" vm=\"%s\" name=\"%s%s\"/>"%(m.group(2),m.group(3),m.group(4),m.group(6),m.group(1).lstrip(),m.group(5).rstrip())
+        dicom = "<entry group=\"%s\" element=\"%s\" vr=\"%s\" vm=\"%s\" name=\"%s%s\"/>\n"%(m.group(2),m.group(3),m.group(4),m.group(6),m.group(1).lstrip(),m.group(5).rstrip())
         #print dicom
         outLines.append( dicom )
       else:
@@ -271,7 +271,7 @@ class TextParser8:
     infile = file(inputfilename, 'r')
     outLines = []
     for line in infile.readlines():
-      patt = re.compile("^\s*([A-Za-z0-9> -]+)\s+\(([0-9]+),([0-9A-Fx]+)\)\s+([A-Z][A-Z])\s+([1-9n-]+)\s+([A-Za-z0-9. ]+)\s*$")
+      patt = re.compile("^\s*([A-Za-z0-9()> -]+)\s+\(([0-9]+),([0-9A-Fx]+)\)\s+([A-Z][A-Z])\s+([1-9n-]+)\s+([A-Za-z_0-9. ]+)\s*$")
       m = patt.match(line)
       if m:
         # <entry group="0001" element="0001" vr="LO" vm="1" owner="Private Creator"/>
