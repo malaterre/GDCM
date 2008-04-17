76776fa7052977fab8a05bcf931512c72670244a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-04-17 20:43:45 +0000

    ENH: adding placeholder

---
+++ Source/MediaStorageAndFileFormat/gdcmSegmentedPaletteColorLookupTable.h
@@ -0,0 +1,34 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __gdcmSegmentedPaletteColorLookupTable_h
+#define __gdcmSegmentedPaletteColorLookupTable_h
+
+#include "gdcmLookupTable.h"
+
+namespace gdcm
+{
+  
+class GDCM_EXPORT SegmentedPaletteColorLookupTable : public LookupTable
+{
+public:
+  SegmentedPaletteColorLookupTable();
+  ~SegmentedPaletteColorLookupTable();
+  void Print(std::ostream &) const {}
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmSegmentedPaletteColorLookupTable_h
