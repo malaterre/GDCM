67ef56af2ee85d0a340b34d10043a6cca1588eae
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-01 20:14:06 +0000

    ENH: test for Spacing / IPPSorter

--- Utilities/VTK/Testing/Cxx/TestvtkGDCMImageReader2.cxx
+++ Utilities/VTK/Testing/Cxx/TestvtkGDCMImageReader2.cxx
@@ -14,6 +14,8 @@
 =========================================================================*/
 #include "vtkGDCMImageReader.h"
 #include "vtkStringArray.h"
+#include "vtkImageData.h"
+#include "vtkImageChangeInformation.h"
 
 #include "gdcmFilename.h"
 #include "gdcmTesting.h"
@@ -22,6 +24,10 @@
 #include "gdcmDirectory.h"
 #include "gdcmIPPSorter.h"
 
+/*
+ * Test to show the pipeline for 
+ * IPPSorter -> vtkGDCMImageReader -> vtkImageChangeInformation
+ */
 int TestvtkGDCMImageReader2(int argc, char *argv[])
 {
   const char *directory = gdcm::Testing::GetDataRoot();
@@ -48,6 +54,12 @@ int TestvtkGDCMImageReader2(int argc, char *argv[])
 
   std::cout << "Found z-spacing:" << std::endl;
   std::cout << s.GetZSpacing() << std::endl;
+  double ippzspacing = s.GetZSpacing();
+  if( ippzspacing != 5.5 )
+  {
+	  // This should be test in another specific test ...
+	  return 1;
+  }
 
   const std::vector<std::string> & sorted = s.GetFilenames();
   vtkGDCMImageReader * reader = vtkGDCMImageReader::New();
@@ -58,9 +70,42 @@ int TestvtkGDCMImageReader2(int argc, char *argv[])
 	  const std::string &f = *it;
   files->InsertNextValue( f.c_str() );
   }
+  reader->SetFileNames( files );
+  reader->Update();
 
+  const double *spacing = reader->GetOutput()->GetSpacing();
+  std::cout << spacing[0] << "," << spacing[1] << "," << spacing[2] << std::endl;
+  int ret = 0;
+  if( spacing[2] != 0.5 )
+  {
+	  // Spacing Between Slice is set to 0.5 in those files
+	  ret++;
+  }
 
+  // try again but this time we want 5.5 to be the spacing
+  vtkGDCMImageReader * reader2 = vtkGDCMImageReader::New();
+  reader2->SetDataSpacing( spacing[0], spacing[1], ippzspacing );
+  reader2->SetFileNames( files );
+  reader2->Update();
+  const double *spacing2 = reader2->GetOutput()->GetSpacing();
+  std::cout << spacing2[0] << "," << spacing2[1] << "," << spacing2[2] << std::endl;
+  // You need to use this class to preserve spacing
+  // across pipeline re-execution
+  vtkImageChangeInformation *change = vtkImageChangeInformation::New();
+  change->SetInput( reader2->GetOutput() );
+  change->SetOutputSpacing( spacing2[0], spacing2[1], ippzspacing );
+  change->Update();
+
+  const double *spacing3 = change->GetOutput()->GetSpacing();
+  std::cout << spacing3[0] << "," << spacing3[1] << "," << spacing3[2] << std::endl;
+  if( spacing3[2] != 5.5 )
+  {
+	  ret++;
+  }
   
+  change->Delete();
   reader->Delete();
-  return 0;
+  files->Delete();
+
+  return ret;
 }
