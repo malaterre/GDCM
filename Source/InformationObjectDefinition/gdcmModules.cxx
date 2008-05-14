255107430b2bd95a071938d69574705de7cba8d2
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-14 14:27:26 +0000

    ENH: todo

--- Source/InformationObjectDefinition/gdcmModules.cxx
+++ Source/InformationObjectDefinition/gdcmModules.cxx
@@ -12,7 +12,7 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
-#include "gdcmModule.h"
+#include "gdcmModules.h"
 
 namespace gdcm
 {
