aaed3de8ecb7d24cccf3242faa06570dc847b4a3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-18 13:38:18 +0000

    ENH: cleanup

--- Examples/Cxx/delta_encode.cxx
+++ Examples/Cxx/delta_encode.cxx
@@ -14,10 +14,9 @@
 =========================================================================*/
 /*
  */
-#include "gdcmReader.h"
-#include "gdcmPrivateTag.h"
-#include "gdcmImageWriter.h"
 
+#include <vector>
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 
@@ -81,8 +80,11 @@ void delta_encode(unsigned short *inbuffer, size_t length)
 	    output[i-2+2] = prev0;
 	    output.erase( output.begin() + i, output.begin() + i - 2 + j);
     }
-    else if( output[i] == prev1 && prev1 == 0x0E /*&& false*/ ) 
+    else if( output[i] == prev1 && prev1 == 0x0E ) 
     {
+	    /*
+	     * Why would you want to replace 0E 0E with 05 01 0E ???
+	     */
 	    std::cerr << "HACK: " << i << std::endl;
 	    if( i == 14426 )
 	    {
@@ -95,6 +97,11 @@ void delta_encode(unsigned short *inbuffer, size_t length)
     prev1 = output[i];
   }
 
+  // DICOM need % 2
+  if( output.size() % 2 )
+  {
+	  output.push_back( 0x0 );
+  }
   std::cout << output.size() << std::endl;
   std::ofstream out("comp.rle");
   out.write( &output[0], output.size() );
@@ -105,16 +112,6 @@ int main(int argc, char *argv [])
 {
   if( argc < 2 ) return 1;
   const char *filename = argv[1];
-	/*
-  gdcm::Reader reader;
-  reader.SetFileName( filename );
-  if( !reader.Read() )
-    {
-    std::cerr << "Failed to read: " << filename << std::endl;
-    return 1;
-    }
-  const gdcm::DataSet& ds = reader.GetFile().GetDataSet();
-*/
 
   std::ifstream i( filename );
   const unsigned int size = 131072;
@@ -123,66 +120,6 @@ int main(int argc, char *argv [])
   i.read( ref, size);
   i.close();
   delta_encode((unsigned short*)ref, size / 2);
-
-
-  /*
-  // (07a1,1011) CS [PMSCT_RLE1]                                       # 10,1 Tamar Compression Type
-  const gdcm::PrivateTag tcompressiontype(0x07a1,0x0011,"ELSCINT1");
-  if( !ds.FindDataElement( tcompressiontype ) ) return 1;
-  const gdcm::DataElement& compressiontype = ds.GetDataElement( tcompressiontype );
-  if ( compressiontype.IsEmpty() ) return 1;
-  const gdcm::ByteValue * bv = compressiontype.GetByteValue();
-  std::string comp = "PMSCT_RLE1";
-  if( strncmp( bv->GetPointer(), comp.c_str(), comp.size() ) != 0 ) return 1;
-  
-  const gdcm::PrivateTag tcompressedpixeldata(0x07a1,0x000a,"ELSCINT1");
-  if( !ds.FindDataElement( tcompressedpixeldata) ) return 1;
-  const gdcm::DataElement& compressionpixeldata = ds.GetDataElement( tcompressedpixeldata);
-  if ( compressionpixeldata.IsEmpty() ) return 1;
-  const gdcm::ByteValue * bv2 = compressionpixeldata.GetByteValue();
-
-  char *ref = new char [131072];
-  memset(ref,0,131072);
-  delta_decode((unsigned short *)ref, bv2->GetPointer(), bv2->GetLength(), 131072);
-
-  gdcm::DataElement pixeldata( gdcm::Tag(0x7fe0,0x0010) );
-  pixeldata.SetVR( gdcm::VR::OB );
-  pixeldata.SetByteValue( ref, 131072);
-  delete[] ref;
-
-  gdcm::ImageWriter writer;
-  //writer.SetFile( reader.GetFile() );
-
-  gdcm::ImageValue &image = dynamic_cast<gdcm::ImageValue&>(writer.GetImage());
-  image.SetNumberOfDimensions( 2 ); // good default
-  image.SetDimension(0, 256 );
-  image.SetDimension(1, 256 );
-  //image.SetSpacing(0, spacing[0] );
-  //image.SetSpacing(1, spacing[1] );
-  gdcm::PixelFormat pixeltype = gdcm::PixelFormat::INT16; // bytepix = spm_type('int16','bits')/8;
-
-  image.SetNumberOfDimensions( 2 );
-  //image.SetDimension(2, p / pixeltype.GetPixelSize() );
-  //image.SetDimension(2, numberOfImagesInMosaic );
-
-  gdcm::PhotometricInterpretation pi;
-  pi = gdcm::PhotometricInterpretation::MONOCHROME2;
-  //pixeltype.SetSamplesPerPixel(  );
-  image.SetPhotometricInterpretation( pi );
-  image.SetPixelFormat( pixeltype );
-
-  image.SetDataElement( pixeldata );
-
-  std::string outfilename = "outrle.dcm";
-  writer.SetFileName( outfilename.c_str() );
-  if( !writer.Write() )
-    {
-    std::cerr << "Failed to write" << std::endl;
-    return 1;
-    }
-
-    std::cout << "sucess !" << std::endl;
-    */
 
   return 0;
 }
