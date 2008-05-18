d90f5a0be0ff875a8114c7403a0ca8387854099a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-18 10:05:57 +0000

    ENH: add delta decoder

--- Examples/Cxx/delta_encode.cxx
+++ Examples/Cxx/delta_encode.cxx
@@ -13,66 +13,20 @@
 
 =========================================================================*/
 /*
- * Special thanks to:
- * Mauro Maiorca for bringing to our attention on this new ELSCINT1 
- * compression algorithm : PMSCT_RLE1 (different from the 'LOSSLESS RICE')
- * See post at:
- * http://groups.google.com/group/comp.protocols.dicom/msg/f2b99bf706a7f8ca
- * 
- * Thanks to Jesús Spínola, for more datasets,
- * http://www.itk.org/pipermail/insight-users/2008-April/025571.html
- *
- * And last but not least, a very big thank to Ivo van Poorten, without
- * whom we would still be looking at this compressed byte stream as if
- * it was RLE compressed.
  */
 #include "gdcmReader.h"
 #include "gdcmPrivateTag.h"
 #include "gdcmImageWriter.h"
 
-void delta_decode(unsigned short *outbuffer, const char *inbuffer, size_t length, size_t outlength)
+void delta_encode(unsigned short *outbuffer, const char *inbuffer, size_t length, size_t outlength)
 {
-  unsigned short t = 0;
-  size_t i;
-  size_t j;
-  for (i=0,j=0; i < length; ++i,++j)
-    {
-    if( inbuffer[i] == (0xa5-256) )
-      {
-      int repeat = inbuffer[i+1] + 1;
-      int value = inbuffer[i+2];
-      i+=2;
-      for( int r = 0; r < repeat; ++r, ++j )
-        {
-        outbuffer[j] = value + t;
-        t = outbuffer[j];
-        }
-      --j;
-      }
-    else if( inbuffer[i] == 0x5a )
-      {
-      unsigned char v1 = (unsigned char)inbuffer[i+1];
-      unsigned char v2 = (unsigned char)inbuffer[i+2];
-      outbuffer[j] = v2 * 256 + v1;
-      t = outbuffer[j];
-      i+=2;
-      }
-    else
-      {
-	      if( j < outlength / 2 ) // FIXME
-	      {
-      //assert( ((int)inbuffer[i] + (int)t) >= 0 );
-      outbuffer[j] = (int)inbuffer[i] + (int)t;
-      t = outbuffer[j];
-	      }
-      }
-    }
 }
 
 int main(int argc, char *argv [])
 {
   if( argc < 2 ) return 1;
   const char *filename = argv[1];
+	/*
   gdcm::Reader reader;
   reader.SetFileName( filename );
   if( !reader.Read() )
@@ -81,7 +35,18 @@ int main(int argc, char *argv [])
     return 1;
     }
   const gdcm::DataSet& ds = reader.GetFile().GetDataSet();
+*/
 
+  std::ifstream i( filename );
+  const unsigned int size = 131072;
+  char *ref = new char [size];
+  memset(ref,0,size);
+  i.read( ref, size);
+  i.close();
+  delta_encode(ref, size);
+
+
+  /*
   // (07a1,1011) CS [PMSCT_RLE1]                                       # 10,1 Tamar Compression Type
   const gdcm::PrivateTag tcompressiontype(0x07a1,0x0011,"ELSCINT1");
   if( !ds.FindDataElement( tcompressiontype ) ) return 1;
@@ -138,6 +103,7 @@ int main(int argc, char *argv [])
     }
 
     std::cout << "sucess !" << std::endl;
+    */
 
   return 0;
 }
