deacd08a73427745aeb732da21aab2752e1c700e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-19 21:16:18 +0000

    ENH: More packaging stuff: need to reuse zlib from system.

---
+++ Utilities/gdcm_zlib.h
@@ -0,0 +1,26 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef __gdcm_zlib_h
+#define __gdcm_zlib_h
+
+/* Use the zlib library configured for gdcm.  */
+#include "gdcmTypes.h"
+#ifdef GDCM_USE_SYSTEM_ZLIB
+# include <zlib/zlib.h>
+#else
+# include <gdcmzlib/zlib.h>
+#endif
+
+#endif
