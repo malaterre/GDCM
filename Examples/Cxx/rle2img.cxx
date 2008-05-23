4807a2829c8d16133753465081728be82bdfde02
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-23 13:31:11 +0000

    ENH: reactivate for next release

---
+++ Examples/Cxx/rle2img.cxx
@@ -0,0 +1,166 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/*
+ * This example shows how to rewrite a ELSCINT1/PMSCT_RLE1 compressed
+ * image so that it is readable by most 3rd party software (DICOM does
+ * not specify this particular encoding).
+ * This is required for the sake of interoperability with any standard
+ * conformring DICOM system.
+ *
+ * Special thanks to:
+ * Mauro Maiorca for bringing to our attention on this new ELSCINT1 
+ * compression algorithm : PMSCT_RLE1 (different from the 'LOSSLESS RICE')
+ * See post at:
+ * http://groups.google.com/group/comp.protocols.dicom/msg/f2b99bf706a7f8ca
+ * 
+ * Thanks to Jesús Spínola, for more datasets,
+ * http://www.itk.org/pipermail/insight-users/2008-April/025571.html
+ *
+ * And last but not least, a very big thank to Ivo van Poorten, without
+ * whom we would still be looking at this compressed byte stream as if
+ * it was RLE compressed.
+ */
+#include "gdcmReader.h"
+#include "gdcmPrivateTag.h"
+#include "gdcmImageWriter.h"
+
+void delta_decode(const char *inbuffer, size_t length, std::vector<unsigned short> &output)
+{
+  // RLE pass
+  std::vector<char> temp;
+  for(size_t i = 0; i < length; ++i)
+    {
+    if( inbuffer[i] == 0xa5 - 256 )
+      {
+      unsigned char repeat = (unsigned char)inbuffer[i+1] + 1;
+      char value = inbuffer[i+2];
+      while(repeat)
+        {
+        temp.push_back( value );
+        --repeat;
+        }
+      i+=2;
+      }
+    else
+      {
+      temp.push_back( inbuffer[i] );
+      }
+    }
+
+  // Delta encoding pass
+  unsigned short delta = 0;
+  for(size_t i = 0; i < temp.size(); ++i)
+    {
+    if( temp[i] == 0x5a )
+      {
+      unsigned char v1 = (unsigned char)temp[i+1];
+      unsigned char v2 = (unsigned char)temp[i+2];
+      int value = v2 * 256 + v1;
+      output.push_back( value );
+      delta = value;
+      i+=2;
+      }
+    else
+      {
+      int value = temp[i] + delta;
+      output.push_back( value );
+      delta = value;
+      }
+    //assert( output[output.size()-1] == ref[output.size()-1] );
+    }
+
+  if ( output.size() % 2 )
+    {
+    output.resize( output.size() - 1 );
+    }
+  std::cout << length << " -> " << output.size() * 2 << std::endl;
+}
+
+int main(int argc, char *argv [])
+{
+  if( argc < 2 ) return 1;
+  const char *filename = argv[1];
+  gdcm::Reader reader;
+  reader.SetFileName( filename );
+  if( !reader.Read() )
+    {
+    std::cerr << "Failed to read: " << filename << std::endl;
+    return 1;
+    }
+  const gdcm::DataSet& ds = reader.GetFile().GetDataSet();
+
+  // (07a1,1011) CS [PMSCT_RLE1]                                       # 10,1 Tamar Compression Type
+  const gdcm::PrivateTag tcompressiontype(0x07a1,0x0011,"ELSCINT1");
+  if( !ds.FindDataElement( tcompressiontype ) ) return 1;
+  const gdcm::DataElement& compressiontype = ds.GetDataElement( tcompressiontype );
+  if ( compressiontype.IsEmpty() ) return 1;
+  const gdcm::ByteValue * bv = compressiontype.GetByteValue();
+  std::string comp = "PMSCT_RLE1";
+  if( strncmp( bv->GetPointer(), comp.c_str(), comp.size() ) != 0 ) return 1;
+  
+  const gdcm::PrivateTag tcompressedpixeldata(0x07a1,0x000a,"ELSCINT1");
+  if( !ds.FindDataElement( tcompressedpixeldata) ) return 1;
+  const gdcm::DataElement& compressionpixeldata = ds.GetDataElement( tcompressedpixeldata);
+  if ( compressionpixeldata.IsEmpty() ) return 1;
+  const gdcm::ByteValue * bv2 = compressionpixeldata.GetByteValue();
+
+  std::vector<unsigned short> buffer;
+  delta_decode(bv2->GetPointer(), bv2->GetLength(), buffer);
+
+  gdcm::DataElement pixeldata( gdcm::Tag(0x7fe0,0x0010) );
+  pixeldata.SetVR( gdcm::VR::OB );
+  pixeldata.SetByteValue( (char*)&buffer[0], buffer.size() * sizeof( unsigned short ) );
+
+
+  gdcm::ImageWriter writer;
+  writer.SetFile( reader.GetFile() );
+
+  // Cleanup stuff:
+  // FIXME does not work...
+  //writer.GetFile().GetDataSet().Remove( tcompressedpixeldata );
+
+  gdcm::ImageValue &image = dynamic_cast<gdcm::ImageValue&>(writer.GetImage());
+  image.SetNumberOfDimensions( 2 ); // good default
+  image.SetDimension(0, 256 );
+  image.SetDimension(1, 256 );
+  //image.SetSpacing(0, spacing[0] );
+  //image.SetSpacing(1, spacing[1] );
+  gdcm::PixelFormat pixeltype = gdcm::PixelFormat::INT16;
+
+  image.SetNumberOfDimensions( 2 );
+  //image.SetDimension(2, p / pixeltype.GetPixelSize() );
+  //image.SetDimension(2, numberOfImagesInMosaic );
+
+  gdcm::PhotometricInterpretation pi;
+  pi = gdcm::PhotometricInterpretation::MONOCHROME2;
+  //pixeltype.SetSamplesPerPixel(  );
+  image.SetPhotometricInterpretation( pi );
+  image.SetPixelFormat( pixeltype );
+
+  image.SetDataElement( pixeldata );
+
+  std::string outfilename = "outrle.dcm";
+  writer.SetFileName( outfilename.c_str() );
+  if( !writer.Write() )
+    {
+    std::cerr << "Failed to write" << std::endl;
+    return 1;
+    }
+
+    std::cout << "sucess !" << std::endl;
+
+  return 0;
+}
+
