09f770c617c90af4cfddea6058b73ae93f7f8791
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-27 15:04:58 +0000

    BUG: fix logic

--- Source/DataStructureAndEncodingDefinition/gdcmByteValue.h
+++ Source/DataStructureAndEncodingDefinition/gdcmByteValue.h
@@ -54,27 +54,7 @@ public:
 
   // When 'dumping' dicom file we still have some information from
   // Either the VR: eg LO (private tag)
-  void PrintASCII(std::ostream &os, VL maxlength ) const {
-    VL length = std::min(maxlength, Length);
-    // Special case for VR::UI, do not print the trailing \0
-    if( length && length == Length )
-      {
-      if( *Internal.rbegin() == 0 )
-        {
-        length = length - 1;
-        }
-      }
-    // I cannot check IsPrintable some file contains \2 or \0 in a VR::LO element
-    // See: acr_image_with_non_printable_in_0051_1010.acr 
-    //assert( IsPrintable(length) );
-    std::vector<char>::const_iterator it = Internal.begin();
-    for(; it != Internal.begin()+length; ++it)
-      {
-      const char &c = *it;
-      if ( !( isprint((unsigned char)c) || isspace((unsigned char)c) ) ) os << ".";
-      else os << c;
-      }
-  }
+  void PrintASCII(std::ostream &os, VL maxlength ) const;
 
   void PrintHex(std::ostream &os, VL maxlength) const;
 
