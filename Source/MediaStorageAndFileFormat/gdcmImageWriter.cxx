a75c451f94edac0cfe3de8c270832160e1b0eb68
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-05-30 14:14:20 +0000

    ENH: local change

--- Source/MediaStorageAndFileFormat/gdcmImageWriter.cxx
+++ Source/MediaStorageAndFileFormat/gdcmImageWriter.cxx
@@ -280,17 +280,6 @@ bool ImageWriter::Write()
       }
     }
 
-  // Do the Rescale Intercept & Slope
-  if( PixelData.GetIntercept() != 0 || PixelData.GetSlope() != 1. )
-    {
-    Attribute<0x0028,0x1052> at1;
-    at1.SetValue( PixelData.GetIntercept() );
-    ds.Insert( at1.GetAsDataElement() );
-    Attribute<0x0028,0x1053> at2;
-    at2.SetValue( PixelData.GetSlope() );
-    ds.Insert( at2.GetAsDataElement() );
-    }
-
   MediaStorage ms;
   ms.SetFromFile( GetFile() );
   assert( ms != MediaStorage::MS_END );
