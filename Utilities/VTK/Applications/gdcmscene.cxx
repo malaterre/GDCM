c9fcc9909561d19836a883db4bab48e3e1a6d1f4
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-06-13 17:04:45 +0000

    ENH: render

--- Utilities/VTK/Applications/gdcmscene.cxx
+++ Utilities/VTK/Applications/gdcmscene.cxx
@@ -16,12 +16,22 @@
 
 #include "vtkAppendPolyData.h"
 #include "vtkPolyDataWriter.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderer.h"
+#include "vtkCamera.h"
+#include "vtkProperty.h"
+
+
+// gdcmDataExtra/gdcmNonImageData/exRT_Structure_Set_Storage.dcm
+// gdcmDataExtra/gdcmNonImageData/RT/RTStruct.dcm
 
 int main(int argc, char *argv[])
 {
   if( argc < 2 )
     {
-    std::cerr << argv[0] << " filename1.dcm [filename2.dcm ...]\n";
+    std::cerr << argv[0] << " filename1.dcm\n";
     return 1;
     }
   const char * filename = argv[1];
@@ -37,9 +47,51 @@ int main(int argc, char *argv[])
   vtkPolyDataWriter * writer = vtkPolyDataWriter::New();
   writer->SetInput( reader->GetOutput() );
   writer->SetFileName( "rtstruct.vtk" );
-  writer->Write();
+  //writer->Write();
+
+  // Now we'll look at it.
+  vtkPolyDataMapper *cubeMapper = vtkPolyDataMapper::New();
+      cubeMapper->SetInput( reader->GetOutput() );
+      cubeMapper->SetScalarRange(0,7);
+  vtkActor *cubeActor = vtkActor::New();
+      cubeActor->SetMapper(cubeMapper);
+  vtkProperty * property = cubeActor->GetProperty();
+  property->SetRepresentationToWireframe();
+
+
+  // The usual rendering stuff.
+//  vtkCamera *camera = vtkCamera::New();
+//      camera->SetPosition(1,1,1);
+//      camera->SetFocalPoint(0,0,0);
+
+  vtkRenderer *renderer = vtkRenderer::New();
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+    renWin->AddRenderer(renderer);
+
+  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+    iren->SetRenderWindow(renWin);
+
+  renderer->AddActor(cubeActor);
+      //renderer->SetActiveCamera(camera);
+      renderer->ResetCamera();
+      renderer->SetBackground(1,1,1);
+  
+  renWin->SetSize(300,300);
+
+  // interact with data
+  renWin->Render();
+  iren->Start();
+
+
   
   reader->Delete();
+  cubeMapper->Delete();
+  cubeActor->Delete();
+//  camera->Delete();
+  renderer->Delete();
+  renWin->Delete();
+  iren->Delete();
+
   writer->Delete();
 
   return 0;
