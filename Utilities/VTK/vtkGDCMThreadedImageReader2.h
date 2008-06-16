f4b5108d87eee5141aa23c99870e26fc3c2536c6
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-06-16 11:31:18 +0000

    ENH: add new implementation to support thread in a cross platform way

---
+++ Utilities/VTK/vtkGDCMThreadedImageReader2.h
@@ -0,0 +1,84 @@
+
+#ifndef __vtkGDCMThreadedImageReader2_h
+#define __vtkGDCMThreadedImageReader2_h
+
+//#include "vtkGDCMImageReader.h"
+#include "vtkThreadedImageAlgorithm.h"
+
+class vtkStringArray;
+class VTK_EXPORT vtkGDCMThreadedImageReader2 : public vtkThreadedImageAlgorithm
+{
+public:
+  static vtkGDCMThreadedImageReader2 *New();
+  vtkTypeRevisionMacro(vtkGDCMThreadedImageReader2,vtkThreadedImageAlgorithm);
+  virtual void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkGetMacro(FileLowerLeft,int);
+  vtkSetMacro(FileLowerLeft,int);
+  vtkBooleanMacro(FileLowerLeft,int);
+
+  vtkGetMacro(NumberOfOverlays,int);
+
+  vtkSetMacro(DataScalarType,int);
+  vtkGetMacro(DataScalarType,int);
+
+  vtkSetMacro(NumberOfScalarComponents,int);
+  vtkGetMacro(NumberOfScalarComponents,int);
+
+  vtkGetMacro(LoadOverlays,int);
+  vtkSetMacro(LoadOverlays,int);
+  vtkBooleanMacro(LoadOverlays,int);
+
+  vtkSetVector6Macro(DataExtent,int);
+  vtkGetVector6Macro(DataExtent,int);
+
+  vtkGetStringMacro(FileName);
+  vtkSetStringMacro(FileName);
+
+  virtual void SetFileNames(vtkStringArray*);
+  vtkGetObjectMacro(FileNames, vtkStringArray);
+
+  int SplitExtent(int splitExt[6], int startExt[6], 
+                  int num, int total);
+
+protected:
+  vtkGDCMThreadedImageReader2();
+  ~vtkGDCMThreadedImageReader2();
+
+  int RequestInformation(vtkInformation *request,
+                         vtkInformationVector **inputVector,
+                         vtkInformationVector *outputVector);
+  //int RequestData(vtkInformation *request,
+  //                vtkInformationVector **inputVector,
+  //                vtkInformationVector *outputVector);
+
+protected:
+void ThreadedRequestData (
+  vtkInformation * vtkNotUsed( request ), 
+  vtkInformationVector** vtkNotUsed( inputVector ),
+  vtkInformationVector * vtkNotUsed( outputVector ),
+  vtkImageData ***inData, 
+  vtkImageData **outData,
+  int outExt[6], int id);
+
+private:
+  int FileLowerLeft;
+  char *FileName;
+  vtkStringArray *FileNames;
+  int LoadIconImage;
+  int DataExtent[6];
+  int LoadOverlays;
+    int NumberOfOverlays;
+  int DataScalarType;
+
+  int NumberOfScalarComponents;
+  double DataSpacing[3];
+  double DataOrigin[3];
+  int IconImageDataExtent[6];
+
+private:
+  vtkGDCMThreadedImageReader2(const vtkGDCMThreadedImageReader2&);  // Not implemented.
+  void operator=(const vtkGDCMThreadedImageReader2&);  // Not implemented.
+};
+
+#endif
