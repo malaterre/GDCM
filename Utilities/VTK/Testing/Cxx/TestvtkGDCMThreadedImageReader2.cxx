dbda96b249d01b17ed701f4a62676b890d14e4be
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-07-10 09:15:55 +0000

    ENH: copy/paste for threaded 2

---
+++ Utilities/VTK/Testing/Cxx/TestvtkGDCMThreadedImageReader2.cxx
@@ -0,0 +1,320 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkGDCMThreadedImageReader2.h"
+#include "vtkGDCMImageReader.h"
+#include "vtkCommand.h"
+
+#include "gdcmDirectory.h"
+#include "gdcmSystem.h"
+#include "gdcmImageReader.h"
+#include "gdcmTesting.h"
+
+#include "vtkPNGWriter.h"
+#include "vtkStringArray.h"
+#include "vtkStructuredPointsWriter.h"
+#include "vtkImageData.h"
+#if (VTK_MAJOR_VERSION >= 5) || ( VTK_MAJOR_VERSION == 4 && VTK_MINOR_VERSION > 5 )
+#include <vtksys/SystemTools.hxx>
+#include "vtkVolumeTextureMapper3D.h"
+#endif
+
+#include "vtkPiecewiseFunction.h"
+#include "vtkColorTransferFunction.h"
+#include "vtkVolumeProperty.h"
+#include "vtkVolume.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+
+
+class ProgressObserver : public vtkCommand
+{
+public:
+  static ProgressObserver* New() {
+    return new ProgressObserver;
+  }
+
+  virtual void Execute(vtkObject* caller, unsigned long event, void *callData)
+    {
+    (void)callData;
+    if( event == vtkCommand::ProgressEvent )
+      {
+      std::cout << ((vtkGDCMThreadedImageReader2*)caller)->GetProgress() << std::endl;
+      }
+    }
+};
+
+
+template <typename TReader>
+int ExecuteInformation(const char *filename, TReader *vtkreader)
+{
+  gdcm::ImageReader reader;
+  reader.SetFileName( filename );
+  if( !reader.Read() )
+    {
+    return 0;
+    }
+  const gdcm::Image &image = reader.GetImage();
+  const unsigned int *dims = image.GetDimensions();
+
+  // Set the Extents.
+  assert( image.GetNumberOfDimensions() >= 2 );
+  int dataextent[6] = {};
+  dataextent[0] = 0;
+  dataextent[1] = dims[0] - 1;
+  dataextent[2] = 0;
+  dataextent[3] = dims[1] - 1;
+  if( image.GetNumberOfDimensions() == 2 )
+    {
+    vtkStringArray *filenames = vtkreader->GetFileNames();
+    if ( filenames && filenames->GetNumberOfValues() > 1 )
+      {
+      dataextent[4] = 0;
+      dataextent[5] = filenames->GetNumberOfValues() - 1;
+      }
+    else
+      {
+      dataextent[4] = 0;
+      dataextent[5] = 0;
+      }
+    }
+  else
+    {
+    assert( image.GetNumberOfDimensions() == 3 );
+    //this->FileDimensionality = 3;
+    dataextent[4] = 0;
+    dataextent[5] = dims[2] - 1;
+    }
+  //this->DataSpacing[0] = 1.;
+  //this->DataSpacing[1] = -1.;
+  //this->DataSpacing[2] = 1.;
+
+  gdcm::PixelFormat pixeltype = image.GetPixelFormat();
+  int datascalartype = VTK_VOID;
+  switch( pixeltype )
+    {
+  case gdcm::PixelFormat::INT8:
+    datascalartype = VTK_CHAR;
+    break;
+  case gdcm::PixelFormat::UINT8:
+    datascalartype = VTK_UNSIGNED_CHAR;
+    break;
+  case gdcm::PixelFormat::INT16:
+    datascalartype = VTK_SHORT;
+    break;
+  case gdcm::PixelFormat::UINT16:
+    datascalartype = VTK_UNSIGNED_SHORT;
+    break;
+  case gdcm::PixelFormat::INT32:
+    datascalartype = VTK_INT;
+    break;
+  case gdcm::PixelFormat::UINT32:
+    datascalartype = VTK_UNSIGNED_INT;
+    break;
+  default:
+    ;
+    }
+  if( datascalartype == VTK_VOID )
+    {
+    return 0;
+    }
+
+  unsigned int numberOfScalarComponents = pixeltype.GetSamplesPerPixel();
+
+  vtkreader->SetDataExtent( dataextent );
+  vtkreader->SetDataScalarType ( datascalartype );
+  //vtkreader->SetShift( image.GetIntercept() );
+  //vtkreader->SetScale( image.GetSlope() );
+  vtkreader->SetNumberOfScalarComponents( numberOfScalarComponents );
+  vtkreader->LoadOverlaysOff();
+  if( image.GetNumberOfOverlays() )
+    {
+    vtkreader->LoadOverlaysOn();
+    }
+
+  return 1;
+}
+
+template <typename TReader>
+int TestvtkGDCMThreadedImageRead2(const char *filename, bool verbose = false)
+{
+  TReader *reader = TReader::New();
+  reader->FileLowerLeftOn();
+  //reader->CanReadFile( filename );
+  if( verbose) std::cerr << "Reading : " << filename << std::endl;
+
+  const char *refimage = NULL;
+  if( gdcm::System::FileIsDirectory( filename ) )
+    {
+    gdcm::Directory d;
+    d.Load( filename );
+    gdcm::Directory::FilenamesType l = d.GetFilenames();
+    const unsigned int nfiles = l.size();
+    vtkStringArray *sarray = vtkStringArray::New();
+    for(unsigned int i = 0; i < nfiles; ++i)
+      {
+      sarray->InsertNextValue( l[i] );
+      }
+    assert( sarray->GetNumberOfValues() == (int)nfiles );
+    reader->SetFileNames( sarray );
+    sarray->Delete();
+    refimage = sarray->GetValue( 0 ); // Ok since sarray is ref count
+    }
+  else
+    {
+    reader->SetFileName( filename );
+    refimage = filename;
+    }
+
+  // In all cases we need to explicitely say what the image type is:
+  if( !ExecuteInformation<TReader>(refimage, reader) )
+    {
+    std::cerr << "file: " << refimage << " is not an image. giving up" << std::endl;
+    reader->Delete();
+    return 0;
+    }
+
+
+  ProgressObserver *obs = ProgressObserver::New();
+  if( verbose )
+    {
+    reader->AddObserver( vtkCommand::ProgressEvent, obs);
+    }
+
+  reader->Update();
+  obs->Delete();
+
+  //reader->GetOutput()->Print( cout );
+  //reader->GetOutput(1)->Print( cout );
+
+  if( reader->GetNumberOfOverlays() )
+    {
+    vtkPNGWriter *writer = vtkPNGWriter::New();
+    writer->SetInput( reader->GetOutput(1) );
+    const char subdir[] = "TestvtkGDCMThreadedImageReader2";
+    // Create directory first:
+    std::string tmpdir = gdcm::Testing::GetTempDirectory( subdir );
+    if( !gdcm::System::FileIsDirectory( tmpdir.c_str() ) )
+      {
+      gdcm::System::MakeDirectory( tmpdir.c_str() );
+      //return 1;
+      }
+    std::string pngfile = gdcm::Testing::GetTempFilename( filename, subdir );
+    //pngfile += vtksys::SystemTools::GetFilenameWithoutExtension( filename );
+    pngfile += ".png";
+    writer->SetFileName( pngfile.c_str() );
+    if( verbose ) std::cerr << pngfile << std::endl;
+    //writer->Write();
+    writer->Delete();
+    }
+
+/*
+  vtkStructuredPointsWriter *writer = vtkStructuredPointsWriter::New();
+  writer->SetInput( reader->GetOutput() );
+  writer->SetFileName( "TestvtkGDCMThreadedImageReader2.vtk" );
+  writer->SetFileTypeToBinary();
+  //writer->Write();
+  writer->Delete();
+*/
+
+  bool compute = false;
+  if( verbose && compute )
+    {
+#if (VTK_MAJOR_VERSION >= 5) || ( VTK_MAJOR_VERSION == 4 && VTK_MINOR_VERSION > 2 )
+    double *s = reader->GetOutput()->GetScalarRange();
+#else
+    float *s = reader->GetOutput()->GetScalarRange();
+#endif
+    std::cout << s[0] << " " << s[1] << std::endl;
+    }
+
+/*
+  // Create transfer functions for opacity and color
+  vtkPiecewiseFunction *opacityTransferFunction = vtkPiecewiseFunction::New();
+  opacityTransferFunction->AddPoint(  600 , 0.0);
+  opacityTransferFunction->AddPoint( 2000 , 1.0);
+
+  vtkColorTransferFunction *colorTransferFunction = vtkColorTransferFunction::New();
+  //colorTransferFunction ->ClampingOff();
+  //colorTransferFunction ->AddHSVPoint(      0.0, 0.01, 1.0, 1.0);
+  //colorTransferFunction ->AddHSVPoint(   1000.0, 0.50, 1.0, 1.0);
+  //colorTransferFunction ->AddHSVPoint(   2000.0, 0.99, 1.0, 1.0);
+  //colorTransferFunction ->SetColorSpaceToHSV();
+  colorTransferFunction ->AddRGBPoint  (    s[0], 0.0, 0.0, 0.0);
+  colorTransferFunction ->AddRGBPoint  (    (s[0] + s[1]) / 8., 0.25, 0.25, 0.25);
+  colorTransferFunction ->AddRGBPoint  (    (s[0] + s[1]) / 4., 0.5, 0.25, 0.25);
+  colorTransferFunction ->AddRGBPoint  (    s[1], 1.0, 1.0, 1.0);
+  //colorTransferFunction ->AddRGBPoint  ( 3.* (s[0] + s[1]) / 4 , 1.0, 0.0, 0.0);
+  //colorTransferFunction ->AddRGBPoint  (  (s[0] + s[1]) / 2, 0.0, 0.0, 1.0);
+  //colorTransferFunction ->AddRGBPoint  ( 1.*(s[0] + s[1]) / 4 , 0.0, 1.0, 0.0);
+  //colorTransferFunction ->AddRGBPoint  (  s[0], 0.0, 0.2, 0.0);
+
+  // Create properties, mappers, volume actors, and ray cast function
+  vtkVolumeProperty *volumeProperty = vtkVolumeProperty::New();
+  volumeProperty ->SetColor( colorTransferFunction);
+  volumeProperty ->SetScalarOpacity (opacityTransferFunction);
+
+  vtkVolumeTextureMapper3D *volumeMapper = vtkVolumeTextureMapper3D::New();
+  volumeMapper ->SetInputConnection (reader->GetOutputPort());
+  volumeMapper ->SetSampleDistance (0.25);
+
+  vtkVolume *volume = vtkVolume::New();
+  volume ->SetMapper (volumeMapper);
+  volume ->SetProperty (volumeProperty);
+
+  // Okay now the graphics stuff
+  vtkRenderer *ren1 = vtkRenderer::New();
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+  renWin ->ReportGraphicErrorsOn();
+  renWin ->AddRenderer (ren1);
+  renWin ->SetSize (256, 256);
+  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+  iren ->SetRenderWindow (renWin);
+
+  //ren1 ->SetBackground (0.1, 0.2, 0.4);
+  ren1-> AddViewProp (volume);
+  renWin->Render();
+  ren1 ->ResetCameraClippingRange();
+
+  iren->Initialize();
+  iren->Start();
+*/
+  reader->Delete();
+
+  return 0; 
+}
+
+int TestvtkGDCMThreadedImageReader2(int argc, char *argv[])
+{
+  if( argc == 2 )
+    {
+    const char *filename = argv[1];
+    return TestvtkGDCMThreadedImageRead2<vtkGDCMThreadedImageReader2>(filename, true);
+    }
+
+  // else
+  gdcm::Trace::DebugOff();
+  gdcm::Trace::WarningOff();
+  int r = 0, i = 0;
+  const char *filename;
+  const char * const *filenames = gdcm::Testing::GetFileNames();
+  while( (filename = filenames[i]) )
+    {
+    r += TestvtkGDCMThreadedImageRead2<vtkGDCMThreadedImageReader2>( filename );
+    ++i;
+    }
+
+  return r;
+}
