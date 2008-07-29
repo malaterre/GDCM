29bb70f488adcfc2241feee81501ef5c0aa937d3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-07-29 12:29:20 +0000

    ENH: start test

---
+++ Testing/Source/MediaStorageAndFileFormat/Python/TestKakaduDecompressionMD5.py
@@ -0,0 +1,39 @@
+############################################################################
+#
+#  Program: GDCM (Grass Root DICOM). A DICOM library
+#  Module:  $URL$
+#
+#  Copyright (c) 2006-2008 Mathieu Malaterre
+#  All rights reserved.
+#  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+#
+#     This software is distributed WITHOUT ANY WARRANTY; without even
+#     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#     PURPOSE.  See the above copyright notice for more information.
+#
+############################################################################
+
+import gdcm
+import os,sys
+
+def TestKakadu(filename):
+  print filename
+  return 0
+
+if __name__ == "__main__":
+  sucess = 0
+  try:
+    filename = os.sys.argv[1]
+    sucess += TestKakadu( filename, True )
+  except:
+    # loop over all files:
+    t = gdcm.Testing()
+    nfiles = t.GetNumberOfFileNames()
+    for i in range(0,nfiles):
+      filename = t.GetFileName(i)
+      sucess += TestKakadu( filename )
+  
+  
+  # Test succeed ?
+  sys.exit(sucess == 0)
+
