9b85e83bbd39d7d1d0d2ff86d905c879ee2ddff3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-07-30 21:52:00 +0000

    ENH: add seaarch for kdu_expand

---
+++ CMake/FindKAKADU.cmake
@@ -0,0 +1,13 @@
+# 
+# this module looks for KAKADu
+# http://www.kakadusoftware.com/
+#
+
+FIND_PROGRAM(KDU_EXPAND_EXECUTABLE
+  kdu_expand
+  )
+
+MARK_AS_ADVANCED(
+  KDU_EXPAND_EXECUTABLE
+  )
+
