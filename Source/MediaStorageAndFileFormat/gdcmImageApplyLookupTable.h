0b8611c11cc713087e844c6a315abf3644880139
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-08-05 11:54:37 +0000

    ENH: add new filter

---
+++ Source/MediaStorageAndFileFormat/gdcmImageApplyLookupTable.h
@@ -0,0 +1,42 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef __gdcmImageApplyLookupTable_h
+#define __gdcmImageApplyLookupTable_h
+
+#include "gdcmImageToImageFilter.h"
+#include "gdcmTransferSyntax.h"
+
+namespace gdcm
+{
+
+
+class DataElement;
+class GDCM_EXPORT ImageApplyLookupTable : public ImageToImageFilter
+{
+public:
+  ImageApplyLookupTable() {}
+  ~ImageApplyLookupTable() {}
+
+  bool Apply();
+
+protected:
+
+private:
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmImageApplyLookupTable_h
+
