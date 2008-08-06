3d2472ca26f81b369fd1d98cd1bcfadcb4682e32
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-08-06 08:43:42 +0000

    ENH: fix for compressed TS

--- Source/MediaStorageAndFileFormat/gdcmImageApplyLookupTable.cxx
+++ Source/MediaStorageAndFileFormat/gdcmImageApplyLookupTable.cxx
@@ -26,11 +26,17 @@ bool ImageApplyLookupTable::Apply()
   int bitsample = lut.GetBitSample();
   assert( bitsample );
 
-  const DataElement& pixeldata = image.GetDataElement();
-  const ByteValue *bv = pixeldata.GetByteValue();
-  const char *p = bv->GetPointer();
-  std::istringstream is;
-  is.str( std::string( p, p + bv->GetLength() ) );
+  //const DataElement& pixeldata = image.GetDataElement();
+  //const ByteValue *bv = pixeldata.GetByteValue();
+  //const char *p = bv->GetPointer();
+  //std::istringstream is;
+  //is.str( std::string( p, p + bv->GetLength() ) );
+  unsigned long len = image.GetBufferLength();
+  char *p = new char[len];
+  image.GetBuffer( p );
+  std::stringstream is;
+  is.write( p, len );
+  delete[] p;
 
   std::ostringstream os;
   lut.Decode(is, os);
@@ -42,6 +48,18 @@ bool ImageApplyLookupTable::Apply()
   Output->SetPhotometricInterpretation( PhotometricInterpretation::RGB );
   Output->GetPixelFormat().SetSamplesPerPixel( 3 );
   Output->SetPlanarConfiguration( 0 ); // FIXME OT-PAL-8-face.dcm has a PlanarConfiguration while being PALETTE COLOR...
+  const gdcm::TransferSyntax &ts = image.GetTransferSyntax();
+  assert( ts == TransferSyntax::RLELossless );
+  if( ts.IsExplicit() )
+    {
+    Output->SetTransferSyntax( TransferSyntax::ExplicitVRLittleEndian );
+    }
+  else
+    {
+    assert( ts.IsImplicit() );
+    Output->SetTransferSyntax( TransferSyntax::ImplicitVRLittleEndian );
+    }
+
 
   bool success = true;
   return success;
