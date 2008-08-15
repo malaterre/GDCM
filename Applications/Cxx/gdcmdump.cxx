d415615c091279378a074a80a4508747cdd4965d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-08-15 19:24:54 +0000

    ENH: cleanup gdcmdump now that there is a nicer interface to PDB

--- Applications/Cxx/gdcmdump.cxx
+++ Applications/Cxx/gdcmdump.cxx
@@ -26,7 +26,7 @@
 #include "gdcmSystem.h"
 #include "gdcmDirectory.h"
 #include "gdcmCSAHeader.h"
-#include "gdcmDeflateStream.h"
+#include "gdcmPDBHeader.h"
 
 #include <string>
 #include <iostream>
@@ -57,81 +57,7 @@ int DoOperation(const std::string & filename)
   return 0;
 }
 
-/*
- * In some GE MEDICAL SYSTEMS image one can find a Data Element: 0025,xx1b,GEMS_SERS_01
- * which is documented as Protocol Data Block (compressed).
- * in fact this is a simple text format compressed using the gzip algorithm
- *
- * Typically one could do:
- *
- *   $ gdcmraw -i input.dcm -o output.raw -t 0025,101b 
- *
- * Skip the binary length (little endian encoding):
- *
- *   $ dd bs=4 skip=1 if=output.raw of=foo 
- *
- * Check file type:
- *
- *   $ file foo
- *   foo: gzip compressed data, was "Ex421Ser8Scan1", from Unix
- *
- * Gunzip !
- *   $ gzip -dc < foo > bar
- *   $ cat bar
- *
- * THANKS to: John Reiser (BitWagon.com) for hints
- *
- * For sample see:
- * GE_MR_0025xx1bProtocolDataBlock.dcm
- * ( <=> http://server.oersted.dtu.dk/personal/jw/jwpublic/courses/31540/mri/second_set/dicom/t2/b17.dcm)
- */
-
-int readprotocoldatablock(const char *input, size_t inputlen, bool verbose)
-{
-  // First 4 bytes are the length (again)
-  uint32_t len = *(uint32_t*)input;
-  if( verbose )
-    std::cout << len << "," << inputlen << std::endl;
-  if( len + 4 + 1 == inputlen )
-    {
-    if( verbose )
-      std::cout << "gzip stream was padded with an extra 0 \n";
-    }
-  else if( len + 4 == inputlen )
-    {
-    if( verbose )
-      std::cout << "gzip stream was not padded with an extra 0 \n";
-    }
-  else
-    {
-    std::cerr << "Found the Protocol Data Block but could not read length..." << std::endl;
-    return 1;
-    }
-  // Alright we need to check if the binary blob was padded, if padded we need to 
-  // discard the trailing \0 to please gzip:
-  std::string str( input + 4, input + len );
-  std::istringstream is( str );
-
-  zlib_stream::zip_istream gzis( is );
-
-//  if (gzis.is_gzip())
-//    {
-//    std::cout<<"crc check: "<<( gzis.check_crc() ? "ok" : "failed");
-//    std::cout << std::endl;
-//    }
-
-  std::string out;
-  //while( gzis >> out )
-  while( std::getline(gzis , out ) )
-    {
-    std::cout << out << std::endl;
-    }
-  //std::cout << out.size();
-
-  return 0;
-}
-
-int DumpProtocolDataBlock(const std::string & filename, bool verbose)
+int PrintPDB(const std::string & filename, bool verbose)
 {
   gdcm::Reader reader;
   reader.SetFileName( filename.c_str() );
@@ -140,20 +66,25 @@ int DumpProtocolDataBlock(const std::string & filename, bool verbose)
     std::cerr << "Failed to read: " << filename << std::endl;
     return 1;
     }
+
+  gdcm::PDBHeader pdb;
   const gdcm::DataSet& ds = reader.GetFile().GetDataSet();
 
-  const gdcm::PrivateTag tprotocoldatablock(0x0025,0x1b,"GEMS_SERS_01");
-  if( !ds.FindDataElement( tprotocoldatablock) ) 
-    {
-    std::cerr << "Could not find tag: " << tprotocoldatablock << std::endl;
-    return 1;
-    }
-  const gdcm::DataElement& protocoldatablock= ds.GetDataElement( tprotocoldatablock);
-  if ( protocoldatablock.IsEmpty() ) return 1;
-  const gdcm::ByteValue * bv = protocoldatablock.GetByteValue();
+  const gdcm::PrivateTag &t1 = pdb.GetPDBInfoTag();
 
-  std::cout << "Dumping: " << tprotocoldatablock << std::endl;
-  int ret = readprotocoldatablock( bv->GetPointer(), bv->GetLength(), verbose );
+  bool found = false;
+  int ret = 0;
+  if( ds.FindDataElement( t1 ) )
+    {
+    pdb.LoadFromDataElement( ds.GetDataElement( t1 ) );
+    pdb.Print( std::cout );
+    found = true;
+    }
+  if( !found )
+    {
+    std::cout << "no pdb tag found" << std::endl;
+    ret = 1;
+    }
 
   return ret;
 }
@@ -475,7 +406,7 @@ int main (int argc, char *argv[])
         }
       else if( printpdb )
         {
-        res += DumpProtocolDataBlock(*it, verbose);
+        res += PrintPDB(*it, verbose);
         }
       else if( printcsa )
         {
@@ -502,7 +433,7 @@ int main (int argc, char *argv[])
       }
     else if( printpdb )
       {
-      res += DumpProtocolDataBlock(filename, verbose);
+      res += PrintPDB(filename, verbose);
       }
     else if( printcsa )
       {
