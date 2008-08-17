d2bf653e32420a3e56f9ec25f1c32570d1febb58
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-08-17 08:21:33 +0000

    COMP: need to commit those files...

---
+++ Source/MediaStorageAndFileFormat/gdcmPNMCodec.h
@@ -0,0 +1,47 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef __gdcmPNMcodec_h
+#define __gdcmPNMcodec_h
+
+#include "gdcmImageCodec.h"
+
+namespace gdcm
+{
+
+/**
+ * \brief Class to do PNM
+ * \note
+ */
+class GDCM_EXPORT PNMCodec : public ImageCodec
+{
+public:
+  PNMCodec();
+  ~PNMCodec();
+  bool CanDecode(TransferSyntax const &ts) const;
+  bool CanCode(TransferSyntax const &ts) const;
+
+  unsigned long GetBufferLength() const { return BufferLength; }
+  void SetBufferLength(unsigned long l) { BufferLength = l; }
+
+  bool Read(const char *filename, DataElement &out) const;
+  bool Write(const char *filename, const DataElement &out) const;
+  //bool Write(const char *filename);
+private:
+  unsigned long BufferLength;
+};
+
+} // end namespace gdcm
+
+#endif //__gdcmPNMcodec_h
