02797e6febdecca2d9d077833c157364a6c075bf
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-08-19 20:20:25 +0000

    ENH: add new test. Plus start playing with the cross platform testing framework

---
+++ Testing/Source/InformationObjectDefinition/Cxx/TestNestedModuleEntries.cxx
@@ -0,0 +1,22 @@
+/*=========================================================================
+
+  Program: GDCM (Grass Root DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmNestedModuleEntries.h"
+
+int TestNestedModuleEntries(int, char *[])
+{
+  gdcm::NestedModuleEntries nme;
+  return 0;
+}
+
