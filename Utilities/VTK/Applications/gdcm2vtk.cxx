082a6c9e25d2f69539d96faf0595a70f6d3956ec
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-08-22 14:35:00 +0000

    ENH: support converting DICOM to DICOM

--- Utilities/VTK/Applications/gdcm2vtk.cxx
+++ Utilities/VTK/Applications/gdcm2vtk.cxx
@@ -29,6 +29,8 @@
 #include "vtkBMPReader.h"
 #include "vtkLookupTable.h"
 #include "vtkPointData.h"
+#include "vtkStructuredPointsReader.h"
+#include "vtkStructuredPoints.h"
 
 #include "gdcmFilename.h"
 
@@ -47,20 +49,34 @@ int main(int argc, char *argv[])
   imgfactory->RegisterReader( reader );
   vtkImageReader2* imgreader =
     imgfactory->CreateImageReader2(filename);
-  if( !imgreader )
+  vtkStructuredPointsReader *datareader = vtkStructuredPointsReader::New();
+  datareader->SetFileName( filename );
+  int res = datareader->IsFileStructuredPoints();
+  if( !imgreader && !res )
     {
     std::cerr << "could not find no reader to handle file: " << filename << std::endl;
     return 1;
     }
-  imgreader->SetFileName(filename);
   imgfactory->Delete(); 
+
+  vtkImageData *imgdata;
+  if( imgreader )
+    {
+    imgreader->SetFileName(filename);
+    imgreader->Update();
+    imgdata = imgreader->GetOutput();
+  std::cout << imgreader->GetClassName() << std::endl;
+    }
+  else if( res )
+    {
+    datareader->Update();
+    imgdata = datareader->GetOutput();
+    }
 
   vtkGDCMImageWriter * writer = vtkGDCMImageWriter::New();
   writer->SetFileName( outfilename );
 
-  imgreader->Update(); // important
-
-  if( imgreader->GetOutput()->GetNumberOfScalarComponents() == 4 )
+  if( imgreader && imgreader->GetOutput()->GetNumberOfScalarComponents() == 4 )
     {
     std::cerr << "alpha channel will be lost" << std::endl;
     vtkImageExtractComponents *extract = vtkImageExtractComponents::New();
@@ -71,7 +87,8 @@ int main(int argc, char *argv[])
     }
   else
     {
-    writer->SetInput( imgreader->GetOutput() );
+    //writer->SetInput( imgreader->GetOutput() );
+    writer->SetInput( imgdata );
 
 #if 0
     vtkImageRGBToYBR * rgb2ybr = vtkImageRGBToYBR::New();
@@ -82,54 +99,61 @@ int main(int argc, char *argv[])
     }
 
   // If input is 3D, let's write output as 3D if possible:
-  if( imgreader->GetOutput()->GetDimensions()[2] != 1 )
+  if( imgdata->GetDimensions()[2] != 1 )
     {
     writer->SetFileDimensionality( 3 );
     }
 
-  std::cout << imgreader->GetClassName() << std::endl;
-  if( vtkGDCMImageReader * reader = vtkGDCMImageReader::SafeDownCast(imgreader) )
+  if( imgreader )
     {
-    writer->SetMedicalImageProperties( reader->GetMedicalImageProperties() );
-    reader->GetMedicalImageProperties()->Print( std::cout );
+    if( vtkGDCMImageReader * reader = vtkGDCMImageReader::SafeDownCast(imgreader) )
+      {
+      writer->SetMedicalImageProperties( reader->GetMedicalImageProperties() );
+      writer->SetDirectionCosines( reader->GetDirectionCosines() );
+      writer->SetShift( reader->GetShift() );
+      writer->SetScale( reader->GetScale() );
+      reader->GetMedicalImageProperties()->Print( std::cout );
+      }
+    else if( vtkBMPReader * reader = vtkBMPReader::SafeDownCast(imgreader) )
+      {
+      reader->Allow8BitBMPOn( );
+      reader->Update( );
+      //reader->GetLookupTable()->Print( std::cout );
+      reader->GetOutput()->GetPointData()->GetScalars()->SetLookupTable( reader->GetLookupTable() );
+      writer->SetImageFormat( VTK_LOOKUP_TABLE );
+      }
+    else if( vtkGESignaReader * reader = vtkGESignaReader::SafeDownCast(imgreader) )
+      {
+      writer->SetMedicalImageProperties( reader->GetMedicalImageProperties() );
+      reader->GetMedicalImageProperties()->Print( std::cout );
+      }
+    else if( vtkMINCImageReader *reader = vtkMINCImageReader::SafeDownCast( imgreader ) )
+      {
+      writer->SetDirectionCosines( reader->GetDirectionCosines() );
+      //writer->GetMedicalImageProperties()->SetModality( "MR" );
+      // the following does not work with VTKData/Data/t3_grid_0.mnc
+      //writer->SetScale( reader->GetRescaleSlope() );
+      //writer->SetShift( reader->GetRescaleIntercept() );
+      reader->GetImageAttributes()->PrintFileHeader();
+      }
+    else if( vtkTIFFReader *reader = vtkTIFFReader::SafeDownCast( imgreader ) )
+      {
+      // TIFF has resolution (spacing), and VTK make sure to set set in mm
+      // For some reason vtkTIFFReader is all skrew up and will load the image in whatever orientation
+      // as stored on file, thus this is up to the user to set it properly...
+      // If anyone has any clue why...
+      reader->SetOrientationType( ORIENTATION_BOTLEFT );
+      }
     }
-  else if( vtkBMPReader * reader = vtkBMPReader::SafeDownCast(imgreader) )
-    {
-    reader->Allow8BitBMPOn( );
-    reader->Update( );
-    //reader->GetLookupTable()->Print( std::cout );
-    reader->GetOutput()->GetPointData()->GetScalars()->SetLookupTable( reader->GetLookupTable() );
-    writer->SetImageFormat( VTK_LOOKUP_TABLE );
-    }
-  else if( vtkGESignaReader * reader = vtkGESignaReader::SafeDownCast(imgreader) )
-    {
-    writer->SetMedicalImageProperties( reader->GetMedicalImageProperties() );
-    reader->GetMedicalImageProperties()->Print( std::cout );
-    }
-  else if( vtkMINCImageReader *reader = vtkMINCImageReader::SafeDownCast( imgreader ) )
-    {
-    writer->SetDirectionCosines( reader->GetDirectionCosines() );
-    //writer->GetMedicalImageProperties()->SetModality( "MR" );
-    // the following does not work with VTKData/Data/t3_grid_0.mnc
-    //writer->SetScale( reader->GetRescaleSlope() );
-    //writer->SetShift( reader->GetRescaleIntercept() );
-    reader->GetImageAttributes()->PrintFileHeader();
-    }
-  else if( vtkTIFFReader *reader = vtkTIFFReader::SafeDownCast( imgreader ) )
-    {
-    // TIFF has resolution (spacing), and VTK make sure to set set in mm
-    // For some reason vtkTIFFReader is all skrew up and will load the image in whatever orientation
-    // as stored on file, thus this is up to the user to set it properly...
-    // If anyone has any clue why...
-    reader->SetOrientationType( ORIENTATION_BOTLEFT );
-    }
+  // nothing special need to be done for vtkStructuredPointsReader 
 
   writer->Write();
 
   writer->GetInput()->Print( std::cout );
 
   writer->Delete();
-  imgreader->Delete();
+  if( imgreader ) imgreader->Delete();
+  datareader->Delete();
   reader->Delete();
 
   return 0;
