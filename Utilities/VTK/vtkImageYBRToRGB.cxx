9956a5b116bb438becc908134a83afd8dbb99f16
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-09-01 10:56:39 +0000

    BUG: YBR_FULL seems to be buggy for some images...

--- Utilities/VTK/vtkImageYBRToRGB.cxx
+++ Utilities/VTK/vtkImageYBRToRGB.cxx
@@ -29,7 +29,6 @@ vtkImageYBRToRGB::vtkImageYBRToRGB()
   this->SetNumberOfOutputPorts(1);
 }
 
-
 //----------------------------------------------------------------------------
 // This templated function executes the filter for any type of data.
 template <class T>
@@ -45,7 +44,7 @@ void vtkImageYBRToRGBExecute(vtkImageYBRToRGB *self,
   // find the region to loop over
   int maxC = inData->GetNumberOfScalarComponents()-1;
   
-  int R, G, B;
+  //int R, G, B;
   // Loop through ouput pixels
   while (!outIt.IsAtEnd())
     {
@@ -55,6 +54,8 @@ void vtkImageYBRToRGBExecute(vtkImageYBRToRGB *self,
     while (outSI != outSIEnd)
       {
       // Pixel operation
+#if 0
+#if 0
       unsigned char a = (unsigned char)(*inSI); ++inSI;
       unsigned char b = (unsigned char)(*inSI); ++inSI;
       unsigned char c = (unsigned char)(*inSI); ++inSI;
@@ -67,12 +68,50 @@ void vtkImageYBRToRGBExecute(vtkImageYBRToRGB *self,
       G = (G+16384)>>15;
       B = (B+16384)>>15;
 
+#else
+      int /*unsigned char*/ y = (unsigned char)(*inSI); ++inSI;
+      y -= 16;
+      unsigned char u = (unsigned char)(*inSI); ++inSI;
+      int Cb = (int)u - 128;
+      unsigned char v = (unsigned char)(*inSI); ++inSI;
+      int Cr = (int)v - 128;
+
+ //     R = y + (1.4075 * (v - 128));
+ //     G = y - (0.3455 * (u - 128) - (0.7169 * (v - 128)));
+ //     B = y + (1.7790 * (u - 128));
+ 	R = y                + 1.40200 * Cr + 0.5;
+ 	G = y - 0.34414 * Cb - 0.71414 * Cr + 0.5;
+ 	B = y + 1.77200 * Cb + 0.5;
+
+
+      //int a = (int)y - 16;
+      //int b = (int)u - 128;
+      //int c = (int)v - 128;
+
+      //R = ( 1.164 * a + 0.    * b + 1.596 * c );
+      //G = ( 1.164 * a + 0.391 * b + 0.813 * c );
+      //B = ( 1.164 * a + 2.018 * b + 0.    * c );
+#endif
       if (R < 0)   R = 0;
       if (G < 0)   G = 0;
       if (B < 0)   B = 0;
       if (R > 255) R = 255;
       if (G > 255) G = 255;
       if (B > 255) B = 255;
+#endif
+
+      double y = *inSI; ++inSI;
+      double u = *inSI; ++inSI;
+      double v = *inSI; ++inSI;
+      unsigned char R,G,B;
+      double maxval = 255.;
+
+  double dr = y + 1.4020 * v - 0.7010 * maxval;
+  double dg = y - 0.3441 * u - 0.7141 * v + 0.5291 * maxval;
+  double db = y + 1.7720 * u - 0.8859 * maxval;
+  R = (dr < 0.0) ? 0 : ((dr+0.5) > maxval) ? maxval : (unsigned char)(dr+0.5);
+  G = (dg < 0.0) ? 0 : ((dg+0.5) > maxval) ? maxval : (unsigned char)(dg+0.5);
+  B = (db < 0.0) ? 0 : ((db+0.5) > maxval) ? maxval : (unsigned char)(db+0.5);
 
       // assign output.
       *outSI = (T)(R); ++outSI;
