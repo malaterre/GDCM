8349076ab8243ee044c0128c32104aa737b757a3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-09-08 21:54:33 +0000

    ENH: create a module to handle cpoyirghts

---
+++ CMake/UseCopyright.cmake
@@ -0,0 +1,20 @@
+# Handy macro to gather all copyright in a single file (to pass to cpack)
+
+MACRO(CREATE_COPYRIGHT_FILE name)
+  # Always cleanup the file:
+  FILE(WRITE ${name} "")
+  SET(COPYRIGHT_MODULE_FILENAME ${name})
+MACRO(CREATE_COPYRIGHT_FILE)
+
+# Append copyright file
+MACRO(APPEND_COPYRIGHT)
+  # need to raise an error if COPYRIGHT_MODULE_FILENAME is not set...
+  IF(EXISTS ${COPYRIGHT_MODULE_FILENAME} )
+    FOREACH(filename ${ARGN})
+      FILE(READ ${filename} content)
+      FILE(APPEND ${COPYRIGHT_MODULE_FILENAME} ${filename})
+    ENDFOREACH(filename)
+  ENDIF(EXISTS ${COPYRIGHT_MODULE_FILENAME} )
+ENDMACRO(ADD_COPYRIGHT)
+
+
