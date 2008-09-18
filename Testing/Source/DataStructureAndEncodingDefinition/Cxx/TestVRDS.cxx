2281e1efa12a82ddf1c3c504204e080457e32a57
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-09-18 10:03:05 +0000

    ENH: float vs double on a 16bytes string

--- Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestVRDS.cxx
+++ Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestVRDS.cxx
@@ -14,11 +14,48 @@
 =========================================================================*/
 #include "gdcmVR.h"
 
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+
+template <typename T, unsigned int MAXBYTES>
+std::string TestVRDSFunc(const char *str)
+{
+  std::istringstream is( str );
+  T d;
+  is >> d;
+  std::ostringstream os;
+  os << std::setprecision(MAXBYTES);
+  os << d;
+  std::cout << std::setprecision(MAXBYTES);
+  std::cout << d << std::endl;
+  std::string copy = os.str();
+  return copy;
+}
+
 /*
  * Test to make sure that double precision ieee 'double' is ok for DICOM VR = 'DS'
  */
 int TestVRDS(int, char *[])
 {
-  const char str[16+1] = "0.1234567891234";
+  const unsigned int dsmaxbytes = 16;
+  const char str[dsmaxbytes+1] = "0.123456789123";
+  std::string copy;
+
+  // Let's demonstrate the float can easily fails;
+  if( (copy = TestVRDSFunc<float,dsmaxbytes>(str)) == str )
+    {
+    std::cerr << "Float works:" << copy << " vs " << str << std::endl;
+    return 1;
+    }
+  
+  // Repeat with double, it works this time
+  if( (copy = TestVRDSFunc<double,dsmaxbytes>(str)) != str )
+    {
+    std::cerr << "Double does not work:" << copy << " vs " << str << std::endl;
+    return 1;
+    }
+
   return 0;
 }
