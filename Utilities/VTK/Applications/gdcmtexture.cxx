fc25a5a20041fb98fcc96df1a3221fd0580a7080
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-10-03 10:42:23 +0000

    ENH: dummy examples

---
+++ Utilities/VTK/Applications/gdcmtexture.cxx
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkGDCMImageReader.h"
+
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkImageData.h"
+#include "vtkLookupTable.h"
+#include "vtkTexture.h"
+#include "vtkPlaneSource.h"
+
+int main( int argc, char *argv[] )
+{
+  vtkGDCMImageReader *reader = vtkGDCMImageReader::New();
+  reader->SetFileName( argv[1] );
+
+  reader->Update();
+  vtkImageData* ima = reader->GetOutput();
+
+  vtkLookupTable* table = vtkLookupTable::New();
+  table->SetNumberOfColors(1000);
+  table->SetTableRange(0,1000);
+  table->SetSaturationRange(0,0);
+  table->SetHueRange(0,1);
+  table->SetValueRange(0,1);
+  table->SetAlphaRange(1,1);
+  table->Build();
+
+  // Texture
+  vtkTexture* texture = vtkTexture::New();
+  texture->SetInput(ima);
+  texture->InterpolateOn();
+  texture->SetLookupTable(table);
+
+  // PlaneSource
+  vtkPlaneSource* plane = vtkPlaneSource::New();
+  plane->SetOrigin( -0.5, -0.5, 0.0);
+  plane->SetPoint1(  0.5, -0.5, 0.0);
+  plane->SetPoint2( -0.5,  0.5, 0.0);
+
+  // PolyDataMapper
+  vtkPolyDataMapper *planeMapper = vtkPolyDataMapper::New();
+  planeMapper->SetInput(plane->GetOutput());
+
+  // Actor
+  vtkActor* planeActor = vtkActor::New();
+  planeActor->SetTexture(texture);
+  planeActor->SetMapper(planeMapper);
+  planeActor->PickableOn();
+
+  // Final rendering with simple interactor:
+  vtkRenderer        *ren = vtkRenderer::New();
+  vtkRenderWindow *renwin = vtkRenderWindow::New();
+  renwin->AddRenderer(ren);
+  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+  iren->SetRenderWindow(renwin);
+  ren->AddActor(planeActor);
+  ren->SetBackground(0,0,0.5);
+  renwin->Render();
+  iren->Start();
+
+  // Clean up:
+  reader->Delete();
+  table->Delete();
+  texture->Delete();
+  plane->Delete();
+  planeMapper->Delete();
+  planeActor->Delete();
+  ren->Delete();
+  renwin->Delete();
+  iren->Delete();
+
+  return 0;
+}
