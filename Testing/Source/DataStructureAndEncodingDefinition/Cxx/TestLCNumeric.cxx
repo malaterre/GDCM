b2a807dfc801d4c735e605f68951079180aa1cde
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-10-08 11:49:36 +0000

    ENH: check LC_NUMERIC issue

---
+++ Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestLCNumeric.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmAttribute.h"
+
+int TestLCNumeric(int argc, char *argv[])
+{
+  setenv("LC_NUMERIC", "fr_FR", 1);
+  std::ostringstream os;
+  double d = 1.2;
+  os << d;
+  std::string s = os.str();
+  std::cout << s << std::endl;
+  std::string::size_type pos_comma = s.find( "," );
+  if( pos_comma != std::string::npos )
+    {
+    return 1;
+    }
+  std::string::size_type pos_dot = s.find( "." );
+  if( pos_dot == std::string::npos )
+    {
+    return 1;
+    }
+  return 0;
+}
