14be7b0beb3d0654113f59b0a6d3ac3dd9ea0476
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-10-09 10:36:08 +0000

    DOC: update

--- Source/MediaStorageAndFileFormat/gdcmRescaler.h
+++ Source/MediaStorageAndFileFormat/gdcmRescaler.h
@@ -32,19 +32,33 @@ public:
   Rescaler():Intercept(0),Slope(1),PF(PixelFormat::UNKNOWN) {}
   ~Rescaler() {}
 
+  /// Direct transform
   bool Rescale(char *out, const char *in, size_t n);
+
+  /// Inverse transform
   bool InverseRescale(char *out, const char *in, size_t n);
 
+  /// Set Intercept: used for both direct&inverse transformation
   void SetIntercept(double i) { Intercept = i; }
+
+  /// Set Slope: user for both direct&inverse transformation
   void SetSlope(double s) { Slope = s; }
+
+  /// Set Pixel Format of input data
   void SetPixelFormat(PixelFormat const & pf) { PF = pf; }
 
   PixelFormat::ScalarType ComputeInterceptSlopePixelType();
+
+  /// Set target interval for output data. A best match will be computed (if possible)
+  /// Used for inverse transformation 
   void SetMinMaxForPixelType(double min, double max)
     {
     ScalarRangeMin = min;
     ScalarRangeMax = max;
     }
+
+  /// Compute the Pixel Format of the output data
+  /// Used for inverse transformation 
   PixelFormat ComputePixelTypeFromMinMax();
 
 protected:
