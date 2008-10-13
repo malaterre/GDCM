03d802eef7b524aa59fbd887545ff7ff4eacd1da
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-10-13 15:37:06 +0000

    ENH: test

---
+++ Wrapping/Csharp/HelloWorld.cs
@@ -0,0 +1,26 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+using System;
+using boolsNamespace;
+
+public class HelloWorld
+{
+  public static void Main()
+    {
+    System.Console.WriteLine("Hello World !");
+    }
+}
+
