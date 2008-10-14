6d6c4a5c63d9c823a5775f665fd669a8ddb5a123
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-10-14 06:50:57 +0000

    ENH: update for SC iamge

--- Examples/Python/MergeFile.py
+++ Examples/Python/MergeFile.py
@@ -13,6 +13,17 @@
 #
 ############################################################################
 
+"""
+Usage:
+
+ python MergeFile.py input1.dcm input2.dcm
+
+  It will produce a 'merge.dcm' output file, which contains all meta information from input1.dcm
+  and copy the Stored Pixel values from input2.dcm
+  This script even works when input2.dcm is a Secondary Capture and does not contains information
+  such as IOP and IPP...
+"""
+
 import sys
 import gdcm
 
@@ -31,9 +42,19 @@ if __name__ == "__main__":
   if not r2.Read():
     sys.exit(1)
 
+  print dir(r2.GetImage())
+  print r2.GetImage().GetDataElement()
+
+  # Image from r2 could be Secondary Capture and thus would not contains neither IPP nor IOP
+  # Instead always prefer to only copy the Raw Data Element.
+  # Warning ! Image need to be identical ! Only the value of Stored Pixel can be different.
+  r1.GetImage().SetDataElement( r2.GetImage().GetDataElement() )
+
   w = gdcm.ImageWriter()
   w.SetFile( r1.GetFile() )
-  w.SetImage( r2.GetImage() )
+  #w.SetImage( r2.GetImage() )  # See comment above
+  w.SetImage( r1.GetImage() )
+
   w.SetFileName( "merge.dcm" )
   if not w.Write():
     sys.exit(1)
