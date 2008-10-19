2e97131830a3b65252c8602e666eeb7432382ce5
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-10-19 11:52:22 +0000

    ENH: debug

--- Source/MediaStorageAndFileFormat/gdcmImageHelper.cxx
+++ Source/MediaStorageAndFileFormat/gdcmImageHelper.cxx
@@ -793,6 +793,7 @@ std::vector<double> ImageHelper::GetSpacingValue(File const & f)
     const DataElement& de = ds.GetDataElement( Tag(0x0028,0x0009) );
     gdcm::Attribute<0x0028,0x0009> at;
     at.SetFromDataElement( de );
+    assert( ds.FindDataElement( at.GetValue() ) );
     if( ds.FindDataElement( at.GetValue() ) )
       {
 /*
