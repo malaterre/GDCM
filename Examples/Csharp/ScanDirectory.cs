5f3f202d5d13e01a2a07947a7c58b6ccbe322515
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-12-03 16:54:30 +0000

    ENH: add dummy example

---
+++ Examples/Csharp/ScanDirectory.cs
@@ -0,0 +1,46 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2008 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+/*
+ * Usage: 
+ * $ export LD_LIBRARY_PATH=$HOME/Projects/gdcm/debug-gcc/bin
+ * $ mono bin/ScanDirectory.exe /path/to/gdcmData/
+ */
+using System;
+using gdcm;
+
+public class ScanDirectory
+{
+  public static int Main(string[] args)
+    {
+    string directory = args[0];
+    Tag t = new Tag(0x8,0x8);
+
+    Directory d = new Directory();
+    uint nfiles = d.Load( directory );
+    //System.Console.WriteLine( "Files:\n" + d.toString() );
+
+    Scanner s = new Scanner();
+    s.AddTag( t );
+    bool b = s.Scan( d.GetFilenames() );
+    if(!b) return 1;
+
+    System.Console.WriteLine( "Scan:\n" + s.toString() );
+    
+    System.Console.WriteLine( "success" );
+    return 0;
+    }
+}
+
