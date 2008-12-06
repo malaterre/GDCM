6d5ef135a8e70220b5d4369302576130d3318a85
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-12-06 10:42:07 +0000

    ENH: new unit test

---
+++ Testing/Source/MediaStorageAndFileFormat/Python/TestOrientation.py
@@ -0,0 +1,34 @@
+############################################################################
+#
+#  Program: GDCM (Grassroots DICOM). A DICOM library
+#  Module:  $URL$
+#
+#  Copyright (c) 2006-2008 Mathieu Malaterre
+#  All rights reserved.
+#  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+#
+#     This software is distributed WITHOUT ANY WARRANTY; without even
+#     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#     PURPOSE.  See the above copyright notice for more information.
+#
+############################################################################
+
+import gdcm
+import os,sys
+
+if __name__ == "__main__":
+  sucess = True
+
+  ori1 = (1,0,0,0,1,0)
+  ori2 = (1,0,0,0,0,1)
+
+  label1 = gdcm.Orientation.GetLabel( gdcm.Orientation.GetType( ori1 ) )
+  if label1 != 'AXIAL':
+    sucess = False
+  label2 = gdcm.Orientation.GetLabel( gdcm.Orientation.GetType( ori2 ) )
+  if label1 != 'CORONAL':
+    sucess = False
+
+  # Test succeed ?
+  sys.exit(sucess)
+
