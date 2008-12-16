bde0e33a7baff5017e8d03496e1e56bf63a91110
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-12-16 13:54:42 +0000

    ENH: finished script

--- Utilities/VTK/Examples/Python/CastConvertPhilips.py
+++ Utilities/VTK/Examples/Python/CastConvertPhilips.py
@@ -25,19 +25,23 @@ import sys
 import gdcm
 
 filename = sys.argv[1]
+outfilename = sys.argv[2]
 tmpfile = "/tmp/philips_rescaled.dcm"
 
-#vtkreader = vtkgdcm.vtkGDCMImageReader()
-#vtkreader.SetFileName( filename )
-#vtkreader.Update()
+vtkreader = vtkgdcm.vtkGDCMImageReader()
+vtkreader.SetFileName( filename )
+vtkreader.Update()
 
-#ss = vtk.vtkImageShiftScale()
-#ss.SetInput( vtkreader.GetOutput() )
-#ss.SetShift( shift )
-#ss.SetScale( scale )
-#ss.Update()
 
-# 2nd version simply shift tags around
+# (2005,1409)     DS      4       0.0
+# (2005,140a)     DS      16      1.52283272283272
+
+# (2005,0014)     LO      26      Philips MR Imaging DD 005
+tag1 = gdcm.PrivateTag(0x2005,0x09,"Philips MR Imaging DD 005")
+tag2 = gdcm.PrivateTag(0x2005,0x0a,"Philips MR Imaging DD 005")
+
+
+
 # Need to access some private tags, reread the file (for now):
 reader = gdcm.Reader()
 reader.SetFileName( filename )
@@ -46,36 +50,57 @@ if not reader.Read():
 
 ds = reader.GetFile().GetDataSet()
 
-#print ds
-# (2005,1409)     DS      4       0.0
-# (2005,140a)     DS      16      1.52283272283272
+el1 = ds.GetDataElement( tag1 )
+el2 = ds.GetDataElement( tag2 )
 
-# (2005,0014)     LO      26      Philips MR Imaging DD 005
-tag1 = gdcm.PrivateTag(0x2005,0x09,"Philips MR Imaging DD 005")
-tag2 = gdcm.PrivateTag(0x2005,0x0a,"Philips MR Imaging DD 005")
-print tag1
-print tag2
 
-# make sure to do a copy, we want the private tag to remain
-# otherwise gdcm gives us a reference
-el1 = gdcm.DataElement( ds.GetDataElement( tag1 ) )
-print el1
-el2 = gdcm.DataElement( ds.GetDataElement( tag2 ) )
-print el2
+#pf = gdcm.PythonFilter()
+#pf.SetFile( reader.GetFile() )
+#print el1.GetTag()
 
-# (0028,1052) DS [-1000]                                  #   6, 1 RescaleIntercept
-# (0028,1053) DS [1]                                      #   2, 1 RescaleSlope
+print el1.GetByteValue()
+v1 = eval(el1.GetByteValue().GetBuffer())
+print el2.GetByteValue()
+v2 = eval(el2.GetByteValue().GetBuffer())
 
-el1.SetTag( gdcm.Tag(0x0028,0x1052) )
-el2.SetTag( gdcm.Tag(0x0028,0x1053) )
+print v1
+shift = v1
+print v2
+scale = v2
 
-ds.Insert( el1 )
-ds.Insert( el2 )
+ss = vtk.vtkImageShiftScale()
+ss.SetInput( vtkreader.GetOutput() )
+# because VTK image shift / scale convention is inverted from DICOM make sure shift is 0
+assert shift == 0
+ss.SetShift( shift )
+ss.SetScale( scale )
+ss.SetOutputScalarTypeToUnsignedShort ()
+ss.Update()
 
-w = gdcm.Writer()
-w.SetFileName( tmpfile )
-w.SetFile( reader.GetFile() )
-if not w.Write():
+# vtkGDCMImageWriter does not support Sequence, so let's write a tmp file first:
+vtkwriter = vtkgdcm.vtkGDCMImageWriter()
+vtkwriter.SetFileName( tmpfile )
+vtkwriter.SetMedicalImageProperties( vtkreader.GetMedicalImageProperties() )
+vtkwriter.SetDirectionCosines( vtkreader.GetDirectionCosines() )
+vtkwriter.SetInput( ss.GetOutput() )
+#vtkwriter.Update()
+vtkwriter.Write()
+
+# ok now rewrite the exact same file as the original (keep all info)
+# but use the Pixel Data Element from the written file
+tmpreader = gdcm.ImageReader()
+tmpreader.SetFileName( tmpfile )
+if not tmpreader.Read():
+  sys.exit(1)
+
+writer = gdcm.ImageWriter()
+writer.SetFileName( outfilename )
+# Pass image from vtk written file
+writer.SetImage( tmpreader.GetImage() )
+# pass dataset from initial 'reader'
+writer.SetFile( reader.GetFile() )
+if not writer.Write():
   sys.exit(1)
 
 print "success"
+
