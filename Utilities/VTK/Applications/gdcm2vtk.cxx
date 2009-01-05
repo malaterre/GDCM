5dd10923d5f5f97d253dce03561193ffe8ac923d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-01-05 14:38:05 +0000

    enh: getoptify

--- Utilities/VTK/Applications/gdcm2vtk.cxx
+++ Utilities/VTK/Applications/gdcm2vtk.cxx
@@ -15,6 +15,7 @@
 #include "vtkGDCMImageReader.h"
 #include "vtkGDCMImageWriter.h"
 
+#include "vtkVersion.h"
 #include "vtkImageReader2Factory.h"
 #include "vtkImageReader2.h"
 #include "vtkImageData.h"
@@ -35,15 +36,177 @@
 #include "vtkStructuredPoints.h"
 
 #include "gdcmFilename.h"
+#include "gdcmTrace.h"
+#include "gdcmVersion.h"
+#include "gdcmImageHelper.h"
+
+#include <getopt.h>
+
+void PrintVersion()
+{
+  std::cout << "gdcm2vtk: gdcm " << gdcm::Version::GetVersion() << " ";
+  const char date[] = "$Date$";
+  std::cout << date << std::endl;
+  //std::cout << "             VTK " << vtkVersion::GetVTKVersion() << std::endl;
+  std::cout << "            " << vtkVersion::GetVTKSourceVersion() << std::endl;
+}
+
+void PrintHelp()
+{
+  PrintVersion();
+  std::cout << "Usage: gdcm2vtk [OPTION] input output" << std::endl;
+  std::cout << "Convert a vtk-supported file into DICOM.\n";
+  std::cout << "Options:" << std::endl;
+  std::cout << "     --force-rescale    force rescale." << std::endl;
+  std::cout << "     --force-spacing    force spacing." << std::endl;
+  std::cout << "General Options:" << std::endl;
+  std::cout << "  -V --verbose    more verbose (warning+error)." << std::endl;
+  std::cout << "  -W --warning    print warning info." << std::endl;
+  std::cout << "  -D --debug      print debug info." << std::endl;
+  std::cout << "  -E --error      print error info." << std::endl;
+  std::cout << "  -h --help       print help." << std::endl;
+  std::cout << "  -v --version    print version." << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
-  if( argc < 3 )
+  int c;
+  //int digit_optind = 0;
+
+  std::vector<std::string> filenames;
+  int forcerescale = 0;
+  int forcespacing = 0;
+
+  int verbose = 0;
+  int warning = 0;
+  int debug = 0;
+  int error = 0;
+  int help = 0;
+  int version = 0;
+
+  while (1) {
+    //int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"force-rescale", 0, &forcerescale, 1},
+        {"force-spacing", 0, &forcespacing, 1},
+
+// General options !
+        {"verbose", 0, &verbose, 1},
+        {"warning", 0, &warning, 1},
+        {"debug", 0, &debug, 1},
+        {"error", 0, &error, 1},
+        {"help", 0, &help, 1},
+        {"version", 0, &version, 1},
+
+        {0, 0, 0, 0}
+    };
+
+    c = getopt_long (argc, argv, "VWDEhv",
+      long_options, &option_index);
+    if (c == -1)
+      {
+      break;
+      }
+
+    switch (c)
+      {
+    case 0:
+        {
+        const char *s = long_options[option_index].name;
+        //printf ("option %s", s);
+        if (optarg)
+          {
+          if( option_index == 0 ) /* input */
+            {
+            assert( strcmp(s, "input") == 0 );
+            //assert( filename.empty() );
+            //filename = optarg;
+            }
+          printf (" with arg %s", optarg);
+          }
+        //printf ("\n");
+        }
+      break;
+
+    case 'V':
+      verbose = 1;
+      break;
+
+    case 'W':
+      warning = 1;
+      break;
+
+    case 'D':
+      debug = 1;
+      break;
+
+    case 'E':
+      error = 1;
+      break;
+
+    case 'h':
+      help = 1;
+      break;
+
+    case 'v':
+      version = 1;
+      break;
+
+    case '?':
+      break;
+
+    default:
+      printf ("?? getopt returned character code 0%o ??\n", c);
+      }
+  }
+
+  if (optind < argc)
+    {
+    //printf ("non-option ARGV-elements: ");
+    while (optind < argc)
+      {
+      //printf ("%s ", argv[optind]);
+      filenames.push_back( argv[optind++] );
+      }
+    //printf ("\n");
+    }
+
+  if( version )
+    {
+    //std::cout << "version" << std::endl;
+    PrintVersion();
+    return 0;
+    }
+
+  if( help )
+    {
+    //std::cout << "help" << std::endl;
+    PrintHelp();
+    return 0;
+    }
+
+  // Debug is a little too verbose
+  gdcm::Trace::SetDebug( debug );
+  gdcm::Trace::SetWarning( warning );
+  gdcm::Trace::SetError( error );
+  // when verbose is true, make sure warning+error are turned on:
+  if( verbose )
+    {
+    gdcm::Trace::SetWarning( verbose );
+    gdcm::Trace::SetError( verbose);
+    }
+
+  if( filenames.size() != 2 )
     {
     return 1;
     }
-  const char *filename = argv[1];
-  const char *outfilename = argv[2];
+  const char *filename = filenames[0].c_str();
+  const char *outfilename = filenames[1].c_str();
+
+
+  gdcm::ImageHelper::SetForceRescaleInterceptSlope(forcerescale);
+  gdcm::ImageHelper::SetForcePixelSpacing(forcespacing);
 
   vtkGDCMImageReader *reader = vtkGDCMImageReader::New();
 
