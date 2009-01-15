a60d8ddaee925acc98c0edbd4fbd3237ebb7c10b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-01-15 12:44:21 +0000

    ENH: sort

--- Examples/Cxx/VolumeSorter.cxx
+++ Examples/Cxx/VolumeSorter.cxx
@@ -21,53 +21,64 @@
 #include "gdcmTesting.h"
 
 
-bool mysort(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
+bool mysort1(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
 {
-  //gdcm::Attribute<0x0020,0x0013> at1; // Instance Number 
-  gdcm::Attribute<0x0018,0x1060> at1; // Trigger Time
-  gdcm::Attribute<0x0020,0x0032> at11; // Image Position (Patient)
+  gdcm::Attribute<0x0020,0x000d> at1;
   at1.Set( ds1 );
-  at11.Set( ds1 );
-  //gdcm::Attribute<0x0020,0x0013> at2;
-  gdcm::Attribute<0x0018,0x1060> at2;
-  gdcm::Attribute<0x0020,0x0032> at22;
+  gdcm::Attribute<0x0020,0x000d> at2;
   at2.Set( ds2 );
-  at22.Set( ds2 );
-  if( at11 == at22 )
+  return at1 < at2;
+}
+
+bool mysort2(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
+{
+  gdcm::Attribute<0x0020,0x000e> at1;
+  at1.Set( ds1 );
+  gdcm::Attribute<0x0020,0x000e> at2;
+  at2.Set( ds2 );
+  return at1 < at2;
+}
+
+bool mysort3(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
+{
+  // This is a floating point number is the comparison ok ?
+  gdcm::Attribute<0x0020,0x0037> at1;
+  at1.Set( ds1 );
+  gdcm::Attribute<0x0020,0x0037> at2;
+  at2.Set( ds2 );
+  return at1 < at2;
+}
+
+bool mysort4(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
+{
+  // Do the IPP sorting here
+  gdcm::Attribute<0x0020,0x0032> ipp1;
+  gdcm::Attribute<0x0020,0x0037> iop1;
+  ipp1.Set( ds1 );
+  iop1.Set( ds1 );
+  gdcm::Attribute<0x0020,0x0032> ipp2;
+  gdcm::Attribute<0x0020,0x0037> iop2;
+  ipp2.Set( ds2 );
+  iop2.Set( ds2 );
+  if( !(iop1 == iop2) )
     {
-    return at1 < at2;
+    return true;
     }
-  return at11 < at22;
+
+  // else
+  double normal[3];
+  normal[0] = iop1[1]*iop1[5] - iop1[2]*iop1[4];
+  normal[1] = iop1[2]*iop1[3] - iop1[0]*iop1[5];
+  normal[2] = iop1[0]*iop1[4] - iop1[1]*iop1[3];
+  double dist1 = 0;
+  for (int i = 0; i < 3; ++i) dist1 += normal[i]*ipp1[i];
+  double dist2 = 0;
+  for (int i = 0; i < 3; ++i) dist2 += normal[i]*ipp2[i];
+
+  return dist1 < dist2;
+ 
 }
 
-bool mysort_part1(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
-{
-  gdcm::Attribute<0x0018,0x1060> at1;
-  at1.Set( ds1 );
-  gdcm::Attribute<0x0018,0x1060> at2;
-  at2.Set( ds2 );
-  return at1 < at2;
-}
-
-bool mysort_part2(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
-{
-  gdcm::Attribute<0x0020,0x0032> at1;
-  at1.Set( ds1 );
-  gdcm::Attribute<0x0020,0x0032> at2;
-  at2.Set( ds2 );
-  return at1 < at2;
-}
-
-// technically all files are in the same Frame of Reference, so this function
-// should be a no-op
-bool mysort_dummy(gdcm::DataSet const & ds1, gdcm::DataSet const & ds2 )
-{
-  gdcm::Attribute<0x0020,0x0052> at1; // FrameOfReferenceUID
-  at1.Set( ds1 );
-  gdcm::Attribute<0x0020,0x0052> at2;
-  at2.Set( ds2 );
-  return at1 < at2;
-}
 
 int main(int argc, char *argv[])
 {
@@ -95,16 +106,13 @@ int main(int argc, char *argv[])
   gdcm::Scanner s;
   const gdcm::Tag t1(0x0020,0x000d); // Study Instance UID
   const gdcm::Tag t2(0x0020,0x000e); // Series Instance UID
-  const gdcm::Tag t3(0x0010,0x0010); // Patient's Name
-  const gdcm::Tag t4(0x0004,0x5678); // DUMMY element
-  const gdcm::Tag t5(0x0028,0x0010); // Rows
-  const gdcm::Tag t6(0x0028,0x0011); // Columns
+  //const gdcm::Tag t3(0x0010,0x0010); // Patient's Name
   s.AddTag( t1 );
   s.AddTag( t2 );
-  s.AddTag( t3 );
-  s.AddTag( t4 );
-  s.AddTag( t5 );
-  s.AddTag( t6 );
+  //s.AddTag( t3 );
+  //s.AddTag( t4 );
+  //s.AddTag( t5 );
+  //s.AddTag( t6 );
   bool b = s.Scan( d.GetFilenames() );
   if( !b )
     {
@@ -124,12 +132,21 @@ int main(int argc, char *argv[])
     return 1;
     }
 
-  gdcm::Directory::FilenamesType::const_iterator file = l2.begin();
-  for(;file != l2.end(); ++file)
-    {
-    const char *fn = file->c_str();
-    gdcm::Scanner::TagToValue const &tag2value = s.GetMapping(fn);
-    }
+  gdcm::Sorter sorter;
+  sorter.SetSortFunction( mysort1 );
+  sorter.StableSort( l2 );
+
+  sorter.SetSortFunction( mysort2 );
+  sorter.StableSort( sorter.GetFilenames() );
+
+  sorter.SetSortFunction( mysort3 );
+  sorter.StableSort( sorter.GetFilenames() );
+
+  sorter.SetSortFunction( mysort4 );
+  sorter.StableSort( sorter.GetFilenames() );
+
+  sorter.Print( std::cout );
+
 
   return 0;
 }
