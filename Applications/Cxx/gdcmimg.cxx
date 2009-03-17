c33913e72e53d1da2c7ca1894bc66892466ff633
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-03-17 16:01:16 +0000

    j2c

--- Applications/Cxx/gdcmimg.cxx
+++ Applications/Cxx/gdcmimg.cxx
@@ -753,6 +753,7 @@ int main (int argc, char *argv[])
 
     if(  gdcm::System::StrCaseCmp(inputextension,".jp2") == 0 
       || gdcm::System::StrCaseCmp(inputextension,".j2k") == 0
+      || gdcm::System::StrCaseCmp(inputextension,".j2c") == 0
       || gdcm::System::StrCaseCmp(inputextension,".jpc") == 0 )
       {
       /*
