7c752daf533a34c3a9fb3a0bd243242e90be1bcd
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-03-31 12:34:57 +0000

    ENH: add wrapper

---
+++ Utilities/gdcm_polarssl.h
@@ -0,0 +1,26 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef __gdcm_uuid_h
+#define __gdcm_uuid_h
+
+/* Use the uuid library configured for gdcm.  */
+#include "gdcmTypes.h"
+#ifdef GDCM_USE_SYSTEM_UUID
+# include <uuid/uuid.h>
+#else
+# include <gdcmuuid/uuid.h>
+#endif
+
+#endif
