1ae699dd8776362268773f4d87d7d8992f4667b5
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-04-19 18:12:30 +0000

    COMP: Need stdio.h for EOF. Instead set default to a better DICOM one

--- Source/Common/gdcmString.h
+++ Source/Common/gdcmString.h
@@ -30,7 +30,7 @@ template <char TDelimiter, unsigned int TMaxLength, char TPadChar> std::istream&
  *      TMaxLength is only a hint. Noone actually respect the max length
  *      TPadChar is the string padding (0 or space)
  */
-template <char TDelimiter = EOF, unsigned int TMaxLength = 64, char TPadChar = ' '>
+template <char TDelimiter = '\\', unsigned int TMaxLength = 64, char TPadChar = ' '>
 class /*GDCM_EXPORT*/ String : public std::string /* PLEASE do not export me */
 {
   // UI wants \0 for pad character, while ASCII ones wants space char... do not allow anything else
