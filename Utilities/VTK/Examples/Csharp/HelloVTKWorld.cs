b6142b11e3f75ae35e049cdacaa1e71499ddc2ef
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-04-30 12:36:28 +0000

    ENH: prepend with swig for the internals c++ binding layer

---
+++ Utilities/VTK/Examples/Csharp/HelloVTKWorld.cs
@@ -0,0 +1,39 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+using System;
+using vtkgdcmswig;
+using Kitware.VTK;
+
+namespace vtkgdcm
+{
+  public class vtkGDCMImageReader : vtkMedicalImageReader2
+    {
+    vtkGDCMImageReader() {}
+    }
+}
+
+public class HelloVTKWorld
+{
+  public static int Main(string[] args)
+    {
+    string filename = args[0];
+    vtkGDCMImageReader reader = new vtkGDCMImageReader();
+    reader.SetFileName( filename );
+    reader.Update();
+
+    return 0;
+    }
+}
+
