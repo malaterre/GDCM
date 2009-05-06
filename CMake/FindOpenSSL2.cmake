858775c82ed1e51f6e588f4decd7e7874ed726dd
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-05-06 09:00:05 +0000

    fix on win32

--- CMake/FindOpenSSL2.cmake
+++ CMake/FindOpenSSL2.cmake
@@ -29,16 +29,24 @@ IF(WIN32 AND MSVC)
    # others, the libnames have to change here too
    # use also ssl and ssleay32 in debug as fallback for openssl < 0.9.8b
 
-   FIND_LIBRARY(SSL_EAY_DEBUG NAMES ssleay32MDd ssl ssleay32
-  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/bin"
+   FIND_LIBRARY(LIB_EAY_DEBUG NAMES libeay32MDd libeay32
+  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/lib/VC"
 )
+    FIND_LIBRARY(LIB_EAY_RELEASE NAMES libeay32MD libeay32
+  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/lib/VC"
+)
+   # FIND_LIBRARY(SSL_EAY_DEBUG NAMES  ssleay32
+   FIND_LIBRARY(SSL_EAY_DEBUG NAMES ssleay32MDd ssl ssleay32
+  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/lib/VC"
+)
+   #FIND_LIBRARY(SSL_EAY_RELEASE NAMES  ssleay32
    FIND_LIBRARY(SSL_EAY_RELEASE NAMES ssleay32MD ssl ssleay32
-  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/bin"
+  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/lib/VC"
 )
 
    IF(MSVC_IDE)
       IF(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
-         SET(OPENSSL_LIBRARIES optimized ${SSL_EAY_RELEASE} debug ${SSL_EAY_DEBUG})
+         SET(OPENSSL_LIBRARIES optimized ${SSL_EAY_RELEASE} ${LIB_EAY_RELEASE} debug ${SSL_EAY_DEBUG} ${LIB_EAY_DEBUG})
       ELSE(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
          SET(OPENSSL_LIBRARIES NOTFOUND)
          MESSAGE(STATUS "Could not find the debug and release version of openssl")
@@ -55,7 +63,7 @@ IF(WIN32 AND MSVC)
 ELSE(WIN32 AND MSVC)
 
    FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssl ssleay32 ssleay32MD 
-  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/bin"
+#  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;Inno Setup: App Path]/bin"
 )
 
 ENDIF(WIN32 AND MSVC)
