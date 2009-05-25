9ce3ef7a3d567fe4abb0b92917981576d577fd34
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-05-25 15:39:16 +0000

    cleanup dstor

--- Source/MediaStorageAndFileFormat/gdcmSimpleSubjectWatcher.cxx
+++ Source/MediaStorageAndFileFormat/gdcmSimpleSubjectWatcher.cxx
@@ -62,76 +62,34 @@ SimpleSubjectWatcher::SimpleSubjectWatcher(Subject *s, const char *comment):m_Su
 
 SimpleSubjectWatcher::~SimpleSubjectWatcher()
 {
-//  // Remove any observers we have on the old process object
-//  if (m_Process)
-//    {
-//    if (m_StartFilterCommand)
-//      {
-//      m_Process->RemoveObserver(m_StartTag);
-//      }
-//    if (m_EndFilterCommand)
-//      {
-//      m_Process->RemoveObserver(m_EndTag);
-//      }
-//    if (m_ProgressFilterCommand)
-//      {
-//      m_Process->RemoveObserver(m_ProgressTag);
-//      }
-//    if (m_IterationFilterCommand)
-//      {
-//      m_Process->RemoveObserver(m_IterationTag);
-//      }
-//    if (m_AbortFilterCommand)
-//      {
-//      m_Process->RemoveObserver(m_AbortTag);
-//      }
-//    }
-//  
-//  // Initialize state
-//  m_TimeProbe = watch.m_TimeProbe;
-//  m_Process = watch.m_Process;
-//  m_Steps = watch.m_Steps;
-//  m_Comment = watch.m_Comment;
-//  m_TestAbort = watch.m_TestAbort;
-//  m_Quiet = watch.m_Quiet;
-//
-//  m_StartTag = 0;
-//  m_EndTag = 0;
-//  m_ProgressTag = 0;
-//  m_IterationTag = 0;
-//  m_AbortTag = 0;
-//  
-//  // Create a series of commands
-//  if (m_Process)
-//    {
-//    m_StartFilterCommand =      CommandType::New();
-//    m_EndFilterCommand =        CommandType::New();
-//    m_ProgressFilterCommand =   CommandType::New();
-//    m_IterationFilterCommand =  CommandType::New();
-//    m_AbortFilterCommand =      CommandType::New();
-//    
-//    // Assign the callbacks
-//    m_StartFilterCommand->SetCallbackFunction(this,
-//                                        &SimpleFilterWatcher::StartFilter);
-//    m_EndFilterCommand->SetCallbackFunction(this,
-//                                        &SimpleFilterWatcher::EndFilter);
-//    m_ProgressFilterCommand->SetCallbackFunction(this,
-//                                        &SimpleFilterWatcher::ShowProgress);
-//    m_IterationFilterCommand->SetCallbackFunction(this,
-//                                        &SimpleFilterWatcher::ShowIteration);
-//    m_AbortFilterCommand->SetCallbackFunction(this,
-//                                        &SimpleFilterWatcher::ShowAbort);
-//
-//
-//    // Add the commands as observers
-//    m_StartTag = m_Process->AddObserver(StartEvent(), m_StartFilterCommand);
-//    m_EndTag = m_Process->AddObserver(EndEvent(), m_EndFilterCommand);
-//    m_ProgressTag
-//      = m_Process->AddObserver(ProgressEvent(), m_ProgressFilterCommand);
-//    m_IterationTag
-//      = m_Process->AddObserver(IterationEvent(), m_IterationFilterCommand);
-//    m_AbortTag = m_Process->AddObserver(AbortEvent(), m_AbortFilterCommand);
-//    }
+  // Remove any observers we have on the old process object
+  if (m_Subject)
+    {
+    if (m_StartFilterCommand)
+      {
+      m_Subject->RemoveObserver(m_StartTag);
+      }
+    if (m_EndFilterCommand)
+      {
+      m_Subject->RemoveObserver(m_EndTag);
+      }
+    if (m_ProgressFilterCommand)
+      {
+      m_Subject->RemoveObserver(m_ProgressTag);
+      }
+    if (m_IterationFilterCommand)
+      {
+      m_Subject->RemoveObserver(m_IterationTag);
+      }
+    if (m_AbortFilterCommand)
+      {
+      m_Subject->RemoveObserver(m_AbortTag);
+      }
+    if (m_AnonymizeFilterCommand)
+      {
+      m_Subject->RemoveObserver(m_AnonymizeTag);
+      }
+    }
 }
 
 void SimpleSubjectWatcher::StartFilter()
