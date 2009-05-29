3f19eb1dfa0f38d107eb1e025bc78eb7342918bd
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-05-29 16:18:22 +0000

    ENH: adding all kind of example... they are all working

---
+++ Utilities/VTK/Examples/Csharp/HelloActiviz4.cs
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+using Kitware.VTK;
+using vtkgdcm;
+
+/*
+ * $ MONO_PATH=/home/mmalaterre/Software/ActiViz.NET-5.4.0.455-Linux-x86_64-Personal/bin/ LD_LIBRARY_PATH=/home/mmalaterre/Software/ActiViz.NET-5.4.0.455-Linux-x86_64-Personal/bin/:/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/ mono ./bin/HelloActiviz4.exe ~/Creatis/gdcmData/test.acr
+ *
+ */
+public class HelloActiviz4
+{
+  public static int Main(string[] args)
+    {
+    string filename = args[0];
+
+    vtkGDCMImageReader reader = new vtkGDCMImageReader();
+    vtkStringArray array = vtkStringArray.New();
+    array.InsertNextValue(filename);
+
+    reader.SetFileNames(array);
+    reader.Update();
+
+    //System.Console.Write(reader.GetOutput());
+
+    vtkRenderWindowInteractor iren = vtkRenderWindowInteractor.New();
+
+    vtkImageViewer viewer = vtkImageViewer.New();
+    viewer.SetInput(reader.GetOutput());
+    viewer.SetupInteractor(iren);
+    viewer.SetSize(600, 600);
+    viewer.Render();
+
+    iren.Initialize();
+    iren.Start();
+
+    return 0;
+    }
+}
+
---
+++ Utilities/VTK/Examples/Csharp/HelloActiviz5.cs
@@ -0,0 +1,68 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+using Kitware.VTK;
+using vtkgdcm;
+
+/*
+ * $ MONO_PATH=/home/mmalaterre/Software/ActiViz.NET-5.4.0.455-Linux-x86_64-Personal/bin/ LD_LIBRARY_PATH=/home/mmalaterre/Software/ActiViz.NET-5.4.0.455-Linux-x86_64-Personal/bin/:/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/ mono ./bin/HelloActiviz5.exe ~/Creatis/gdcmData/test.acr
+ *
+ */
+public class HelloActiviz5
+{
+  public static int Main(string[] args)
+    {
+    string filename = args[0];
+
+    vtkGDCMImageReader reader = new vtkGDCMImageReader();
+    vtkStringArray array = vtkStringArray.New();
+    array.InsertNextValue(filename);
+    reader.SetFileNames(array);
+    reader.Update();
+
+    //System.Console.Write(reader.GetOutput());
+
+    vtkRenderWindowInteractor iren = vtkRenderWindowInteractor.New();
+
+    vtkRenderer ren1 = vtkRenderer.New();
+    vtkRenderWindow renWin = vtkRenderWindow.New();
+    renWin.AddRenderer(ren1);
+
+    vtkImageActor actor = vtkImageActor.New();
+
+    vtkImageMapToWindowLevelColors coronalColors = vtkImageMapToWindowLevelColors.New();
+    coronalColors.SetInput(reader.GetOutput());
+
+    //System.Console.Write(reader.GetOutput());
+    actor.SetInput(coronalColors.GetOutput());
+
+    vtkCamera aCamera = vtkCamera.New();
+    aCamera.SetViewUp (0, 0, -1);
+    aCamera.SetPosition (0, 1, 0);
+    aCamera.SetFocalPoint (0, 0, 0);
+    aCamera.ComputeViewPlaneNormal();
+
+    ren1.AddActor(actor);
+    iren.SetRenderWindow(renWin);
+    ren1.SetActiveCamera(aCamera);
+    ren1.Render();
+    ren1.ResetCamera();
+
+    iren.Initialize();
+    iren.Start();
+
+    return 0;
+    }
+}
+
