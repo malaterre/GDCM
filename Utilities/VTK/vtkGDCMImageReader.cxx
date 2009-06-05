580d4806c1c55fb63837c7caa6e450f9269262de
Rogue Research <rogueresearch@users.sourceforge.net>
2009-06-05 15:53:04 +0000

    replaced abort() with assert(0)

--- Utilities/VTK/vtkGDCMImageReader.cxx
+++ Utilities/VTK/vtkGDCMImageReader.cxx
@@ -847,7 +847,7 @@ void InPlaceYFlipImage(vtkImageData* data)
 #endif
     default:
       //vtkErrorMacro("do not support scalar type: " << data->GetScalarType() );
-      abort();
+      assert(0);
     }
   outsize *= (dext[1] - dext[0] + 1);
   char * ref = static_cast<char*>(data->GetScalarPointer());
