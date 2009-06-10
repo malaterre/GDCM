03fee5d00280c69f30a29b24935616a1eeb57208
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-06-10 13:38:32 +0000

    java wrap

---
+++ Utilities/VTK/Examples/Java/HelloVTKWorld.java
@@ -0,0 +1,84 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// We are required to call the package 'vtk' eventhough I (MM) would have prefered
+// an import statement along the line of:
+// import vtkgdcm.*;
+import vtk.*;
+
+/*
+ * Compilation:
+ * $ CLASSPATH=/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/vtkgdcm.jar:/home/mmalaterre/Projects/VTK54-gcc/bin/vtk.jar javac HelloVTKWorld.java 
+ *
+ * Usage:
+ * LD_LIBRARY_PATH=/usr/lib/jvm/java-6-sun-1.6.0.07/jre/lib/amd64/xawt/:/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/:/home/mmalaterre/Projects/VTK54-gcc/bin/ CLASSPATH=/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/vtkgdcm.jar:/home/mmalaterre/Projects/VTK54-gcc/bin/vtk.jar:. java HelloVTKWorld ~/Creatis/gdcmData/012345.002.050.dcm out.dcm
+ *
+ */
+public class HelloVTKWorld
+{
+  static { 
+    System.loadLibrary("vtkCommonJava"); 
+    System.loadLibrary("vtkFilteringJava"); 
+    System.loadLibrary("vtkIOJava"); 
+    System.loadLibrary("vtkImagingJava"); 
+    System.loadLibrary("vtkGraphicsJava"); 
+    System.loadLibrary("vtkRenderingJava"); 
+    System.loadLibrary("vtkgdcmJava"); 
+    try {
+      System.loadLibrary("vtkHybridJava");
+    } catch (Throwable e) {
+      System.out.println("cannot load vtkHybrid, skipping...");
+    }
+    try {
+      System.loadLibrary("vtkVolumeRenderingJava");
+    } catch (Throwable e) {
+      System.out.println("cannot load vtkVolumeRendering, skipping...");
+    }
+  }
+
+  public static void main(String[] args)
+    {
+    String filename = args[0];
+    vtkGDCMImageReader reader = new vtkGDCMImageReader();
+    reader.SetFileName( filename );
+    reader.Update();
+
+    vtkMedicalImageProperties prop = reader.GetMedicalImageProperties();
+    System.out.println( prop.GetPatientName() ); // 
+
+//    if( reader.GetImageFormat() == vtkgdcm.vtkgdcm.VTK_LUMINANCE ) // MONOCHROME2
+//      {
+//      System.out.println( "Image is MONOCHROME2" ); // 
+//      }
+
+    // Just for fun, invert the direction cosines, output should reflect that:
+    vtkMatrix4x4 dircos = reader.GetDirectionCosines();
+    dircos.Invert();
+
+    String outfilename = args[1];
+    vtkGDCMImageWriter writer = new vtkGDCMImageWriter();
+    writer.SetMedicalImageProperties( reader.GetMedicalImageProperties() );
+    writer.SetDirectionCosines( dircos );
+    writer.SetShift( reader.GetShift() );
+    writer.SetScale( reader.GetScale() );
+    writer.SetImageFormat( reader.GetImageFormat() );
+    writer.SetFileName( outfilename );
+    //writer.SetInputConnection( reader.GetOutputPort() ); // new
+    writer.SetInput( reader.GetOutput() ); // old
+    writer.Write();
+
+    System.out.println("Success reading: " + filename );
+    }
+}
+
