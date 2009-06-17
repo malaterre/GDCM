2cd138dfbb721a52bb3e1b6f52a3450999a7bde4
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-06-17 14:09:38 +0000

    style

--- Examples/Cxx/ConvertToQImage.cxx
+++ Examples/Cxx/ConvertToQImage.cxx
@@ -21,7 +21,7 @@
  * http://doc.trolltech.com/4.5/qimage.html
  *
  * Usage:
- *  ConvertToQImage gdcmData/012345.002.050.dcm
+ *  ConvertToQImage gdcmData/012345.002.050.dcm output.png
 
  * Thanks:
  *   Sylvain ADAM (sylvain51 hotmail com) for contributing this example
@@ -29,71 +29,119 @@
 
 #include "gdcmImageReader.h"
 #include <QImage>
+#include <QImageWriter>
 
-int main(int, char *[])
+bool ConvertToFormat_RGB888(gdcm::Image const & gimage, char *buffer, QImage* &imageQt)
 {
+  const unsigned int* dimension = gimage.GetDimensions();
+
+  unsigned int dimX = dimension[0];
+  unsigned int dimY = dimension[1];
+
+  gimage.GetBuffer(buffer);
+
+  // Let's start with the easy case:
+  if( gimage.GetPhotometricInterpretation() == gdcm::PhotometricInterpretation::RGB )
+    {
+    if( gimage.GetPixelFormat() != gdcm::PixelFormat::UINT8 )
+      {
+      return false;
+      }
+    unsigned char *ubuffer = (unsigned char*)buffer;
+    // QImage::Format_RGB888	13	The image is stored using a 24-bit RGB format (8-8-8).
+    imageQt = new QImage(ubuffer, dimX, dimY, QImage::Format_RGB888);
+    }
+  else if( gimage.GetPhotometricInterpretation() == gdcm::PhotometricInterpretation::MONOCHROME2 )
+    {
+    if( gimage.GetPixelFormat() == gdcm::PixelFormat::UINT8 )
+      {
+      // We need to copy each individual 8bits into R / G and B:
+      unsigned char *ubuffer = new unsigned char[dimX*dimY*3];
+      unsigned char *pubuffer = ubuffer;
+      for(int i = 0; i < dimX*dimY; i++)
+        {
+        *pubuffer++ = *buffer;
+        *pubuffer++ = *buffer;
+        *pubuffer++ = *buffer++;
+        }
+
+      imageQt = new QImage(ubuffer, dimX, dimY, QImage::Format_RGB888);
+      }
+    else if( gimage.GetPixelFormat() == gdcm::PixelFormat::INT16 )
+      {
+      // We need to copy each individual 16bits into R / G and B (truncate value)
+      short *buffer16 = (short*)buffer;
+      unsigned char *ubuffer = new unsigned char[dimX*dimY*3];
+      unsigned char *pubuffer = ubuffer;
+      for(int i = 0; i < dimX*dimY; i++)
+        {
+        // Scalar Range of gdcmData/012345.002.050.dcm is [0,192], we could simply do:
+        // *pubuffer++ = *buffer16;
+        // *pubuffer++ = *buffer16;
+        // *pubuffer++ = *buffer16;
+        *pubuffer++ = *buffer16 / 256;
+        *pubuffer++ = *buffer16 / 256;
+        *pubuffer++ = *buffer16 / 256;
+        buffer16++;
+        }
+
+      imageQt = new QImage(ubuffer, dimX, dimY, QImage::Format_RGB888);
+      }
+    else
+      {
+      std::cerr << "Pixel Format is: " << gimage.GetPixelFormat() << std::endl;
+      return false;
+      }
+    }
+  else
+    {
+    std::cerr << "Unhandled PhotometricInterpretation: " << gimage.GetPhotometricInterpretation() << std::endl;
+    return false;
+    }
+
+  return true;
+}
+
+int main(int argc, char *argv[])
+{
+  if( argc < 2 )
+    {
+    return 1;
+    }
+  const char *filename = argv[1];
+  const char *outfilename = argv[2];
 
   gdcm::ImageReader ir;
-  ir.SetFileName("wedges.ima");
-
+  ir.SetFileName( filename );
   if(!ir.Read())
     {
     //Read failed
+    return 1;
     }
 
   std::cout<<"Getting image from ImageReader..."<<std::endl;
 
-  gdcm::Image &gimage = ir.GetImage();
-
-  const unsigned int* dimension = gimage.GetDimensions();
-
-  int dimX = int(dimension[0]);
-  int dimY = int(dimension[1]);
-
+  const gdcm::Image &gimage = ir.GetImage();
   //This buffer has been declared elsewhere
   char *buffer = new char[gimage.GetBufferLength()];
 
-  gimage.GetBuffer(buffer);
 
-  //Let's now convert each 16-bits pixel to a 8-bits pixel, leveled to the maximum intensity value of the image
-  //Because the image is a 16-bits grayscale, we thus have 65536 available values to store the gray value.
-  // BUT, usually, the value doesn't oversize around 2000~2200. So if we directly convert the 16-bits to 8-bits (from max. 65536 to max255)
-  // and try to display it, we will have very dark images. Then, we will level all the intensities to the maximum intensity value.
-  //We are gonna use a RGB888 format for Qt display. In a grayscale image, R=G=B = the gray value.
-  //The buffer length, in char, will thus be: 256*256*3 (if the image is 256x256) = 196608 chars.
-
-  unsigned short *buffer16 = new unsigned short [dimX*dimY];
-  unsigned short maxValue = 0;
-
-  for(unsigned int i = 0; i < gimage.GetBufferLength(); i+=2)
+  QImage *imageQt = NULL;
+  if( !ConvertToFormat_RGB888( gimage, buffer, imageQt ) )
     {
-    //@todo: think about #typedef to avoid these long conversions lines.
-    //Bitwise left shift and bitwise OR
-    unsigned short pixel16bitValue = ((((unsigned short ) ((unsigned char) buffer [i+1]))<<8)|(unsigned char) buffer [i]);
-
-    buffer16[i/2] = pixel16bitValue;
-    if(pixel16bitValue > maxValue)
-      {
-      maxValue = pixel16bitValue;
-      }
+    return 1;
     }
 
-  unsigned char *buffer888 = new unsigned char[dimX*dimY*3];
 
-  for(int i = 0; i < dimX*dimY; i++)
+  QImageWriter writer;
+  writer.setFormat("png");
+  writer.setFileName( outfilename );
+  if( !writer.write( *imageQt ) )
     {
-    unsigned char pixel8bitValue = (unsigned char) (buffer16[i]*255/maxValue);
-    buffer888[i*3] = pixel8bitValue;
-    buffer888[i*3+1] = pixel8bitValue;
-    buffer888[i*3+2] = pixel8bitValue;
+    return 1;
     }
 
-  QImage *imageQt = new QImage(buffer888, dimX, dimY, QImage::Format_RGB888);
+//  delete[] buffer;
 
-  //The DICOM image is a 16-bits image
-  //For a 256x256 image, it's 256x256 = 65536 pixels.
-  //So, normally in a char* (1 char = 1 byte = 1 bit), it does have a length of 65536
-  //BUT as it's a 16-bits image (2*8-bits), we thus have 131072 chars
-  // ============================================================================
   return 0;
 }
