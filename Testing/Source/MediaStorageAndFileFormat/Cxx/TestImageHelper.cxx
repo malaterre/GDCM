225f86aa9d269136a21992ddf767e168c21c2c2d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-10-08 14:07:08 +0000

    update from Daniele input

--- Testing/Source/MediaStorageAndFileFormat/Cxx/TestImageHelper.cxx
+++ Testing/Source/MediaStorageAndFileFormat/Cxx/TestImageHelper.cxx
@@ -13,6 +13,9 @@
 
 =========================================================================*/
 #include "gdcmImageHelper.h"
+#include "gdcmMediaStorage.h"
+#include "gdcmDataSet.h"
+#include "gdcmAttribute.h"
 
 int TestImageHelper(int, char *[])
 {
@@ -31,6 +34,56 @@ int TestImageHelper(int, char *[])
   std::cout << spacing[0] << std::endl;
   std::cout << spacing[1] << std::endl;
   std::cout << spacing[2] << std::endl;
+
+
+  std::vector<double> dircos;
+  // make it an invalid call
+  dircos.resize(6);
+  dircos[0] = 1;
+  dircos[1] = 0;
+  dircos[2] = 0;
+  dircos[3] = 1;
+  dircos[4] = 0;
+  dircos[5] = 0;
+  // Try SC
+{
+  gdcm::MediaStorage ms( gdcm::MediaStorage::SecondaryCaptureImageStorage );
+  gdcm::DataSet ds;
+
+    gdcm::DataElement de( gdcm::Tag(0x0008, 0x0016) );
+    const char* msstr = gdcm::MediaStorage::GetMSString(ms);
+    de.SetByteValue( msstr, strlen(msstr) );
+    de.SetVR( gdcm::Attribute<0x0008, 0x0016>::GetVR() );
+    ds.Insert( de );
+
+  // Since SC this is a no-op
+  gdcm::ImageHelper::SetDirectionCosinesValue( ds, dircos );
+
+  // previous call removed the attribute
+  gdcm::Tag iop(0x0020,0x0037);
+  if(  ds.FindDataElement( iop ) ) return 1;
+}
+
+  // MR now
+{
+  gdcm::MediaStorage ms( gdcm::MediaStorage::MRImageStorage );
+  gdcm::DataSet ds;
+
+    gdcm::DataElement de( gdcm::Tag(0x0008, 0x0016) );
+    const char* msstr = gdcm::MediaStorage::GetMSString(ms);
+    de.SetByteValue( msstr, strlen(msstr) );
+    de.SetVR( gdcm::Attribute<0x0008, 0x0016>::GetVR() );
+    ds.Insert( de );
+
+  // Since SC this is a no-op
+  gdcm::ImageHelper::SetDirectionCosinesValue( ds, dircos );
+
+  // previous call should not have executed anything
+  gdcm::Tag iop(0x0020,0x0037);
+  if(  ds.FindDataElement( iop ) ) return 1;
+
+  // TODO: need to check DirectionCosines .IsValid()
+} 
   
   return 0;
 }
