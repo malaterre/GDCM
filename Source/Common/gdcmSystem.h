d343df39f5489c83415bed938cab98870ec12502
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-11-19 17:44:19 +0000

    style

--- Source/Common/gdcmSystem.h
+++ Source/Common/gdcmSystem.h
@@ -28,7 +28,7 @@ class GDCM_EXPORT System
 {
 public:
   /// Create a directory
-  static bool MakeDirectory(const char *path); 
+  static bool MakeDirectory(const char *path);
   /// Check whether the specified file exist on the sytem
   static bool FileExists(const char* filename);
   /// Check whether the file specified is a directory:
