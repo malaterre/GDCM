1320c73070d35748505f21b3533f1c2ec5b3d44c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-11-25 14:28:46 +0000

    coverage

---
+++ Testing/Source/MediaStorageAndFileFormat/Cxx/TestFileDerivation.cxx
@@ -0,0 +1,22 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmFileDerivation.h"
+
+int TestFileDerivation(int, char *[])
+{
+  gdcm::FileDerivation fd;
+  return 0;
+}
+
