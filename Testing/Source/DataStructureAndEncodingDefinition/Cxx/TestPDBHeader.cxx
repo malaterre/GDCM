b323b5e34ca9789727d2e01bc1e6d4e75763408e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-11-25 14:33:32 +0000

    coverage

---
+++ Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestPDBHeader.cxx
@@ -0,0 +1,23 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmPDBHeader.h"
+
+int TestPDBHeader(int , char *[])
+{
+  gdcm::PDBHeader pdb;
+  (void)pdb;
+  return 0;
+}
+
