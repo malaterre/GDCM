2fd044365596eb3990bcceb83f50bbb2c7e73492
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-11-30 15:42:45 +0000

    Update test to actually do the right thing

--- Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestLCNumeric.cxx
+++ Testing/Source/DataStructureAndEncodingDefinition/Cxx/TestLCNumeric.cxx
@@ -23,29 +23,53 @@ int TestLCNumeric(int argc, char *argv[])
   //setenv("LC_NUMERIC", "fr_FR", 1);
   //const char ss[] = "LC_NUMERIC=fr_FR";
   //setlocale(LC_NUMERIC,"C");
+
+  // Seems to only affect the local sscanf
   char *l = setlocale(LC_NUMERIC,"fr_FR.UTF-8");
   if( !l )
     {
     std::cerr << "Could not set LC_NUMERIC" << std::endl;
     return 1;
     }
-  //char *copy = strdup(ss);
-  //putenv(copy);
-  //free(copy);
-  std::ostringstream os;
-  double d = 1.2;
-  os << d;
-  std::string s = os.str();
-  std::cout << "s:" << s << std::endl;
-  std::string::size_type pos_comma = s.find( "," );
-  if( pos_comma != std::string::npos )
+
+  float a = 1. / 3;
+
+  printf("Float: %f\n", a );
+
+  // The following affect all ostringstream
+  try 
     {
-    return 1;
+    //std::locale b = std::locale( "fr_FR" );
+    std::locale::global( std::locale( "fr_FR.UTF-8" ) ) ;
+
+    //char *copy = strdup(ss);
+    //putenv(copy);
+    //free(copy);
+    std::ostringstream os;
+    os.imbue(std::locale::classic());
+    double d = 1.2;
+    os << d;
+    std::string s = os.str();
+    std::cout << "s:" << s << std::endl;
+    std::string::size_type pos_comma = s.find( "," );
+    if( pos_comma != std::string::npos )
+      {
+      std::cerr << "We found the comma symbol" << std::endl;
+      return 1;
+      }
+    std::string::size_type pos_dot = s.find( "." );
+    if( pos_dot == std::string::npos )
+      {
+      std::cerr << "We did not found the dot symbol" << std::endl;
+      return 1;
+      }
     }
-  std::string::size_type pos_dot = s.find( "." );
-  if( pos_dot == std::string::npos )
+  catch(std::exception &ex)
     {
-    return 1;
+    // ok something went wrong when setting up fr_FR locale.
+    // just ignore for now
+    std::cerr << "What: " << ex.what() << std::endl;
     }
+
   return 0;
 }
