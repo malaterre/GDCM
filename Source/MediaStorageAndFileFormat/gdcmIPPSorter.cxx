895df7a31e844498d234825e25ac0e2a7ad6d788
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-12-04 09:13:18 +0000

    Make the IPPSorter discard non-DICOM file (I think this is ok). Also discard any DICOM file not containing IPP value (think DICOMDIR maybe...)

--- Source/MediaStorageAndFileFormat/gdcmIPPSorter.cxx
+++ Source/MediaStorageAndFileFormat/gdcmIPPSorter.cxx
@@ -128,28 +128,36 @@ bool IPPSorter::Sort(std::vector<std::string> const & filenames)
   for(; it != filenames.end(); ++it)
     {
     const char *filename = it->c_str();
-    const char *value =  scanner.GetValue(filename, ipp);
-    if( value )
+    bool iskey = scanner.IsKey(filename);
+    if( iskey )
       {
-      //gdcmDebugMacro( filename << " has " << ipp << " = " << value );
-      Element<VR::DS,VM::VM3> ipp;
-      std::stringstream ss;
-      ss.str( value );
-      ipp.Read( ss );
-      double dist = 0;
-      for (int i = 0; i < 3; ++i) dist += normal[i]*ipp[i];
-      // FIXME: This test is weak, since implicitely we are doing a != on floating point value
-      if( sorted.find(dist) != sorted.end() )
+      const char *value =  scanner.GetValue(filename, ipp);
+      if( value )
         {
-        gdcmDebugMacro( "dist: " << dist << " already found" );
-        return false;
+        //gdcmDebugMacro( filename << " has " << ipp << " = " << value );
+        Element<VR::DS,VM::VM3> ipp;
+        std::stringstream ss;
+        ss.str( value );
+        ipp.Read( ss );
+        double dist = 0;
+        for (int i = 0; i < 3; ++i) dist += normal[i]*ipp[i];
+        // FIXME: This test is weak, since implicitely we are doing a != on floating point value
+        if( sorted.find(dist) != sorted.end() )
+          {
+          gdcmDebugMacro( "dist: " << dist << " already found" );
+          return false;
+          }
+        sorted.insert(
+          SortedFilenames::value_type(dist,filename) );
         }
-      sorted.insert(
-            SortedFilenames::value_type(dist,filename) );
+      else
+        {
+        gdcmDebugMacro( "File: " << filename << " has no Tag" << ipp << ". Skipping." );
+        }
       }
     else
       {
-      assert(0);
+      gdcmDebugMacro( "File: " << filename << " could not be read. Skipping." );
       }
     }
 }
