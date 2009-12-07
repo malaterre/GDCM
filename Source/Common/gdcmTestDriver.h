8737d843ffc12a7b9a48bd0a609571de39dc2127
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-12-07 17:12:48 +0000

    Work toward getting lc_numeric fixed in GDCM once for all...

---
+++ Source/Common/gdcmTestDriver.h
@@ -0,0 +1,25 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2009 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// This header is included by all the C++ test drivers in GDCM.
+#ifndef __gdcmTestDriver_h
+#define __gdcmTestDriver_h
+
+// CREATE_TEST_SOURCELIST supports the flag EXTRA_INCLUDE but only one per call.
+// So there is no way to specify we want to include two files... instead
+// gather the #include in a single file and include that one...
+#include <clocale> // C setlocale()
+#include <locale> // C++ locale
+
+#endif // __gdcmTestDriver_h
