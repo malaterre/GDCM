23f92b51595ba9f6f651437c9d7c603a75bb85f0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-12-16 21:17:55 +0000

    Better test

--- Testing/Source/MediaStorageAndFileFormat/Cxx/TestSplitMosaicFilter.cxx
+++ Testing/Source/MediaStorageAndFileFormat/Cxx/TestSplitMosaicFilter.cxx
@@ -13,9 +13,53 @@
 
 =========================================================================*/
 #include "gdcmSplitMosaicFilter.h"
+#include "gdcmTesting.h"
+#include "gdcmSystem.h"
+#include "gdcmImageReader.h"
 
 int TestSplitMosaicFilter(int, char *[])
 {
   gdcm::SplitMosaicFilter s;
+
+  const char *extradataroot = gdcm::Testing::GetDataExtraRoot();
+  if( !extradataroot )
+    {
+    return 1;
+    }
+  if( !gdcm::System::FileIsDirectory(extradataroot) )
+    {
+    std::cerr << "No such directory: " << extradataroot <<  std::endl;
+    return 1;
+    }
+
+  std::string filename = extradataroot;
+  filename += "/gdcmSampleData/images_of_interest/MR-sonata-3D-as-Tile.dcm";
+
+  // std::cout << filename << std::endl;
+  if( !gdcm::System::FileExists(filename.c_str()) )
+    {
+    return 1;
+    }
+
+  gdcm::ImageReader reader;
+  reader.SetFileName( filename.c_str() );
+  if( !reader.Read() )
+    {
+    std::cerr << "could not read: " << filename << std::endl;
+    return 1;
+    }
+
+  gdcm::SplitMosaicFilter filter;
+  filter.SetImage( reader.GetImage() );
+  filter.SetFile( reader.GetFile() );
+  bool b = filter.Split();
+  if( !b )
+    {
+    std::cerr << "Could not split << " << filename << std::endl;
+    return 1;
+    }
+
+  const gdcm::Image &image = filter.GetImage();
+
   return 0;
 }
