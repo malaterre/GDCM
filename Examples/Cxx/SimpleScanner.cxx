fb6f351b637c02e563b5d54762456f7944b6a675
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-04-07 12:45:14 +0000

    simple example

---
+++ Examples/Cxx/SimpleScanner.cxx
@@ -0,0 +1,93 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2010 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/* 
+ * Simple example to show how to use Scanner API.
+ *
+ * ./SimpleScanner gdcmData/012345.002.050.dcm
+ */
+
+#include "gdcmScanner.h"
+
+int main(int argc, char *argv[])
+{
+  if( argc < 2 )
+    {
+    return 1;
+    }
+  const char *filename = argv[1];
+  const char filename_invalid[] = "this is a file that may not exist on this disk.dcm";
+
+  gdcm::Scanner s;
+  
+  const gdcm::Tag tag_array[] = {
+    gdcm::Tag(0x8,0x50),
+    gdcm::Tag(0x8,0x51),
+    gdcm::Tag(0x8,0x60),
+  };
+  s.AddTag( tag_array[0] );
+  s.AddTag( tag_array[1] );
+  s.AddTag( tag_array[2] );
+
+  gdcm::Directory::FilenamesType filenames;
+  filenames.push_back( filename );
+  filenames.push_back( filename_invalid );
+
+  if( !s.Scan( filenames ) )
+    {
+    return 1;
+    }
+
+  //s.Print( std::cout );
+
+
+  if( s.IsKey( filename ) )
+    {
+    std::cout << "INFO:" << filename << " is a proper Key for the Scanner (this is a DICOM file)" << std::endl;
+    }
+
+  if( !s.IsKey( filename_invalid ) )
+    {
+    std::cout << "INFO:" << filename_invalid << " is not a proper Key for the Scanner (this is either not a DICOM file or file does not exist)" << std::endl;
+    }
+
+  gdcm::Scanner::TagToValue const &ttv = s.GetMapping(filename);
+
+  const gdcm::Tag *ptag = tag_array;
+  for( ; ptag != tag_array + 3; ++ptag )
+    {
+    //std::cout << *ptag << std::endl;
+    if( ttv.find( *ptag ) != ttv.end() )
+      {
+      std::cout << *ptag << " was properly found in this file" << std::endl;
+      gdcm::Scanner::TagToValue::const_iterator it = ttv.find( *ptag );
+      // assert( *patg == it->first );
+      const char *value = it->second;
+      if( *value )
+        {
+        std::cout << "  It has the value: " << value << std::endl;
+        }
+      else
+        {
+        std::cout << "  It has no value (empty)" << std::endl;
+        }
+      }
+    else
+      {
+      std::cout << "Sorry " << *ptag << " could not be found in this file" << std::endl;
+      }
+    }
+
+  return 0;
+}
