36d39d20c231d5c00c588fb03856e80faea80c2e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-06-09 15:32:53 +0000

    Add 3d support

--- Source/MediaStorageAndFileFormat/gdcmKAKADUCodec.cxx
+++ Source/MediaStorageAndFileFormat/gdcmKAKADUCodec.cxx
@@ -63,67 +63,154 @@ bool KAKADUCodec::Decode(DataElement const &in, DataElement &out)
   const gdcm::SequenceOfFragments *sf = in.GetSequenceOfFragments();
   if(!sf) return false;
 
-  // http://msdn.microsoft.com/en-us/library/hs3e7355.aspx
-  // -> check if tempnam needs the 'free'
-  char *tempinput  = tempnam(0, "gdcminkduexp");
-  char *tempoutput = tempnam(0, "gdcmoutkduexp");
-  if( !tempinput || !tempoutput ) 
+  if( NumberOfDimensions == 2 )
     {
-    //free(input);
-    //free(output);
+    // http://msdn.microsoft.com/en-us/library/hs3e7355.aspx
+    // -> check if tempnam needs the 'free'
+    char *tempinput  = tempnam(0, "gdcminkduexp");
+    char *tempoutput = tempnam(0, "gdcmoutkduexp");
+    if( !tempinput || !tempoutput ) 
+      {
+      //free(input);
+      //free(output);
+      return false;
+      }
+    std::string input = tempinput;
+    input += ".j2k";
+    std::string output = tempoutput;
+    output += ".rawl";
+
+    std::ofstream outfile(input.c_str(), std::ios::binary);
+    sf->WriteBuffer(outfile);
+    outfile.close(); // flush !
+
+    //gdcm::Filename fn( System::GetCurrentProcessFileName() );
+    //std::string executable_path = fn.GetPath();
+#ifdef GDCM_USE_SYSTEM_KAKADU
+    std::string kakadu_command = GDCM_KAKADU_EXPAND_EXECUTABLE;
+    kakadu_command += " -quiet";
+#else
+#error not implemented
+#endif
+    // ./bin/kakadujpeg -d -s jpeg.jpg -ci 0 out.raw  
+    kakadu_command += " -i ";
+    kakadu_command += input;
+    kakadu_command += " -o ";
+    kakadu_command += output;
+
+    //std::cerr << kakadu_command << std::endl;
+    gdcmDebugMacro( kakadu_command );
+    int ret = system(kakadu_command.c_str());
+    //std::cerr << "system: " << ret << std::endl;
+
+    size_t len = gdcm::System::FileSize(output.c_str());
+    if(!len) return false;
+
+    std::ifstream is(output.c_str());
+    char * buf = new char[len];
+    is.read(buf, len);
+    out.SetTag( gdcm::Tag(0x7fe0,0x0010) );
+    out.SetByteValue( buf, len );
+    delete[] buf;
+
+    if( !System::RemoveFile(input.c_str()) )
+      {
+      gdcmErrorMacro( "Could not delete input: " << input );
+      }
+
+    if( !System::RemoveFile(output.c_str()) )
+      {
+      gdcmErrorMacro( "Could not delete output: " << output );
+      }
+
+    free(tempinput);
+    free(tempoutput);
+    }
+  else if ( NumberOfDimensions == 3 )
+    {
+    std::stringstream os;
+    if( sf->GetNumberOfFragments() != Dimensions[2] )
+      {
+      gdcmErrorMacro( "Not handled" );
+      return false;
+      }
+
+    for(unsigned int i = 0; i < sf->GetNumberOfFragments(); ++i)
+      {
+      // http://msdn.microsoft.com/en-us/library/hs3e7355.aspx
+      // -> check if tempnam needs the 'free'
+      char *tempinput  = tempnam(0, "gdcminkduexp");
+      char *tempoutput = tempnam(0, "gdcmoutkduexp");
+      if( !tempinput || !tempoutput ) 
+        {
+        //free(input);
+        //free(output);
+        return false;
+        }
+      std::string input = tempinput;
+      input += ".j2k";
+      std::string output = tempoutput;
+      output += ".rawl";
+
+      std::ofstream outfile(input.c_str(), std::ios::binary);
+      const Fragment &frag = sf->GetFragment(i);
+      assert( !frag.IsEmpty() );
+      const ByteValue *bv = frag.GetByteValue();
+      assert( bv );
+      //sf->WriteBuffer(outfile);
+      bv->WriteBuffer( outfile );
+      outfile.close(); // flush !
+
+      //gdcm::Filename fn( System::GetCurrentProcessFileName() );
+      //std::string executable_path = fn.GetPath();
+#ifdef GDCM_USE_SYSTEM_KAKADU
+      std::string kakadu_command = GDCM_KAKADU_EXPAND_EXECUTABLE;
+      kakadu_command += " -quiet";
+#else
+#error not implemented
+#endif
+      // ./bin/kakadujpeg -d -s jpeg.jpg -ci 0 out.raw  
+      kakadu_command += " -i ";
+      kakadu_command += input;
+      kakadu_command += " -o ";
+      kakadu_command += output;
+
+      //std::cerr << kakadu_command << std::endl;
+      gdcmDebugMacro( kakadu_command );
+      int ret = system(kakadu_command.c_str());
+      //std::cerr << "system: " << ret << std::endl;
+
+      size_t len = gdcm::System::FileSize(output.c_str());
+      if(!len) return false;
+
+      std::ifstream is(output.c_str());
+      char * buf = new char[len];
+      is.read(buf, len);
+      os.write(buf, len);
+      //out.SetByteValue( buf, len );
+      delete[] buf;
+
+      if( !System::RemoveFile(input.c_str()) )
+        {
+        gdcmErrorMacro( "Could not delete input: " << input );
+        }
+      if( !System::RemoveFile(output.c_str()) )
+        {
+        gdcmErrorMacro( "Could not delete output: " << output );
+        }
+      free(tempinput);
+      free(tempoutput);
+      }
+    std::string str = os.str();
+    assert( str.size() );
+    out.SetTag( gdcm::Tag(0x7fe0,0x0010) );
+    out.SetByteValue( &str[0], str.size() );
+    }
+  else
+    {
+    gdcmErrorMacro( "Not handled" );
     return false;
     }
-  std::string input = tempinput;
-  input += ".j2k";
-  std::string output = tempoutput;
-  output += ".rawl";
-
-  std::ofstream outfile(input.c_str(), std::ios::binary);
-  sf->WriteBuffer(outfile);
-  outfile.close(); // flush !
-
-  //gdcm::Filename fn( System::GetCurrentProcessFileName() );
-  //std::string executable_path = fn.GetPath();
-#ifdef GDCM_USE_SYSTEM_KAKADU
-  std::string kakadu_command = GDCM_KAKADU_EXPAND_EXECUTABLE;
-  kakadu_command += " -quiet";
-#else
-#error bla
-#endif
-  // ./bin/kakadujpeg -d -s jpeg.jpg -ci 0 out.raw  
-  kakadu_command += " -i ";
-  kakadu_command += input;
-  kakadu_command += " -o ";
-  kakadu_command += output;
-
-  //std::cerr << kakadu_command << std::endl;
-  gdcmDebugMacro( kakadu_command );
-  int ret = system(kakadu_command.c_str());
-  //std::cerr << "system: " << ret << std::endl;
-
-  size_t len = gdcm::System::FileSize(output.c_str());
-  if(!len) return false;
-  assert( len );
-
-  std::ifstream is(output.c_str());
-  char * buf = new char[len];
-  is.read(buf, len);
-  out.SetTag( gdcm::Tag(0x7fe0,0x0010) );
-  out.SetByteValue( buf, len );
-  delete[] buf;
-
-  if( !System::RemoveFile(input.c_str()) )
-    {
-    gdcmErrorMacro( "Could not delete input: " << input );
-    }
-
-  if( !System::RemoveFile(output.c_str()) )
-    {
-    gdcmErrorMacro( "Could not delete output: " << output );
-    }
-
-  free(tempinput);
-  free(tempoutput);
 
   // FIXME:
   //LossyFlag = true;
