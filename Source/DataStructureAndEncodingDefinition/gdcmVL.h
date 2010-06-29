a598f6755ad693fb534caaff74fd464b6523fe1b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-06-29 19:46:06 +0000

    Add typedef to access internal representation

--- Source/DataStructureAndEncodingDefinition/gdcmVL.h
+++ Source/DataStructureAndEncodingDefinition/gdcmVL.h
@@ -29,6 +29,7 @@ namespace gdcm
  */
 class GDCM_EXPORT VL
 {
+  typedef uint32_t Type;
 public:
   VL(uint32_t vl = 0) : ValueLength(vl) { }
 
@@ -43,10 +44,12 @@ public:
     ValueLength = 0xFFFFFFFF;
   }
 
+  /// Return whether or not the VL is odd or not.
   bool IsOdd() const {
     return !IsUndefined() && ValueLength % 2;
   }
 
+  /// += operator
   VL& operator+=(VL const &vl) {
     ValueLength += vl.ValueLength;
     return *this;
