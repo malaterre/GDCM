94436ee6e4c8b8f046809bc003aa131467debd2f
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-10-23 16:05:55 +0200

    Add a simple example

---
+++ zorglub-gcc4-64-makefiles.cmake
@@ -0,0 +1,17 @@
+# Client maintainer: mathieu . malaterre  gmail . com
+
+set(CTEST_SITE "zorglub")
+set(CTEST_BUILD_NAME "Debian-stable-gcc4-64bit")
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_root_name "MyTests")
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 2)
+
+macro(dashboard_hook_init)
+  set( dashboard_cache "
+    GDCM_BUILD_SHARED_LIBS:BOOL=ON
+    "
+    )
+endmacro(dashboard_hook_init)
+
+include(${CTEST_SCRIPT_DIRECTORY}/gdcm_common.cmake)
