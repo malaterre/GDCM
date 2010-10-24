2d4a2658bd73e23f0a2861bbdae934dab909f11e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-10-24 10:58:11 +0200

    Adding all other ctest script

--- zorglub-gcc-snap-64-makefiles.cmake
+++ zorglub-gcc-snap-64-makefiles.cmake
@@ -1,7 +1,7 @@
 # Client maintainer: mathieu . malaterre  gmail . com
 
 set(CTEST_SITE "zorglub")
-set(CTEST_BUILD_NAME "Debian-stable-gcc4-64bit")
+set(CTEST_BUILD_NAME "Linux-gcc-snap-GIT")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(dashboard_root_name "MyTests")
@@ -9,9 +9,23 @@ SET(CTEST_BUILD_FLAGS "-j16")
 set(dashboard_source_name "gdcm")
 set(dashboard_binary_name "gdcm-nightly-snap")
 set(dashboard_do_memcheck TRUE)
+set(dashboard_do_coverage FALSE)
+set(CTEST_TEST_TIMEOUT 3000)
 
 SET(CTEST_MEMORYCHECK_COMMAND           "/usr/bin/valgrind")
 SET(CTEST_MEMORYCHECK_COMMAND_OPTIONS   "-q --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=10")
+
+SET(ENV{GLIBCPP_FORCE_NEW} 1)
+SET(ENV{GLIBCXX_FORCE_NEW} 1)
+# $ apt-cache policy gcc-snapshot 20090923-1
+SET(JAVA_HOME_PATH "/usr/lib/gcc-snapshot/lib/jvm/java-1.5.0-gcj-4.5-snap")
+SET(ENV{JAVA_HOME} ${JAVA_HOME_PATH})
+
+# http://www.cmake.org/Wiki/CTest:Coverage
+SET(ENV{CXXFLAGS} "-g -O0 -Wall -W -Wextra -Wshadow -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -pedantic -Wno-long-long -Wsign-promo -Weffc++ -Wabi -D_GLIBCXX_DEBUG")
+SET(ENV{CFLAGS}   "-g -O0 -Wall -W -Wextra -pedantic -Wno-long-long")
+SET(ENV{PATH} "/usr/lib/jvm/java-1.5.0-gcj-4.6-snap/bin/:${JAVA_HOME_PATH}/bin/:/usr/lib/gcc-snapshot/bin:$ENV{PATH}")
+SET(ENV{LD_LIBRARY_PATH} "/usr/lib/gcc-snapshot/lib")
 
 macro(dashboard_hook_init)
   set( dashboard_cache "
---
+++ zorglub-gcc43-64-makefiles.cmake
@@ -0,0 +1,49 @@
+# Client maintainer: mathieu . malaterre  gmail . com
+
+set(CTEST_SITE "zorglub")
+set(CTEST_BUILD_NAME "Linux-gcc-snap-GIT")
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_root_name "MyTests")
+SET(CTEST_BUILD_FLAGS "-j16")
+set(dashboard_source_name "gdcm")
+set(dashboard_binary_name "gdcm-gcc43-nightly")
+set(dashboard_do_memcheck FALSE)
+set(dashboard_do_coverage FALSE)
+
+SET(ENV{CC} "gcc-4.3")
+SET(ENV{CXX} "g++-4.3")
+SET(ENV{GDCM_DATA_ROOT} "$ENV{HOME}/Creatis/gdcmData")
+SET(CTEST_GDCM_PIXEL_SPACING_DATA_ROOT "$ENV{HOME}/Dashboards/Theralys/pixelspacingtestimages")
+SET(ENV{CXXFLAGS} "-g -O0 -Wall -W -Wextra -Wshadow -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -Wsign-promo -Weffc++ -Wabi -D_GLIBCXX_DEBUG")
+SET(ENV{CFLAGS}   "-g -O0 -Wall -W -Wextra -pedantic -Wno-long-long")
+
+macro(dashboard_hook_init)
+  set( dashboard_cache "
+  #CMAKE_BUILD_TYPE:STRING=Debug
+  #MEMORYCHECK_COMMAND:FILEPATH=${CTEST_MEMORYCHECK_COMMAND}
+  #MEMORYCHECK_COMMAND_OPTIONS:STRING=${CTEST_MEMORYCHECK_COMMAND_OPTIONS}
+GDCM_WRAP_PYTHON:BOOL=ON
+GDCM_BUILD_SHARED_LIBS:BOOL=ON
+GDCM_TEST_DCMTK:BOOL=OFF
+GDCM_TEST_BOOTSTRAP:BOOL=OFF
+CMAKE_MAKE_PROGRAM:FILEPATH=/usr/bin/make
+GDCM_BUILD_APPLICATIONS:BOOL=ON
+
+GDCM_USE_VTK:BOOL=ON
+VTK_DIR:PATH=/home/mathieu/Kitware/VTK56-gcc
+GDCM_DOCUMENTATION:BOOL=ON
+GDCM_VTK_DOCUMENTATION:BOOL=ON
+MAKECOMMAND:STRING=/usr/bin/make -j8
+GDCM_DATA_EXTRA_ROOT:PATH=$ENV{HOME}/Creatis/gdcmDataExtra
+#GDCM_PIXEL_SPACING_DATA_ROOT:PATH=${CTEST_GDCM_PIXEL_SPACING_DATA_ROOT}
+CMAKE_OVERRIDE_COMPILER_MISMATCH:BOOL=ON
+
+CMAKE_INSTALL_PREFIX:PATH=/tmp/local43
+# secret option
+GDCM_TESTING_USE_LC_NUMERIC:BOOL=ON
+    "
+    )
+endmacro(dashboard_hook_init)
+
+include(${CTEST_SCRIPT_DIRECTORY}/gdcm_common.cmake)
