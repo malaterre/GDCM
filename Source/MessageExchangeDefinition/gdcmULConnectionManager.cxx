63b12e123bb1c61944881eff104ff2b422f73009
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-10-25 15:29:00 +0200

    Do some doc cleanups for doxygen

--- Source/MessageExchangeDefinition/gdcmULConnectionManager.cxx
+++ Source/MessageExchangeDefinition/gdcmULConnectionManager.cxx
@@ -15,18 +15,8 @@
  *  limitations under the License.
  *
  *=========================================================================*/
-
-/*
-
-The ULConnectionManager performs actions on the ULConnection given inputs from
-the user and from the state of what's going on around the connection (ie,
-timeouts of the ARTIM timer, responses from the peer across the connection, etc).
-
-Its inputs are ULEvents, and it performs ULActions.
-
-*/
-
 #include "gdcmULConnectionManager.h"
+
 #include "gdcmUserInformation.h"
 #include "gdcmULEvent.h"
 #include "gdcmPDUFactory.h"
