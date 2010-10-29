582096c2e7ceb88ddf75c0f70b5aa8f95037dd09
Mark Roden <mmroden@gmail.com>
2010-10-29 09:50:24 -0700

    fixing the aassociateacpdu

--- Source/MessageExchangeDefinition/gdcmULActionAE.cxx
+++ Source/MessageExchangeDefinition/gdcmULActionAE.cxx
@@ -160,6 +160,8 @@ EStateID ULActionAE6::PerformAction(ULEvent& inEvent, ULConnection& inConnection
       pcac1.SetTransferSyntax( ts1 );
       acpdu.AddPresentationContextAC( pcac1 );
     }
+    acpdu.SetCalledAETitle(inConnection.GetConnectionInfo().GetCalledAETitle());
+    acpdu.SetCallingAETitle(inConnection.GetConnectionInfo().GetCallingAETitle());
     acpdu.Write( *inConnection.GetProtocol() );
     inConnection.GetProtocol()->flush();
 
