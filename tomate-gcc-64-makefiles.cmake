3f79640ffbceb2c57801c437d2aeac5ef8e5a855
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-04 15:55:41 +0100

    Add driver + script for tomate

---
+++ tomate-gcc-64-makefiles.cmake
@@ -0,0 +1,25 @@
+# Client maintainer: mathieu . malaterre  gmail . com
+
+set(CTEST_SITE "tomate")
+set(CTEST_BUILD_NAME "Linux-gcc-GIT")
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_root_name "MyTests")
+SET(CTEST_BUILD_FLAGS "-j16")
+set(dashboard_source_name "gdcm")
+set(dashboard_binary_name "gdcm-nightly")
+set(dashboard_do_memcheck FALSE)
+set(dashboard_do_coverage FALSE)
+
+SET(ENV{CXXFLAGS} "-g -O0 -Wall -W -Wshadow -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings ")
+SET(ENV{CFLAGS}   "-g -O0 -Wall -W -pedantic -Wno-long-long")
+
+macro(dashboard_hook_init)
+  set( dashboard_cache "
+GDCM_BUILD_SHARED_LIBS:BOOL=ON
+GDCM_BUILD_APPLICATIONS:BOOL=ON
+    "
+    )
+endmacro(dashboard_hook_init)
+
+include(${CTEST_SCRIPT_DIRECTORY}/gdcm_common.cmake)
