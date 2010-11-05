cf70c97629d3f6339cae3afdee42f16a35571e8d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-05 11:02:35 +0100

    Add driver for VTK

--- runtests_tomate.sh
+++ runtests_tomate.sh
@@ -4,5 +4,6 @@ cd $HOME/Dashboards/GDCMScripts && git pull
 
 PATH=$HOME/Software/cmake-2.8.2-Linux-i386/bin:$PATH
 
+ctest -S tomate-vtk-gcc-64-makefiles.cmake -V -O tomate-vtk-gcc-64-makefiles.log
 ctest -S tomate-gcc-64-makefiles.cmake -V -O tomate-gcc-64-makefiles.log
 
