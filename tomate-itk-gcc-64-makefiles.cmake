2b979193388772922350f488a4b41e845725ea35
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-05 16:41:03 +0100

    Start running itk dashboard on tomate

--- tomate-itk-gcc-64-makefiles.cmake
+++ tomate-itk-gcc-64-makefiles.cmake
@@ -1,11 +1,14 @@
 # Client maintainer: mathieu malaterre gmail com
 
-set(CTEST_SITE "zorglub")
-set(CTEST_BUILD_NAME "Debian-stable-gcc4-64bit")
+set(CTEST_SITE "tomate")
+set(CTEST_BUILD_NAME "Linux-gcc")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(dashboard_root_name "MyTests")
 #set(CTEST_TEST_ARGS PARALLEL_LEVEL 2)
+set(dashboard_source_name ITK)
+set(dashboard_binary_name ITK-gcc-tomate)
+set(dashboard_model Nightly)
 
 macro(dashboard_hook_init)
   set( dashboard_cache "
