b2a931efd34f1f3e11423793939191fc9b31a5bd
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-08 03:12:29 +0100

    Adding kwstyle to driver

--- runtests_zorglub.sh
+++ runtests_zorglub.sh
@@ -11,6 +11,7 @@ ctest -S zorglub-gcc43-64-makefiles.cmake -V -O zorglub-gcc43-64-makefiles.log
 ctest -S zorglub-gcc-64-makefiles-dcmqrscp.cmake -V -O zorglub-gcc-64-makefiles-dcmqrscp.log
 ctest -S zorglub-gcc-64-makefiles.cmake -V -O zorglub-gcc-64-makefiles.log
 ctest -S zorglub-gcc-snap-64-makefiles.cmake -V -O zorglub-gcc-snap-64-makefiles.log
+ctest -S zorglub-gcc-64-makefiles-kwstyle.cmake -V -O zorglub-gcc-64-makefiles-kwstyle.log
 
 echo "Stopping dcmqrscp"
 
