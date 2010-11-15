7670e9134ad2617e531c41b4da7ae546d2dc45fe
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-15 14:02:22 +0100

    Fix name

--- runtests_vbox.bat
+++ runtests_vbox.bat
@@ -2,4 +2,4 @@
 cd %HOME%/Dashboards/GDCMScripts && git pull
 
 @rem ctest -S vbox-vtk-gcc-64-makefiles.cmake -V -O vbox-vtk-gcc-64-makefiles.log
-ctest -S vbox-gcc-64-makefiles.cmake -V -O vbox-gdcm-vs2010-debug.log
+ctest -S vbox_winxp_vs9_debug.cmake -V -O vbox-gdcm-vs2010-debug.log
