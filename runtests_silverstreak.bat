1204edd9ff64fd8df0a84ec40c4834be0a4227b6
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-15 18:14:06 +0100

    Add driver

---
+++ runtests_silverstreak.bat
@@ -0,0 +1,5 @@
+ctest -S silverstreak-vtk-vs9-release-64.cmake
+ctest -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake
+ctest -S silverstreak_gdcm_Nightly_VS9_Release.cmake
+ctest -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake
+ctest -S silverstreak_gdcm_Nightly_VS9_Debug.cmake
