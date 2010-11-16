4691951519b6c0e60cff9b573e447df34b1f5822
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-16 17:29:45 +0100

    Be nice with user and trap NULL

---
+++ Utilities/VTK/Examples/Cxx/GenerateRTSTRUCT.cxx
@@ -0,0 +1,40 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2010 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkGDCMPolyDataWriter.h"
+
+#include "vtkPolyData.h"
+#include "vtkMedicalImageProperties.h"
+#include "vtkRTStructSetProperties.h"
+
+int main(int argc, char *argv[])
+{
+  const char * filename = argv[1];
+
+  int numinput = 1;
+  vtkPolyData *pd = vtkPolyData::New();
+  vtkMedicalImageProperties *iprop = vtkMedicalImageProperties::New();
+  vtkRTStructSetProperties *rtprop = vtkRTStructSetProperties::New();
+
+  vtkGDCMPolyDataWriter * writer = vtkGDCMPolyDataWriter::New();
+  writer->SetNumberOfInputPorts( numinput );
+  for(int num = 0; num < numinput; ++num )
+    writer->SetInput( num, pd );
+  writer->SetFileName( filename );
+  writer->SetMedicalImageProperties( iprop );
+  writer->SetRTStructSetProperties( rtprop );
+  writer->Write();
+
+  return 0;
+}
