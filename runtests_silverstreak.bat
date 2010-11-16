5040abbf341d8c1d961602583fa272955d1b13bb
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-16 16:53:22 +0100

    Update bat to dump log file

--- runtests_silverstreak.bat
+++ runtests_silverstreak.bat
@@ -1,9 +1,8 @@
-git pull
 @rem update git
 git pull
 
-ctest -S silverstreak-vtk-vs9-release-64.cmake
-ctest -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake
-ctest -S silverstreak_gdcm_Nightly_VS9_Release.cmake
-ctest -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake
-ctest -S silverstreak_gdcm_Nightly_VS9_Debug.cmake
+ctest -S silverstreak-vtk-vs9-release-64.cmake          -V -O silverstreak-vtk-vs9-release-64.log
+ctest -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake -V -O silverstreak-gdcm-vs9-release-64.log
+ctest -S silverstreak_gdcm_Nightly_VS9_Release.cmake    -V -O silverstreak-gdcm-vs9-release.log
+ctest -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake   -V -O silverstreak-gdcm-vs9-debug-64.log
+ctest -S silverstreak_gdcm_Nightly_VS9_Debug.cmake      -V -O silverstreak-gdcm-vs9-debug.log
