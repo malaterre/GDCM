31a2c26ca3b59f2f9c338ff19be7023cb88869c3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-17 18:02:03 +0100

    Add more log

--- runtests_silverstreak.bat
+++ runtests_silverstreak.bat
@@ -4,8 +4,12 @@
 @rem update git
 @rem git pull
 
+echo %date% %time% Starting GDCM >> runtests_silverstreak.log
+
 "c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak-vtk-vs9-release-64.cmake          -V -O silverstreak-vtk-vs9-release-64.log
 "c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake -V -O silverstreak-gdcm-vs9-release-64.log
 "c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Release.cmake    -V -O silverstreak-gdcm-vs9-release.log
 "c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake   -V -O silverstreak-gdcm-vs9-debug-64.log
 "c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Debug.cmake      -V -O silverstreak-gdcm-vs9-debug.log
+
+echo %date% %time% Finish GDCM >> runtests_silverstreak.log
