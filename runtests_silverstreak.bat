a5f0e20dbbdc97b40669c25012697d1a806600b3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-17 17:57:18 +0100

    Remove cd and add quotes

--- runtests_silverstreak.bat
+++ runtests_silverstreak.bat
@@ -1,11 +1,11 @@
 @rem go the GDCMScripts dir:
-cd h:\src\dashboard\gdcmscripts
+@rem cd h:\src\dashboard\gdcmscripts
 
 @rem update git
 @rem git pull
 
-c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak-vtk-vs9-release-64.cmake          -V -O silverstreak-vtk-vs9-release-64.log
-c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake -V -O silverstreak-gdcm-vs9-release-64.log
-c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Release.cmake    -V -O silverstreak-gdcm-vs9-release.log
-c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake   -V -O silverstreak-gdcm-vs9-debug-64.log
-c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Debug.cmake      -V -O silverstreak-gdcm-vs9-debug.log
+"c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak-vtk-vs9-release-64.cmake          -V -O silverstreak-vtk-vs9-release-64.log
+"c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake -V -O silverstreak-gdcm-vs9-release-64.log
+"c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Release.cmake    -V -O silverstreak-gdcm-vs9-release.log
+"c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake   -V -O silverstreak-gdcm-vs9-debug-64.log
+"c:\program files (x86)\cmake 2.8\bin\ctest" -S silverstreak_gdcm_Nightly_VS9_Debug.cmake      -V -O silverstreak-gdcm-vs9-debug.log
