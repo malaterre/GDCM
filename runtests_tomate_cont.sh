340c5b0492f7a94f3a5097911bc5319b45469eb6
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-17 09:25:13 +0100

    Fix path

--- runtests_tomate_cont.sh
+++ runtests_tomate_cont.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd $HOME/Dashboards/GDCMScripts && git pull
+
 PATH=$HOME/Software/cmake-2.8.2-Linux-i386/bin:$PATH
 
 ctest -S tomate-gdcm-gcc-64-makefiles-cont.cmake -V -O tomate-gdcm-gcc-64-makefiles-cont.log
