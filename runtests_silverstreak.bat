2a9c6f7c9b44ebcee399dfabdf58561cba9ca56e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-17 17:53:20 +0100

    Hopefully fixes issues

--- runtests_silverstreak.bat
+++ runtests_silverstreak.bat
@@ -1,8 +1,11 @@
-@rem update git
-git pull
+@rem go the GDCMScripts dir:
+cd h:\src\dashboard\gdcmscripts
 
-ctest -S silverstreak-vtk-vs9-release-64.cmake          -V -O silverstreak-vtk-vs9-release-64.log
-ctest -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake -V -O silverstreak-gdcm-vs9-release-64.log
-ctest -S silverstreak_gdcm_Nightly_VS9_Release.cmake    -V -O silverstreak-gdcm-vs9-release.log
-ctest -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake   -V -O silverstreak-gdcm-vs9-debug-64.log
-ctest -S silverstreak_gdcm_Nightly_VS9_Debug.cmake      -V -O silverstreak-gdcm-vs9-debug.log
+@rem update git
+@rem git pull
+
+c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak-vtk-vs9-release-64.cmake          -V -O silverstreak-vtk-vs9-release-64.log
+c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Release_64.cmake -V -O silverstreak-gdcm-vs9-release-64.log
+c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Release.cmake    -V -O silverstreak-gdcm-vs9-release.log
+c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Debug_64.cmake   -V -O silverstreak-gdcm-vs9-debug-64.log
+c:\program files (x86)\cmake 2.8\bin\ctest -S silverstreak_gdcm_Nightly_VS9_Debug.cmake      -V -O silverstreak-gdcm-vs9-debug.log
