ae7ef72f29e3600f83f66fa9807f6d6202ed00e0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-17 09:43:53 +0100

    Some cleanups

--- tomate-vtk-gcc-64-makefiles.cmake
+++ tomate-vtk-gcc-64-makefiles.cmake
@@ -11,24 +11,21 @@ set(CTEST_SITE "tomate")
 set(CTEST_BUILD_NAME "Linux-gcc")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(dashboard_root_name "MyTests")
 set(CTEST_DASHBOARD_ROOT "/local/mmalater")
+set(dashboard_root_name "MyTests")
 set(dashboard_source_name VTK)
 set(dashboard_binary_name VTK-gcc-tomate)
 set(dashboard_model Nightly)
 set(dashboard_do_test FALSE)
-
-#set(VTK_DATA_ROOT ${CTEST_DASHBOARD_ROOT}/VTKData)
-#set(VTK_LARGE_DATA_ROOT ${CTEST_DASHBOARD_ROOT}/VTKLargeData)
-#set(CTEST_TEST_TIMEOUT 120)
 
 set(dashboard_cache "
 BUILD_SHARED_LIBS:BOOL=ON
 BUILD_EXAMPLES:BOOL=OFF
 VTK_DEBUG_LEAKS:BOOL=ON
 VTK_USE_PARALLEL:BOOL=OFF
-VTK_USE_RENDERING:BOOL=OFF
-#VTK_DATA_ROOT:PATH=${CTEST_DASHBOARD_ROOT}/VTKData
+VTK_USE_RENDERING:BOOL=ON
+VTK_USE_N_WAY_ARRAYS:BOOL=OFF
+VTK_USE_INFOVIS:BOOL=OFF
 ")
 
 include(${CTEST_SCRIPT_DIRECTORY}/vtk_common.cmake)
