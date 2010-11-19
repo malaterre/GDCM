3abb724c46df0a3ef38bb76f095413a98babef84
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2010-11-19 09:05:50 +0100

    Rework the RT stuff helper to make compilation works

--- Utilities/VTK/vtkRTStructSetProperties.h
+++ Utilities/VTK/vtkRTStructSetProperties.h
@@ -22,8 +22,10 @@
 #define VTKRTSTRUCTSETPROPERTIES_H
 
 #include "vtkObject.h"
+#include "gdcmDirectory.h"
 
 class vtkRTStructSetPropertiesInternals;
+class vtkAppendPolyData; // FIXME
 
 class VTK_EXPORT vtkRTStructSetProperties : public vtkObject
 {
@@ -96,6 +98,23 @@ public:
   const char *GetStructureSetROIName(vtkIdType);
   const char *GetStructureSetROIGenerationAlgorithm(vtkIdType);
   const char *GetStructureSetRTROIInterpretedType(vtkIdType id);
+
+  //the function LoadCTImageFromFiles gives us a list of images in IPP-sorted order for
+  //constructing rt structures
+  //(that need both the z position and the appropriate SOP Instance UID for a plane)
+  //so, armed with that list of points and the list of images, the first point in
+  //each polydata object can be appropriately compiled into the rtstruct
+  //we use appendpolydata here-- each polydata is an organ
+  //NOTE: the number of outputs for the appendpolydata MUST MATCH the organ vectors!
+  // FIXME this function can not be wrap by vtk wrapper...
+//BTX
+  static vtkRTStructSetProperties* ProduceStructureSetProperties(const std::string& inDirectory,
+     const std::string& inStructLabel, const std::string& inStructName,
+     vtkAppendPolyData* inPolyData,//should also have colors
+     gdcm::Directory::FilenamesType inROINames,
+     gdcm::Directory::FilenamesType inROIAlgorithmName,
+     gdcm::Directory::FilenamesType inROIType);
+//ETX
 
 protected:
   vtkRTStructSetProperties();
