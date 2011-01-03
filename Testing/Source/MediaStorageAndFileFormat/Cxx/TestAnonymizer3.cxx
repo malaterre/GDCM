519f7745ffa142b6dd7929f60b718db7978bc12c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-01-03 14:06:23 +0100

    Add a test to completely check the UID reference mechanism in anonymization

--- Testing/Source/MediaStorageAndFileFormat/Cxx/TestAnonymizer3.cxx
+++ Testing/Source/MediaStorageAndFileFormat/Cxx/TestAnonymizer3.cxx
@@ -14,6 +14,7 @@
 =========================================================================*/
 #include "gdcmAnonymizer.h"
 #include "gdcmSimpleSubjectWatcher.h"
+#include "gdcmUIDGenerator.h"
 #include "gdcmFilename.h"
 #include "gdcmTesting.h"
 #include "gdcmCryptographicMessageSyntax.h"
@@ -48,28 +49,11 @@ int TestAnonymizer3(int argc, char *argv[])
     //return 1;
     }
   std::string outfilename = Testing::GetTempFilename( filename, subdir );
+  std::string outfilenamelossy = Testing::GetTempFilename( "012345.002.050.lossy.dcm" , subdir );
+  std::string outfilenamelossy2 = Testing::GetTempFilename( "012345.002.050.lossy.anon.dcm" , subdir );
 
-  const char subdir2[] = "TestAnonymizer3Lossy";
-  std::string tmpdir2 = Testing::GetTempDirectory( subdir2 );
-  if( !System::FileIsDirectory( tmpdir2.c_str() ) )
-    {
-    System::MakeDirectory( tmpdir2.c_str() );
-    //return 1;
-    }
-  std::string outfilenamelossy = Testing::GetTempFilename( filename, subdir2 );
-  std::string outfilename2 = "/tmp/d.dcm";
-
-// Encrypt
+// Derive
 {
-  gdcm::CryptographicMessageSyntax cms;
-  if( !cms.ParseCertificateFile( certpath.c_str() ) )
-    {
-    return 1;
-    }
-
-  gdcm::SmartPointer<gdcm::Anonymizer> ano = new gdcm::Anonymizer;
-  ano->SetCryptographicMessageSyntax( &cms );
-
   gdcm::Reader reader;
   reader.SetFileName( filename );
   if( !reader.Read() )
@@ -78,21 +62,14 @@ int TestAnonymizer3(int argc, char *argv[])
     return 1;
     }
 
-  ano->SetFile( reader.GetFile() );
-  if( !ano->BasicApplicationLevelConfidentialityProfile() )
-    {
-    return 1;
-    }
-  gdcm::Writer writer;
-  writer.SetFileName( outfilename.c_str() );
-  writer.SetFile( ano.GetFile() );
-  if( !writer.Write() )
-    {
-    std::cerr << "Failed to write: " << outfilename << std::endl;
-    return 1;
-    }
+  File& file = reader.GetFile();
+  DataSet &ds = file.GetDataSet();
 
-  const File& file = reader.GetFile();
+  DataElement instanceuid = ds.GetDataElement( Tag(0x8,0x18) );
+  UIDGenerator uid;
+  const char *s = uid.Generate();
+  instanceuid.SetByteValue( s, strlen(s) );
+  ds.Replace( instanceuid );
 
   FileDerivation fd;
   fd.SetFile( file );
@@ -114,19 +91,20 @@ int TestAnonymizer3(int argc, char *argv[])
     return 1;
     }
 
-  writer.SetFileName( outfilename2.c_str() );
-  writer.SetFile( reader.GetFile() );
+  gdcm::Writer writer;
+  writer.SetFileName( outfilenamelossy.c_str() );
+  writer.SetFile( fd.GetFile() );
   if( !writer.Write() )
     {
-    std::cerr << "Failed to write: " << outfilename << std::endl;
+    std::cerr << "Failed to write: " << outfilenamelossy << std::endl;
     return 1;
     }
-
 }
-// Decrypt
+
+// Encrypt
 {
   gdcm::CryptographicMessageSyntax cms;
-  if( !cms.ParseKeyFile( keypath.c_str() ) )
+  if( !cms.ParseCertificateFile( certpath.c_str() ) )
     {
     return 1;
     }
@@ -134,31 +112,143 @@ int TestAnonymizer3(int argc, char *argv[])
   gdcm::SmartPointer<gdcm::Anonymizer> ano = new gdcm::Anonymizer;
   ano->SetCryptographicMessageSyntax( &cms );
 
-  //gdcm::SimpleSubjectWatcher watcher(ano, "Anonymizer");
+{
+  // original file
+  gdcm::Reader reader;
+  reader.SetFileName( filename );
+  if( !reader.Read() )
+    {
+    std::cerr << "Could not read: " << filename << std::endl;
+    return 1;
+    }
 
+  // order of operation is important
+  ano->SetFile( reader.GetFile() );
+  if( !ano->BasicApplicationLevelConfidentialityProfile() )
+    {
+    return 1;
+    }
+
+  gdcm::Writer writer;
+  writer.SetFileName( outfilename.c_str() );
+  writer.SetFile( reader.GetFile() );
+  if( !writer.Write() )
+    {
+    std::cerr << "Failed to write: " << outfilename << std::endl;
+    return 1;
+    }
+}
+
+{
+  // derived file
+  gdcm::Reader reader;
+  reader.SetFileName( outfilenamelossy.c_str() );
+  if( !reader.Read() )
+    {
+    std::cerr << "Could not read: " << outfilenamelossy << std::endl;
+    return 1;
+    }
+
+  // order of operation is important
+  ano->SetFile( reader.GetFile() );
+  if( !ano->BasicApplicationLevelConfidentialityProfile() )
+    {
+    return 1;
+    }
+
+  gdcm::Writer writer;
+  writer.SetFileName( outfilenamelossy2.c_str() );
+  writer.SetFile( reader.GetFile() );
+  if( !writer.Write() )
+    {
+    std::cerr << "Failed to write: " << outfilenamelossy2 << std::endl;
+    return 1;
+    }
+}
+}
+
+// Make sure UID is consistant:
+{
+  std::string sopinstanceuid_str1;
+{
+  // original anonymized file
   gdcm::Reader reader;
   reader.SetFileName( outfilename.c_str() );
   if( !reader.Read() )
     {
-    std::cerr << "Could not read: " << outfilename << std::endl;
+    std::cerr << "Could not read: " << filename << std::endl;
     return 1;
     }
+  File &file = reader.GetFile();
+  DataSet &ds = file.GetDataSet();
 
-  ano->SetFile( reader.GetFile() );
-  if( !ano->BasicApplicationLevelConfidentialityProfile(false) )
+  if( !ds.FindDataElement( Tag(0x0008,0x0018) )
+    || ds.GetDataElement( Tag(0x0008,0x0018) ).IsEmpty() )
+    {
+    return 1;
+    }
+  const DataElement &sopinstanceuid = ds.GetDataElement( Tag(0x0008,0x0018) );
+  sopinstanceuid_str1 = std::string( sopinstanceuid.GetByteValue()->GetPointer(), sopinstanceuid.GetByteValue()->GetLength() );
+}
+
+  std::string sopinstanceuid_str2;
+  std::string refsopinstanceuid_str2;
+{
+  // derived anonymized file
+  gdcm::Reader reader;
+  reader.SetFileName( outfilenamelossy2.c_str() );
+  if( !reader.Read() )
+    {
+    std::cerr << "Could not read: " << outfilenamelossy << std::endl;
+    return 1;
+    }
+  File &file = reader.GetFile();
+  DataSet &ds = file.GetDataSet();
+
+  if( !ds.FindDataElement( Tag(0x0008,0x0018) )
+    || ds.GetDataElement( Tag(0x0008,0x0018) ).IsEmpty() )
+    {
+    return 1;
+    }
+  const DataElement &sopinstanceuid = ds.GetDataElement( Tag(0x0008,0x0018) );
+  sopinstanceuid_str2 = std::string( sopinstanceuid.GetByteValue()->GetPointer(), sopinstanceuid.GetByteValue()->GetLength() );
+
+  // Source Image Sequence
+  if( !ds.FindDataElement( Tag(0x0008,0x2112) )
+    || ds.GetDataElement( Tag(0x0008,0x2112) ).IsEmpty() )
     {
     return 1;
     }
 
-  // TODO Need to compare filename to decrypted one.
-  gdcm::Writer writer;
-  writer.SetFileName( outfilename2.c_str() );
-  writer.SetFile( reader.GetFile() );
-  if( !writer.Write() )
+  const DataElement &sourceimagesq = ds.GetDataElement( Tag(0x0008,0x2112) );
+  SmartPointer<SequenceOfItems> sq = sourceimagesq.GetValueAsSQ();
+  gdcm::SequenceOfItems::SizeType n = sq->GetNumberOfItems();
+  if( n != 1 ) return 1;
+  Item &item = sq->GetItem( 1 );
+  DataSet &nested = item.GetNestedDataSet();
+
+  if( !nested.FindDataElement( Tag(0x0008,0x1155) )
+    || nested.GetDataElement( Tag(0x0008,0x1155) ).IsEmpty() )
     {
-    std::cerr << "Failed to write: " << outfilename2 << std::endl;
     return 1;
     }
+  const DataElement &refsopinstanceuid = nested.GetDataElement(
+    Tag(0x0008,0x1155) );
+  refsopinstanceuid_str2 = std::string( refsopinstanceuid.GetByteValue()->GetPointer(), refsopinstanceuid.GetByteValue()->GetLength() );
+
+}
+  std::cout << sopinstanceuid_str1 << std::endl;
+  std::cout << sopinstanceuid_str2 << std::endl;
+  std::cout << refsopinstanceuid_str2 << std::endl;
+
+if( sopinstanceuid_str1 == sopinstanceuid_str2 )
+{
+return 1;
+}
+if( sopinstanceuid_str1 != refsopinstanceuid_str2 )
+{
+return 1;
+}
 }
 
   return 0;
