d765aff4a7b8e9f3586fc77281426975d74ccf85
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-01-06 17:20:08 +0100

    Adding a release dashbaord

--- runtests_zorglub.sh
+++ runtests_zorglub.sh
@@ -12,6 +12,7 @@ dcmqrscp -v -d -c ./dcmqrscp.cfg 5678 &
 ctest -S zorglub-gcc43-64-makefiles.cmake -V -O zorglub-gcc43-64-makefiles.log
 ctest -S zorglub-gcc-64-makefiles-dcmqrscp.cmake -V -O zorglub-gcc-64-makefiles-dcmqrscp.log
 ctest -S zorglub-gcc-64-makefiles.cmake -V -O zorglub-gcc-64-makefiles.log
+ctest -S zorglub-gcc-64-makefiles-release.cmake -V -O zorglub-gcc-64-makefiles-release.log
 ctest -S zorglub-gcc-snap-64-makefiles.cmake -V -O zorglub-gcc-snap-64-makefiles.log
 ctest -S zorglub-gcc-64-makefiles-kwstyle.cmake -V -O zorglub-gcc-64-makefiles-kwstyle.log
 
---
+++ zorglub-gcc-64-makefiles-release.cmake
@@ -0,0 +1,57 @@
+# Client maintainer: mathieu . malaterre  gmail . com
+
+set(CTEST_SITE "zorglub")
+set(CTEST_BUILD_NAME "Linux-gcc-GIT-release")
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_root_name "MyTests")
+SET(CTEST_BUILD_FLAGS "-j16")
+set(dashboard_source_name "gdcm")
+set(dashboard_binary_name "gdcm-nightly-release")
+set(dashboard_git_branch "release")
+set(dashboard_do_memcheck FALSE)
+set(dashboard_do_coverage FALSE)
+
+SET(ENV{GDCM_DATA_ROOT} "$ENV{HOME}/Creatis/gdcmData")
+SET(CTEST_GDCM_PIXEL_SPACING_DATA_ROOT "$ENV{HOME}/Dashboards/Theralys/pixelspacingtestimages")
+SET(ENV{CXXFLAGS} "-g -O0 -Wall -W -Wshadow -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings ")
+SET(ENV{CFLAGS}   "-g -O0 -Wall -W -pedantic -Wno-long-long")
+# set any extra envionment variables here
+SET (ENV{JAVA_HOME} "/usr/lib/jvm/default-java")
+
+# http://www.cmake.org/Wiki/CTest:Coverage
+SET(CTEST_COVERAGE_COMMAND              "/usr/bin/gcov")
+#SET(ENV{CXXFLAGS} "-g -O0 -fprofile-arcs -ftest-coverage -Wno-deprecated -W -Wall")
+#SET(ENV{CFLAGS} "-g -O0 -fprofile-arcs -ftest-coverage -W -Wall")
+#SET(ENV{LDFLAGS} "-fprofile-arcs -ftest-coverage")
+
+macro(dashboard_hook_init)
+  set( dashboard_cache "
+GDCM_WRAP_PYTHON:BOOL=ON
+GDCM_WRAP_CSHARP:BOOL=ON
+GDCM_WRAP_JAVA:BOOL=ON
+GDCM_BUILD_SHARED_LIBS:BOOL=ON
+GDCM_TEST_DCMTK:BOOL=ON
+GDCM_TEST_BOOTSTRAP:BOOL=ON
+GDCM_BUILD_APPLICATIONS:BOOL=ON
+GDCM_USE_VTK:BOOL=ON
+VTK_DIR:PATH=/home/mathieu/Kitware/VTK56-gcc
+GDCM_DOCUMENTATION:BOOL=ON
+GDCM_VTK_DOCUMENTATION:BOOL=ON
+#MAKECOMMAND:STRING=/usr/bin/make -j8
+#GDCM_PIXEL_SPACING_DATA_ROOT:PATH=${CTEST_GDCM_PIXEL_SPACING_DATA_ROOT}
+
+# Temp fix for:
+# Program received signal SIGSEGV, Segmentation fault.
+# 0x00007fffe9c8a2b0 in cio_tell () from /usr/lib/libopenjpeg.so.2
+GDCM_USE_SYSTEM_OPENJPEG:BOOL=OFF
+
+# To do some more testing for the anonymizer
+GDCM_USE_SYSTEM_OPENSSL:BOOL=ON
+
+CMAKE_INSTALL_PREFIX:PATH=/tmp/local-gcc-rel
+    "
+    )
+endmacro(dashboard_hook_init)
+
+include(${CTEST_SCRIPT_DIRECTORY}/gdcm_common.cmake)
