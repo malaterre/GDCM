e519bdcf23b49ed5f37f4c4ec52c15df4168f508
Mark Roden <mmroden@gmail.com>
2011-01-10 11:56:57 -0800

    updating the connection manager to work with callbacks

---
+++ Source/MessageExchangeDefinition/gdcmULConnectionBasicCallback.h
@@ -0,0 +1,47 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef GDCMULCONNECTIONBASICCALLBACK_H
+#define GDCMULCONNECTIONBASICCALLBACK_H
+
+#include "gdcmULConnectionCallback.h"
+#include "gdcmDataSet.h"
+#include <vector>
+
+namespace gdcm 
+{
+  namespace network
+  {
+    ///This is the most basic of callbacks for how the ULConnectionManager handles
+    ///incoming datasets.  DataSets are just concatenated to the mDataSets vector,
+    ///and the result can be pulled out of the vector by later code.
+    ///Alternatives to this method include progress updates, saving to disk, etc.
+    ///This class is NOT THREAD SAFE.  Access the dataset vector after the
+    ///entire set of datasets has been returned by the ULConnectionManager.
+    class GDCM_EXPORT ULConnectionBasicCallback : public ULConnectionCallback{
+      std::vector<DataSet> mDataSets;
+    public:
+      ULConnectionBasicCallback() {};
+      virtual ~ULConnectionBasicCallback() {} //empty, for later inheritance
+
+      virtual void HandleDataSet(const DataSet& inDataSet);
+
+      std::vector<DataSet> GetDataSets() const;
+    };
+  }
+}
+#endif //GDCMULCONNECTIONBASICCALLBACK_H
