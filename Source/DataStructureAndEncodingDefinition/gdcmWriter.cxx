malaterre/GDCM

commit 629b0739626ebae11ae4b277bfa9c46a4ff6d28c

::Write was closing the internal stream. Let's change a little behavior

Source/DataStructureAndEncodingDefinition/gdcmWriter.cxx
@@ -35,10 +35,13 @@ namespace gdcm
 
 Writer::~Writer()
 {
-  if (Ofstream) {
+  if (Ofstream)
+    {
     Ofstream->close();
     delete Ofstream;
-  }
+    Ofstream = NULL;
+    Stream = NULL;
+    }
 }
 
 bool Writer::Write()
@@ -164,17 +167,6 @@ catch(...)
   gdcmErrorMacro( "what the hell" );
   return false;
 }
-
-
-
-  // FIXME : call this function twice...
-  if (Ofstream)
-  {
-  Ofstream->close();
-  delete Ofstream;
-  Ofstream = NULL;
-  Stream = NULL;
-  }
 
   return true;
 }

commit 23475c8261285f03046e40159f38a17f0088ff87

Fix bug #3157294. Make sure to flush the ostream.
Regression was introduced by commit 629b0739626ebae11ae4b277bfa9c46a4ff6d28c

Source/DataStructureAndEncodingDefinition/gdcmWriter.cxx
@@ -172,6 +172,11 @@ catch(...)
   return false;
 }
 
+if (Ofstream)
+{
+  Ofstream->flush();
+}
+
   return true;
 }
 