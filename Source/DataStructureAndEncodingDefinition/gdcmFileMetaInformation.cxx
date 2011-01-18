malaterre/GDCM

commit 864a67013a44bbd675f98e3646c9ce8377245126

Handle file with 0,0 attribute

Source/DataStructureAndEncodingDefinition/gdcmFileMetaInformation.cxx
@@ -604,15 +604,21 @@ std::istream &FileMetaInformation::ReadCompat(std::istream &is)
       }
     else
       {
-    throw Exception( "Cannot find DICOM type. Giving up." );
-    //  std::streampos start = is.tellg();
-    //  ImplicitDataElement ide;
-    //  ide.Read<SwapperNoOp>(is); // might throw an expection which will NOT be caught
-    //  std::streampos cur = is.tellg();
-    //  std::cout << "s-c" << start - cur << std::endl;
-    //  is.seekg( start - cur, std::ios::cur );
-    //  // ok we could read at least one implicit element
-    //  DataSetTS = TransferSyntax::ImplicitVRLittleEndian;
+      DataElement null( Tag(0x0,0x0), 0);
+      ImplicitDataElement ide;
+      ide.Read<SwapperNoOp>(is); // might throw an expection which will NOT be caught
+      if( ide == null )
+        {
+        // Ok big deal we found a null dataelement, should we really add it
+        // in the dataset ? Well let's assume this is not that important
+        ReadCompat(is);
+        assert( DataSetTS == TransferSyntax::ImplicitVRLittleEndian );
+        return is;
+        }
+      else
+        {
+        throw Exception( "Cannot find DICOM type. Giving up." );
+        }
       }
     }
   return is;

commit 597dfc33a6e7c5679f165061d587613940903e5e

This commit fixes bug #3160652
It internally introduce a counter for reading a maximum of 10 null attributes
in a given dataset. It occur in the past that some trailing or starting elements
were all zero.
This fixes regression introduced by 864a67013a44bbd675f98e3646c9ce8377245126

Source/DataStructureAndEncodingDefinition/gdcmFileMetaInformation.cxx
@@ -537,6 +537,11 @@ std::istream &FileMetaInformation::Read(std::istream &is)
 
 std::istream &FileMetaInformation::ReadCompat(std::istream &is)
 {
+  return ReadCompat(is, 0);
+}
+
+std::istream &FileMetaInformation::ReadCompat(std::istream &is, unsigned int counter)
+{
   // First off save position in case we fail (no File Meta Information)
   // See PS 3.5, Data Element Structure With Explicit VR
   if( !IsEmpty() )
@@ -606,12 +611,14 @@ std::istream &FileMetaInformation::ReadCompat(std::istream &is)
       {
       DataElement null( Tag(0x0,0x0), 0);
       ImplicitDataElement ide;
-      ide.Read<SwapperNoOp>(is); // might throw an expection which will NOT be caught
-      if( ide == null )
+      ide.Read<SwapperNoOp>(is); // might throw an exception which will NOT be caught
+      // let's assume that we can read at most 10 null elements, after that we decide
+      // arbitrarily that the cannot possibly be a DICOM file.
+      if( ide == null && counter < 10 )
         {
         // Ok big deal we found a null dataelement, should we really add it
         // in the dataset ? Well let's assume this is not that important
-        ReadCompat(is);
+        ReadCompat(is, counter + 1);
         assert( DataSetTS == TransferSyntax::ImplicitVRLittleEndian );
         return is;
         }

Source/DataStructureAndEncodingDefinition/gdcmFileMetaInformation.h
@@ -125,6 +125,7 @@ protected:
   static const char * GetGDCMSourceApplicationEntityTitle();
 
 private:
+  std::istream &ReadCompat(std::istream &is, unsigned int counter);
   Preamble P;
 
 //static stuff: