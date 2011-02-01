dd411e41f5b293f9615bd289239f3a76b5c55f1f
Mark Roden <mmroden@gmail.com>
2011-02-01 14:25:29 -0800

    I hate it when I forget the branch I'm on

---
+++ Source/MessageExchangeDefinition/gdcmCompositeNetworkFunctions.cxx
@@ -0,0 +1,259 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "gdcmCompositeNetworkFunctions.h"
+
+#include <socket++/echo.h>
+
+#include "gdcmReader.h"
+#include "gdcmAttribute.h"
+#include "gdcmULConnectionManager.h"
+#include "gdcmULConnection.h"
+#include "gdcmDataSet.h"
+#include "gdcmVersion.h"
+#include "gdcmGlobal.h"
+#include "gdcmSystem.h"
+#include "gdcmUIDGenerator.h"
+#include "gdcmStringFilter.h"
+#include "gdcmWriter.h"
+#include "gdcmPrinter.h"
+
+#include "gdcmQueryFactory.h"
+#include "gdcmDirectory.h"
+#include "gdcmStudyRootQuery.h"
+#include "gdcmPatientRootQuery.h"
+#include "gdcmULWritingCallback.h"
+#include "gdcmImageReader.h"
+
+using namespace gdcm;
+
+
+// Execute like this:
+// ./bin/gdcmscu www.dicomserver.co.uk 11112 echo
+bool CompositeNetworkFunctions::CEcho( const char *remote, int portno, std::string const &aetitle,
+            std::string const &call )
+{
+  gdcm::network::ULConnectionManager theManager;
+  gdcm::DataSet blank;
+  if (!theManager.EstablishConnection(aetitle, call, remote, 0, portno, 10,
+      gdcm::network::eEcho, blank))
+    {
+      if (gdcm::Trace::GetErrorFlag())
+        {
+        std::cerr << "Failed to establish connection." << std::endl;
+        }
+    return false;
+    }
+  std::vector<gdcm::network::PresentationDataValue> theValues1 = theManager.SendEcho();
+
+  //should print _something_ to let the user know of success, if they ask for something
+  //other than a return code.
+  if (gdcm::Trace::GetWarningFlag())
+    {
+      gdcm::DataSet ds = gdcm::network::PresentationDataValue::ConcatenatePDVBlobs(theValues1);
+      gdcm::Printer thePrinter;
+      thePrinter.PrintDataSet(ds, std::cout);
+    }
+  theManager.BreakConnection(-1);//wait for a while for the connection to break, ie, infinite
+
+  // Check the Success Status
+  gdcm::DataSet ds = gdcm::network::PresentationDataValue::ConcatenatePDVBlobs( theValues1 );
+  gdcm::Attribute<0x0,0x0900> at;
+  at.SetFromDataSet( ds );
+
+  if( at.GetValue() != 0 )
+    {
+    return false;
+    }
+
+  return true;
+}
+
+//this function will take command line options and construct a cmove query from them
+//returns NULL if the query could not be constructed.
+//note that the caller is responsible for deleting the constructed query.
+//used to build both a move and a find query (true for inMove if it's move, false if it's find)
+gdcm::BaseRootQuery* CompositeNetworkFunctions::ConstructQuery(bool inMove, gdcm::ERootType inRootType, gdcm::EQueryLevel inQueryLevel,
+                                                               const std::vector< std::pair<gdcm::Tag, std::string> >& keys)
+{
+  gdcm::StringFilter sf;
+  std::vector< std::pair<gdcm::Tag, std::string> >::const_iterator it =
+    keys.begin();
+  gdcm::BaseRootQuery* outQuery = gdcm::QueryFactory::ProduceQuery(inRootType, inQueryLevel);
+  if (!outQuery)
+    {
+    if (gdcm::Trace::GetErrorFlag())
+      std::cerr << "Specify the query" << std::endl;
+    return NULL;
+    }
+  gdcm::DataSet ds;
+  for(; it != keys.end(); ++it)
+    {
+    std::string s = sf.FromString( it->first, it->second.c_str(), it->second.size() );
+    if (inMove)
+      {
+      gdcm::DataElement de( it->first );
+      de.SetByteValue ( s.c_str(), s.size() );
+      ds.Insert( de );
+      }
+    outQuery->SetSearchParameter(it->first, s);
+    }
+
+  if (gdcm::Trace::GetWarningFlag())
+    ds.Print( std::cout );
+
+
+  // setup the special character set
+  std::vector<gdcm::ECharSet> inCharSetType;
+  inCharSetType.push_back( gdcm::QueryFactory::GetCharacterFromCurrentLocale() );
+  gdcm::DataElement de = gdcm::QueryFactory::ProduceCharacterSetDataElement(inCharSetType);
+  std::string param ( de.GetByteValue()->GetPointer(),
+    de.GetByteValue()->GetLength() );
+  outQuery->SetSearchParameter(de.GetTag(), param );
+
+  return outQuery;
+}
+
+
+//note that pointer to the base root query-- the caller must instantiated and delete
+bool CompositeNetworkFunctions::CMove( const char *remote, int portno, std::string const &aetitle,
+            std::string const &call, gdcm::BaseRootQuery* query,
+            int portscp, std::string const & outputdir)
+{
+  // $ findscu -v  -d --aetitle ACME1 --call ACME_STORE  -P -k 0010,0010="X*" dhcp-67-183 5678  patqry.dcm
+  // Add a query:
+
+  if (outputdir.empty())
+    {
+      if (gdcm::Trace::GetErrorFlag())
+        {
+        std::cerr << "Output directory not specified." << std::endl;
+        }
+    return false;
+    }
+
+  gdcm::network::ULConnectionManager theManager;
+  if (!theManager.EstablishConnectionMove(aetitle, call, remote, 0, portno, 1000,
+      portscp, query->GetQueryDataSet()))
+    {
+      if (gdcm::Trace::GetErrorFlag())
+        {
+        std::cerr << "Failed to establish connection." << std::endl;
+        }
+    return false;
+    }
+
+  int ret = 0;
+  gdcm::network::ULWritingCallback theCallback;
+  theCallback.SetDirectory(outputdir);
+  theManager.SendMove( query, &theCallback );
+
+  theManager.BreakConnection(-1);//wait for a while for the connection to break, ie, infinite
+  return (ret == 0);//if ret == 0, then nothing was broken
+}
+
+//note that pointer to the base root query-- the caller must instantiated and delete
+std::vector<gdcm::DataSet> CompositeNetworkFunctions::CFind( const char *remote, int portno , std::string const &aetitle,
+                                  std::string const &call , gdcm::BaseRootQuery* query )
+{
+  // $ findscu -v  -d --aetitle ACME1 --call ACME_STORE  -P -k 0010,0010="X*" dhcp-67-183 5678  patqry.dcm
+  // Add a query:
+  gdcm::network::ULConnectionManager theManager;
+  std::vector<gdcm::DataSet> theDataSets;
+  //theManager.EstablishConnection("ACME1", "ACME_STORE", remote, 0, portno, 1000, gdcm::network::eFind, ds);
+  if (!theManager.EstablishConnection(aetitle, call, remote, 0, portno, 1000, gdcm::network::eFind,  query->GetQueryDataSet())){
+
+    if (gdcm::Trace::GetErrorFlag())
+      {
+      std::cerr << "Failed to establish connection." << std::endl;
+      }
+    return theDataSets;
+  }
+  theDataSets = theManager.SendFind( query );
+
+  theManager.BreakConnection(-1);//wait for a while for the connection to break, ie, infinite
+  return theDataSets;
+}
+
+bool CompositeNetworkFunctions::CStore( const char *remote, int portno,
+             std::string const &aetitle, std::string const &call,
+             std::vector<std::string> const & filenames, bool inRecursive )
+{
+  std::string filename = filenames[0];
+  gdcm::network::ULConnectionManager theManager;
+  std::vector<std::string> files;
+  if( gdcm::System::FileIsDirectory(filename.c_str()) )
+    {
+    unsigned int nfiles = 1;
+    gdcm::Directory dir;
+    nfiles = dir.Load(filename, inRecursive);
+    files = dir.GetFilenames();
+    }
+  else
+    {
+    files = filenames;
+    }
+  filename = files[0];
+  gdcm::Reader reader;
+  reader.SetFileName( filename.c_str() );
+  if( !reader.Read() ) return 1;
+  //const gdcm::File &file = reader.GetFile();
+  const gdcm::DataSet &ds = reader.GetFile().GetDataSet();
+
+  if (!theManager.EstablishConnection(aetitle, call, remote, 0,
+      portno, 1000, gdcm::network::eStore, ds))
+    {
+      if (gdcm::Trace::GetErrorFlag())
+        {
+        std::cerr << "Failed to establish connection." << std::endl;
+        }
+      return 1;
+    }
+#if 0
+  // Right now we are never reading back the AC-Acpt to
+  // if we can prefer a compressed TS
+  gdcm::network::ULConnection* uc = theManager.GetConnection();
+  std::vector<gdcm::network::PresentationContext> const &pcs =
+    uc->GetPresentationContexts();
+  std::vector<gdcm::network::PresentationContext>::const_iterator it =
+    pcs.begin();
+  std::cout << "B" << std::endl;
+  for( ; it != pcs.end(); ++it )
+    {
+    it->Print ( std::cout );
+    }
+  std::cout << "BB" << std::endl;
+  theManager.SendStore( (gdcm::DataSet*)&ds );
+#endif
+
+  theManager.SendStore( (gdcm::DataSet*)&ds );
+
+  for( size_t i = 1; i < files.size(); ++i )
+    {
+    const std::string & filename = files[i];
+    gdcm::Reader reader;
+    reader.SetFileName( filename.c_str() );
+    if( !reader.Read() ) return false;
+    const gdcm::DataSet &ds = reader.GetFile().GetDataSet();
+    theManager.SendStore( (gdcm::DataSet*)&ds );
+    }
+
+  theManager.BreakConnection(-1);//wait for a while for the connection to break, ie, infinite
+  return true;
+
+}
---
+++ Source/MessageExchangeDefinition/gdcmCompositeNetworkFunctions.h
@@ -0,0 +1,87 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef GDCMCOMPOSITENETWORKFUNCTIONS_H
+#define GDCMCOMPOSITENETWORKFUNCTIONS_H
+
+#include <vector>
+#include <string>
+#include "gdcmTypes.h"
+#include "gdcmBaseRootQuery.h"
+
+namespace gdcm{
+  class DataSet;
+  class Tag;
+  /// These functions provide a generic API to the DICOM functions implemented in GDCM.
+  /// Advanced users can use this code as a template for building their own versions of
+  /// these functions (for instance, to provide progress bars or some other way of handling
+  /// returned query information), but for most users, these functions should be
+  /// sufficient to interface with a PACS to a local machine.
+  /// Note that these functions are not contained within a static class or some other
+  /// class-style interface, because multiple connections can be instantiated in the same program.
+  /// The DICOM standard is much more function oriented rather than class oriented in this
+  /// instance, so the design of this API reflects that functional approach
+
+  /// The most basic network function.  Use this function to ensure that the remote server is
+  /// responding on the given IP and port number as expected.
+  /// Returns true if it worked.
+  class GDCM_EXPORT CompositeNetworkFunctions 
+    {
+    public:
+    bool CEcho( const char *remote, int portno, const std::string& aetitle,
+                const std::string& call );
+
+    ///This function will take a list of strings and tags and fill in a query that can be used
+    ///for either CFind or CMove (depending on the input boolean inMove).
+    ///Note that the caller is responsible for deleting the constructed query.
+    ///This function is used to build both a move and a find query 
+    ///(true for inMove if it's move, false if it's find)
+    gdcm::BaseRootQuery* ConstructQuery(bool inMove, gdcm::ERootType inRootType, gdcm::EQueryLevel inQueryLevel,
+                                        const std::vector< std::pair<gdcm::Tag, std::string> >& keys);
+
+
+    ///This function will use the provided query to get files from a remote server.
+    ///NOTE that this functionality is essentially equivalent to C-Get in the DICOM standard;
+    ///however, C-Get has been deprecated, so this function allows for the user to ask a remote server
+    ///for files matching a query and return them to the local machine.
+    ///Files will be written to the given output directory.
+    ///If the operation succeeds, the function returns true.
+    ///This function is a prime candidate for being overwritten by expert users; if the datasets
+    ///should remain in memory, for instance, that behavior can be changed by creating a user-level version
+    ///of this function.
+    bool CMove( const char *remote, int portno, std::string const &aetitle,
+                const std::string& call, gdcm::BaseRootQuery* query,
+                int portscp, const std::string& outputdir);
+
+
+    ///This function wil use the provided query to determine what files a remote server contains
+    ///that match the query strings.  The return is a vector of datasets that contain tags as reported
+    ///by the server.  If the dataset is empty, then it is possible that an error condition was encountered;
+    ///in which case, the user should monitor the error and warning streams.
+    std::vector<gdcm::DataSet> CFind( const char *remote, int portno , std::string const &aetitle,
+                                      std::string const &call , gdcm::BaseRootQuery* query );
+
+    ///This function will place the provided files into the remote server.
+    ///The function returns true if it worked.
+    bool CStore( const char *remote, int portno,
+                 std::string const &aetitle, std::string const &call,
+                 std::vector<std::string> const & filenames, bool inRecursive );
+    };
+}
+
+#endif //GDCMCOMPOSITENETWORKFUNCTIONS_H
