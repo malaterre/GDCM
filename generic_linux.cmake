5825a0f3080086918d25a2440325369e04a8113b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-02-10 15:19:24 +0100

    more generic

--- generic_linux.cmake
+++ generic_linux.cmake
@@ -1,7 +1,40 @@
 # Client maintainer: setupgdcm.sh
+cmake_minimum_required(VERSION 2.4)
 
-#set(CTEST_SITE "todo")
-#set(CTEST_BUILD_NAME "todo")
+find_program(HOSTNAME NAMES hostname)
+find_program(UNAME NAMES uname)
+
+# Get the build name and hostname
+exec_program(${HOSTNAME} ARGS OUTPUT_VARIABLE hostname)
+string(REGEX REPLACE "[/\\\\+<> #]" "-" hostname "${hostname}")
+message("HOSTNAME: ${hostname}")
+
+macro(getuname name flag)
+  exec_program("${UNAME}" ARGS "${flag}" OUTPUT_VARIABLE "${name}")
+  string(REGEX REPLACE "[/\\\\+<> #()]" "-" "${name}" "${${name}}")
+  string(REGEX REPLACE "^(......|.....|....|...|..|.).*" "\\1" "${name}" "${${name}}")
+endmacro(getuname)
+
+getuname(osname -s)
+getuname(osver  -v)
+getuname(osrel  -r)
+getuname(cpu    -m)
+if("${osname}" MATCHES Darwin)
+  find_program(SW_VER sw_vers)
+  execute_process(COMMAND "${SW_VER}" -productVersion OUTPUT_VARIABLE osver)
+  string(REPLACE "\n" "" osver "${osver}")
+  set(osname "MacOSX")
+  set(osrel "")
+  if("${cpu}" MATCHES "Power")
+    set(cpu "ppc")
+  endif("${cpu}" MATCHES "Power")
+endif("${osname}" MATCHES Darwin)
+
+set(BUILDNAME "${osname}${osver}${osrel}${cpu}-${compiler}")
+message("BUILDNAME: ${BUILDNAME}")
+
+set(CTEST_SITE "${hostname}")
+set(CTEST_BUILD_NAME "${BUILDNAME}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 macro(dashboard_hook_init)
