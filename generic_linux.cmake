d168e1301e33a0434358d57640f495c7820b98b8
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-02-10 16:07:38 +0100

    Fixing some issues

--- generic_linux.cmake
+++ generic_linux.cmake
@@ -1,5 +1,28 @@
 # Client maintainer: setupgdcm.sh
+#
+# TODO: use cmake --system-information
 cmake_minimum_required(VERSION 2.4)
+
+set(parallel 0)
+if(EXISTS "/proc/cpuinfo")
+  file(STRINGS "/proc/cpuinfo" CPUINFO)
+  foreach(line ${CPUINFO})
+    if("${line}" MATCHES processor)
+      math(EXPR parallel "${parallel} + 1")
+    endif()
+  endforeach(line)
+else()
+  set(parallel 1)
+endif()
+message("parallel: ${parallel}")
+
+#exec_program(${CMAKE_COMMAND} ARGS --system-information
+#OUTPUT_VARIABLE cmake_system_information)
+##execute_process(${CMAKE_COMMAND} --system-information)
+##OUTPUT_VARIABLE cmake_system_information
+##  ERROR_VARIABLE error_cmake_system_information)
+#string(REGEX REPLACE "^CMAKE_SYSTEM_NAME ([A-Za-z])$" "\\1" hostname2 "${cmake_system_information}")
+#message("HOSTNAME2: ${hostname2}")
 
 find_program(HOSTNAME NAMES hostname)
 find_program(UNAME NAMES uname)
@@ -32,10 +55,16 @@ endif("${osname}" MATCHES Darwin)
 
 set(BUILDNAME "${osname}${osver}${osrel}${cpu}-${compiler}")
 message("BUILDNAME: ${BUILDNAME}")
+message("COMPILER: ${CMAKE_CXX_COMPILER}")
 
 set(CTEST_SITE "${hostname}")
 set(CTEST_BUILD_NAME "${BUILDNAME}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+# MM: prefer no space in path:
+set(dashboard_root_name "MyTests")
+SET(CTEST_BUILD_FLAGS "-j${parallel}")
+message("CTEST_BUILD_FLAGS: ${CTEST_BUILD_FLAGS}")
+set(CTEST_BUILD_CONFIGURATION Debug)
 
 macro(dashboard_hook_init)
   set( dashboard_cache "
