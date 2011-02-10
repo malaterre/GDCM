9cb4cd685b0db23332fcd7666f7319afa35b107f
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-02-10 17:33:14 +0100

    Reactive the test TestAllFunctions

--- Testing/Source/MessageExchangeDefinition/Cxx/TestAllFunctions.cxx
+++ Testing/Source/MessageExchangeDefinition/Cxx/TestAllFunctions.cxx
@@ -64,7 +64,7 @@ bool AreDataSetsEqual(const gdcm::DataSet& ds1, const gdcm::DataSet& ds2){
 
 int TestAllFunctions(int argc, char *argv[])
 {
-#if 1
+#if 0
   std::string aetitle("UNITED1");//the ae title of this computer
   std::string call("COMMON");//the ae title of the server
   int portno = 11112;//the port of the server
@@ -85,7 +85,7 @@ int TestAllFunctions(int argc, char *argv[])
   std::string remote = argv[5]; //("192.168.1.4");//the ip address of the remote server
   std::string tmpdir = gdcm::Testing::GetTempDirectory( "TestAllFunctions" );
   std::string outputDir = tmpdir; // ("h:/gdcmtestdataretrievedcmtk");//place to where data is returned by cmove
-  std::string inputDir = tmpdir; //("h:/gdcmtestdataretrievedcmtk");//input collection of data to transfer
+  std::string inputDir = gdcm::Testing::GetDataRoot(); //("h:/gdcmtestdataretrievedcmtk");//input collection of data to transfer
 
 #endif  
   
