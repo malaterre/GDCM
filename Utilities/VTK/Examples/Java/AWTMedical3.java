1dc657ca63ed21462f263d61e43a5b28e53de826
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-02-11 09:23:23 +0100

    Remove until dep to 3rd party lib is fixed

--- Utilities/VTK/Examples/Java/AWTMedical3.java
+++ Utilities/VTK/Examples/Java/AWTMedical3.java
@@ -1,13 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*=========================================================================
 
+  Program: GDCM (Grassroots DICOM). A DICOM library
+  Module:  $URL$
+
+  Copyright (c) 2006-2010 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 package examples;
 
-
 import vtk.*;
-import gdcm.*;
+//import gdcm.*;
 
 import vtk.util.VtkPanelContainer;
 import vtk.util.VtkPanelUtil;
