e5386df1bc59057d311edf9766712fe58f254943
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-02-14 11:33:07 +0100

    Revert copyright to BSD style
    This commit revert changes from commit a5b25efdd9e19bd76982b7e78d0ded69699f942e
    Some files were changed from original BSD to Apache by a different author
    (different from the original author)

--- Source/MessageExchangeDefinition/gdcmPDataTFPDU.h
+++ Source/MessageExchangeDefinition/gdcmPDataTFPDU.h
@@ -1,20 +1,16 @@
 /*=========================================================================
- *
- *  Copyright Insight Software Consortium
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0.txt
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *=========================================================================*/
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+
+  Copyright (c) 2006-2011 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #ifndef GDCMPDATATFPDU_H
 #define GDCMPDATATFPDU_H
 
