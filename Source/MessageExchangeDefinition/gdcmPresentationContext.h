76858e19ec3139d5f6dc88cee88ac64db2b2c128
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-03-01 17:29:45 +0100

    More fixes to testscu

--- Source/MessageExchangeDefinition/gdcmPresentationContext.h
+++ Source/MessageExchangeDefinition/gdcmPresentationContext.h
@@ -61,19 +61,6 @@ public:
 
   void Print(std::ostream &os) const;
 
-  //this function will return the appropriate ID from the above
-  //list, after querying the appropriate tag in the dataset.  If the tag above
-  //does not exist, then the result is a pure verification ID.
-  //if the operation is something other than an echo, that should be interpreted
-  //as a failure; echos themselves take a null dataset.
-  //it is assumed that Find will do the Right Thing, or that the find class is unimportant.
-  //however, this function could be extended in the future to validate that a given
-  //dataset conforms to the expectations of a particular FIND SOP abstract syntax, if necessary.
-  //current tests (7 oct 2010) show that for our test queries, find SOP didn't
-  //change the outcome of the search.
-  //if it's not a verification context ID, then you can also get the UID associated with the data set.
-  //static uint8_t AssignPresentationContextID(const DataSet& inDS, std::string& outUIDString);
-
   bool operator==(const PresentationContext & pc) const
     {
     assert( TransferSyntaxes.size() == 1 ); // TODO
@@ -89,13 +76,12 @@ private:
   static const uint8_t Reserved6;
   static const uint8_t Reserved7;
   static const uint8_t Reserved8;
-/*
-This variable field shall contain the following sub-items: one Abstract
-Syntax and one or more Transfer Syntax(es). For a complete
-description of the use and encoding of these sub-items see Sections
-9.3.2.2.1 and 9.3.2.2.2.
-*/
-
+  /*
+  This variable field shall contain the following sub-items: one Abstract
+  Syntax and one or more Transfer Syntax(es). For a complete
+  description of the use and encoding of these sub-items see Sections
+  9.3.2.2.1 and 9.3.2.2.2.
+   */
   AbstractSyntax SubItems;
   std::vector<TransferSyntaxSub> TransferSyntaxes;
 };
