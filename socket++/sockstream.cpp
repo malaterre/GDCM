e2731c3cc13615089d9a2778a107484110cfe0be
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-03-30 14:36:55 +0200

    Revert "should remove mac inheritance conflict with showmanyc"
    This reverts commit 3da6e6b6f2bdf8d5d77f8738b346f1f2c4b59dc3.

--- socket++/sockstream.cpp
+++ socket++/sockstream.cpp
@@ -455,7 +455,7 @@ int sockbuf::sync ()
     return 0;
 }
 
-streamsize sockbuf::showmanyc_old () const
+streamsize sockbuf::showmanyc () const
 // return the number of chars in the input sequence
 {
     if (gptr () && gptr () < egptr ())
@@ -493,7 +493,7 @@ sockbuf::int_type sockbuf::uflow ()
 
 streamsize sockbuf::xsgetn (char_type* s, streamsize n)
 {
-  std::streamsize rval = showmanyc_old ();
+  std::streamsize rval = showmanyc ();
   if (rval >= n) {
     memcpy (s, gptr (), (size_t)(n * sizeof (char_type)));
     gbump ((int)n);
@@ -1045,7 +1045,7 @@ long sockbuf::howmanyc () const
 // return how many chars are available for reading in the input buffer
 // and the recvbuf of the socket.
 {
-  std::streamsize theShowMany = showmanyc_old();
+  std::streamsize theShowMany = showmanyc();
   assert (theShowMany < INT_MAX);
   return (long)theShowMany + nread ();
 }
