8e16219c0018e4b38cdf02198da0544148657267
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-05-30 18:51:34 +0200

    Minor tweaking of java documentation

--- Examples/Java/HelloSimple.java
+++ Examples/Java/HelloSimple.java
@@ -13,10 +13,10 @@
 =========================================================================*/
 /*
  * Compilation:
- * $ CLASSPATH=/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/gdcm.jar javac HelloSimple.java
+ * $ CLASSPATH=gdcm.jar javac ../../gdcm/Examples/Java/HelloSimple.java -d .
  *
  * Usage:
- * $ LD_LIBRARY_PATH=/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/ CLASSPATH=/home/mmalaterre/Projects/gdcm/debug-gcc43/bin/gdcm.jar:. java HelloSimple ~/Creatis/gdcmData/012345.002.050.dcm
+ * $ LD_LIBRARY_PATH=. CLASSPATH=gdcm.jar:. java HelloSimple gdcmData/012345.002.050.dcm
  */
 import gdcm.*;
 
