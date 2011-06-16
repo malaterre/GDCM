e18826f46ffb9808d8f5938c16d7c89b5e77cd66
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-06-16 21:56:46 +0200

    Now that gdcmData contains two new files, update hash map

--- Testing/Source/MediaStorageAndFileFormat/Cxx/TestImageReader.cxx
+++ Testing/Source/MediaStorageAndFileFormat/Cxx/TestImageReader.cxx
@@ -41,6 +41,7 @@ int TestImageRead(const char* filename, bool verbose = false, bool lossydump = f
     if( reflossy == -1 )
       {
       std::cerr << "Missing lossy flag for: " << filename << std::endl;
+      return 1;
       }
     if( img.IsLossy() != (reflossy > 0 ? true : false)  )//vs10 has a stupid bool/int cast warning
       {
@@ -51,7 +52,7 @@ int TestImageRead(const char* filename, bool verbose = false, bool lossydump = f
     bool res2 = img.GetBuffer(buffer);
     if( !res2 )
       {
-      std::cerr << "res2 failure:" << filename << std::endl;
+      std::cerr << "res2 failure: " << filename << std::endl;
       return 1;
       }
     // On big Endian system we have byteswapped the buffer (duh!)
