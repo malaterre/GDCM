d9e834d74db3e63ceeb7453628ea2375c1c99c64
Jordi ROMERA <jordiromera@users.sourceforge.net>
2011-07-12 14:23:41 +0200

    Added data for SurfaceSegmentationStorage

---
+++ Source/MediaStorageAndFileFormat/gdcmMeshPrimitive.cxx
@@ -0,0 +1,63 @@
+#include "gdcmMeshPrimitive.h"
+#include "gdcmCodeString.h"
+
+#include <cstring>
+
+namespace gdcm
+{
+
+static const char * MPStrings[] = {
+  "VERTEX",
+  "EDGE",
+  "TRIANGLE",
+  "TRIANGLESTRIP",
+  "TRIANGLEFAN",
+  "LINE",
+  "FACET",
+  0
+};
+
+const char * MeshPrimitive::GetMPTypeString(const MPType type)
+{
+  assert( type <= MPType_END );
+  return MPStrings[(int)type];
+}
+
+MeshPrimitive::MPType MeshPrimitive::GetMPType(const char * type)
+{
+  if(!type) return MPType_END;
+
+  for(unsigned int i = 0; MPStrings[i] != 0; ++i)
+  {
+    if( strcmp(type, MPStrings[i]) == 0 )
+    {
+      return (MPType)i;
+    }
+  }
+  // Ouch ! We did not find anything, that's pretty bad, let's hope that
+  // the toolkit which wrote the image is buggy and tolerate space padded binary
+  // string
+  CodeString  codestring  = type;
+  std::string cs          = codestring.GetAsString();
+  for(unsigned int i = 0; MPStrings[i] != 0; ++i)
+  {
+    if( strcmp(cs.c_str(), MPStrings[i]) == 0 )
+    {
+      return (MPType)i;
+    }
+  }
+
+  return MPType_END;
+}
+
+MeshPrimitive::MeshPrimitive():
+  PrimitiveData(),
+  PrimitiveType(MPType_END)
+{
+}
+
+MeshPrimitive::~MeshPrimitive()
+{
+}
+
+}
