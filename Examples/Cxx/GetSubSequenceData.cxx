e2887e64c50899315b613cd8f2c8ac61bec33b10
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-07-14 11:33:22 +0200

    Finish up the GetSubSequenceData example

--- Examples/Cxx/GetSubSequenceData.cxx
+++ Examples/Cxx/GetSubSequenceData.cxx
@@ -23,186 +23,14 @@
 
 #include <map>
 
-bool PrintNameValueMapping( gdcm::SequenceOfItems *sqi_values, gdcm::SequenceOfItems *sqi_names )
-{
-//  std::cout << "PrintNameValueMapping" << std::endl;
-  using namespace gdcm;
-  // prepare names mapping:
-  typedef VRToType<VR::UL>::Type UL;
-  std::map< UL, std::string > names;
-  assert( sqi_names );
-  assert( sqi_values );
-  SequenceOfItems::SizeType s = sqi_names->GetNumberOfItems();
-  PrivateTag tindex(0x7fe1,0x71,"GEMS_Ultrasound_MovieGroup_001");
-  PrivateTag tname (0x7fe1,0x72,"GEMS_Ultrasound_MovieGroup_001");
-  // First sequence contains all possible names (this is a dict)
-  for( SequenceOfItems::SizeType i = 1; i <= s; ++i )
-    {
-    const Item & item = sqi_names->GetItem( i );
-    const DataSet & ds = item.GetNestedDataSet();
-    if( !ds.FindDataElement( tindex )
-      || !ds.FindDataElement( tname ) )
-      {
-      return false;
-      }
-    const DataElement & index = ds.GetDataElement( tindex );
-    const DataElement & name = ds.GetDataElement( tname );
-    if( index.IsEmpty() || name.IsEmpty() )
-      {
-      return false;
-      }
-    gdcm::Element<VR::UL, VM::VM1> el1;
-    el1.SetFromDataElement( index );
-
-    gdcm::Element<VR::LO, VM::VM1> el2;
-    el2.SetFromDataElement( name );
-//    std::cout << el1.GetValue() << " " << el2.GetValue() << std::endl;
-    names.insert( std::make_pair< UL, std::string > ( el1.GetValue(), el2.GetValue() ) );
-    }
-
-  SequenceOfItems::SizeType s2 = sqi_values->GetNumberOfItems();
-  assert( s2 <= s );
-  PrivateTag tindex2(0x7fe1,0x48,"GEMS_Ultrasound_MovieGroup_001");
-  for( SequenceOfItems::SizeType i = 1; i <= s2; ++i )
-    {
-    const Item & item = sqi_values->GetItem( i );
-    const DataSet & ds = item.GetNestedDataSet();
-    if( !ds.FindDataElement( tindex2 ) )
-      {
-      return false;
-      }
-    const DataElement & index2 = ds.GetDataElement( tindex2 );
-    if( index2.IsEmpty() )
-      {
-      return false;
-      }
-    gdcm::Element<VR::FD, VM::VM1_2> el1;
-    el1.SetFromDataElement( index2 );
-
-    UL copy = el1.GetValue();
-    std::cout << "( " << names[ copy ];
-    // (7fe1,1052) FD 1560                                       # 8,1 ?
-    // (7fe1,1057) LT [MscSkelSup]                               # 10,1 ?
-    //PrivateTag tvalue(0x7fe1,0x52,"GEMS_Ultrasound_MovieGroup_001");
-    PrivateTag tvalueint(0x7fe1,0x49,"GEMS_Ultrasound_MovieGroup_001"); // UL
-    PrivateTag tvaluefloat1(0x7fe1,0x51,"GEMS_Ultrasound_MovieGroup_001"); // FL
-    PrivateTag tvaluefloat(0x7fe1,0x52,"GEMS_Ultrasound_MovieGroup_001"); // FD
-    PrivateTag tvalueul(0x7fe1,0x53,"GEMS_Ultrasound_MovieGroup_001"); // UL
-    PrivateTag tvaluesl(0x7fe1,0x54,"GEMS_Ultrasound_MovieGroup_001"); // SL
-    PrivateTag tvalueob(0x7fe1,0x55,"GEMS_Ultrasound_MovieGroup_001"); // OB
-    PrivateTag tvaluetext(0x7fe1,0x57,"GEMS_Ultrasound_MovieGroup_001");  // LT
-    PrivateTag tvaluefd(0x7fe1,0x77,"GEMS_Ultrasound_MovieGroup_001");  // FD / 1-N
-    PrivateTag tvaluesl3(0x7fe1,0x79,"GEMS_Ultrasound_MovieGroup_001");  // SL / 1-N
-    PrivateTag tvaluesl2(0x7fe1,0x86,"GEMS_Ultrasound_MovieGroup_001");  // SL ??
-    PrivateTag tvaluefd1(0x7fe1,0x87,"GEMS_Ultrasound_MovieGroup_001");  // FD / 1-N
-    PrivateTag tvaluefloat2(0x7fe1,0x88,"GEMS_Ultrasound_MovieGroup_001");  // FD ??
-    std::cout << " ) = ";
-    if( ds.FindDataElement( tvalueint ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvalueint );
-      gdcm::Element<VR::UL,VM::VM1> el2;
-      el2.SetFromDataElement( value );
-      std::cout << el2.GetValue() << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluefloat1 ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluefloat1 );
-      gdcm::Element<VR::FL,VM::VM1> el2;
-      el2.SetFromDataElement( value );
-      std::cout << el2.GetValue() << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluefloat ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluefloat );
-      gdcm::Element<VR::FD,VM::VM1> el2;
-      el2.SetFromDataElement( value );
-      std::cout << el2.GetValue() << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluesl ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluesl );
-      gdcm::Element<VR::SL,VM::VM1> el2;
-      el2.SetFromDataElement( value );
-      std::cout << el2.GetValue() << std::endl;
-      }
-    else if( ds.FindDataElement( tvalueul ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvalueul );
-      gdcm::Element<VR::UL,VM::VM1_n> el2;
-      el2.SetFromDataElement( value );
-      assert( el2.GetLength() == 1 );
-      std::cout << el2.GetValue() << std::endl;
-      }
-    else if( ds.FindDataElement( tvalueob ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvalueob );
-//      gdcm::Element<VR::SL,VM::VM1> el2;
-//      el2.SetFromDataElement( value );
-//      std::cout << el2.GetValue() << std::endl;
-      std::cout << value << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluetext ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluetext );
-      gdcm::Element<VR::LT,VM::VM1> el2;
-      el2.SetFromDataElement( value );
-      std::cout << el2.GetValue() << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluesl2 ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluesl2 );
-      gdcm::Element<VR::SL,VM::VM1_n> el2;
-      el2.SetFromDataElement( value );
-      el2.Print( std::cout );
-      assert( el2.GetLength() == 4 );
-      std::cout << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluesl3 ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluesl3 );
-      gdcm::Element<VR::SL,VM::VM1_n> el2;
-      el2.SetFromDataElement( value );
-      el2.Print( std::cout );
-//      assert( el2.GetLength() == 4 );
-      std::cout << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluefd ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluefd );
-      gdcm::Element<VR::FD,VM::VM1_n> el2;
-      el2.SetFromDataElement( value );
-      el2.Print( std::cout );
-//      assert( el2.GetLength() == 4 || el2.GetLength() == 3 || el2.GetLength() == 8 );
-      std::cout << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluefloat2 ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluefloat2 );
-      gdcm::Element<VR::FD,VM::VM1_n> el2;
-      el2.SetFromDataElement( value );
-      el2.Print( std::cout );
-      assert( el2.GetLength() == 2 );
-      std::cout << std::endl;
-      }
-    else if( ds.FindDataElement( tvaluefd1 ) )
-      {
-      const DataElement & value = ds.GetDataElement( tvaluefd1 );
-      gdcm::Element<VR::FD,VM::VM1_n> el2;
-      el2.SetFromDataElement( value );
-      el2.Print( std::cout );
-      assert( el2.GetLength() == 4 );
-      std::cout << std::endl;
-      }
-    else
-      {
-      std::cout << "(no value)" << std::endl;
-//      std::cout << ds << std::endl;
-      assert( ds.Size() == 2 );
-      }
-    }
-  return true;
-}
-
+/*
+ * This example will extract the Movie from the private group of
+ * GEMS_Ultrasound_MovieGroup_001 See Attribute
+ * (7fe1,60,GEMS_Ultrasound_MovieGroup_001)
+ *
+ * The output file will be stored in `outvid.dcm` as
+ * MultiframeGrayscaleByteSecondaryCaptureImageStorage
+ */
 int main(int argc, char *argv[])
 {
   if( argc < 2 ) return 1;
@@ -224,18 +52,6 @@ int main(int argc, char *argv[])
   Item &item = sqi->GetItem(1);
   DataSet &subds = item.GetNestedDataSet();
 
-  const PrivateTag tseq_values(0x7fe1,0x8,"GEMS_Ultrasound_MovieGroup_001");
-  if( !subds.FindDataElement( tseq_values ) ) return 1;
-  const DataElement& seq_values = subds.GetDataElement( tseq_values );
-  SmartPointer<SequenceOfItems> sqi_values = seq_values.GetValueAsSQ();
-
-  const PrivateTag tseq_names(0x7fe1,0x70,"GEMS_Ultrasound_MovieGroup_001");
-  if( !subds.FindDataElement( tseq_names ) ) return 1;
-  const DataElement& seq_names = subds.GetDataElement( tseq_names );
-  SmartPointer<SequenceOfItems> sqi_names = seq_names.GetValueAsSQ();
-
-  PrintNameValueMapping( sqi_values, sqi_names );
-
   const PrivateTag tseq1(0x7fe1,0x10,"GEMS_Ultrasound_MovieGroup_001");
 
   if( !subds.FindDataElement( tseq1 ) ) return 1;
@@ -248,13 +64,6 @@ int main(int argc, char *argv[])
   DataSet &subds2 = item2.GetNestedDataSet();
 
   const PrivateTag tseq2(0x7fe1,0x20,"GEMS_Ultrasound_MovieGroup_001");
-  const PrivateTag tseq_values2(0x7fe1,0x18,"GEMS_Ultrasound_MovieGroup_001");
-  if( !subds2.FindDataElement( tseq_values2 ) ) return 1;
-  const DataElement& seq_values2 = subds2.GetDataElement( tseq_values2 );
-  SmartPointer<SequenceOfItems> sqi_values2 = seq_values2.GetValueAsSQ();
-
-  std::cout << std::endl;
-  PrintNameValueMapping( sqi_values2, sqi_names );
 
   if( !subds2.FindDataElement( tseq2 ) ) return 1;
   const DataElement& seq2 = subds2.GetDataElement( tseq2 );
@@ -267,79 +76,101 @@ int main(int argc, char *argv[])
   Item &item3 = sqi3->GetItem(1);
   DataSet &subds3 = item3.GetNestedDataSet();
 
-  const PrivateTag tseq3(0x7fe1,0x36,"GEMS_Ultrasound_MovieGroup_001");
-  for( int i = 1; i <= ni3; ++i )
+  const PrivateTag tseq6(0x7fe1,0x26,"GEMS_Ultrasound_MovieGroup_001");
+  if( !subds3.FindDataElement( tseq6 ) ) return 1;
+  const DataElement& seq6 = subds3.GetDataElement( tseq6 );
+  SmartPointer<SequenceOfItems> sqi6 = seq6.GetValueAsSQ();
+  int ni6= sqi6->GetNumberOfItems();
+  assert( sqi6->GetNumberOfItems() >= 1 );
+  const PrivateTag tseq7(0x7fe1,0x86,"GEMS_Ultrasound_MovieGroup_001");
+  int dimx, dimy;
+  for( int i6 = 1; i6 <= ni6; ++i6 )
     {
-    Item &item3 = sqi3->GetItem(i);
-    DataSet &subds3 = item3.GetNestedDataSet();
+    Item &item6 = sqi6->GetItem(i6);
+    DataSet &subds6 = item6.GetNestedDataSet();
 
-    const PrivateTag tseq_values3(0x7fe1,0x26,"GEMS_Ultrasound_MovieGroup_001");
-    std::cout << tseq_values3 << std::endl;
-    if( !subds3.FindDataElement( tseq_values3 ) ) return 1;
-    const DataElement& seq_values3 = subds3.GetDataElement( tseq_values3 );
-    SmartPointer<SequenceOfItems> sqi_values3 = seq_values3.GetValueAsSQ();
-
-    std::cout << std::endl;
-    PrintNameValueMapping( sqi_values3, sqi_names );
+    if( subds6.FindDataElement( tseq7 ) )
+      {
+      Element<VR::SL, VM::VM4> el;
+      el.SetFromDataElement( subds6.GetDataElement( tseq7 ) );
+      std::cout << "El= " << el.GetValue() << std::endl;
+      dimx = el.GetValue(0);
+      dimy = el.GetValue(1);
+      }
     }
 
+  const PrivateTag tseq3(0x7fe1,0x36,"GEMS_Ultrasound_MovieGroup_001");
   if( !subds3.FindDataElement( tseq3 ) ) return 1;
   const DataElement& seq3 = subds3.GetDataElement( tseq3 );
 
 //    std::cout << seq3 << std::endl;
 
   SmartPointer<SequenceOfItems> sqi4 = seq3.GetValueAsSQ();
-  //int ni4= sqi4->GetNumberOfItems();
+  int ni4= sqi4->GetNumberOfItems();
   assert( sqi4->GetNumberOfItems() >= 1 );
-  Item &item4 = sqi4->GetItem(1);
-  DataSet &subds4 = item4.GetNestedDataSet();
-
+  const PrivateTag tseq8(0x7fe1,0x37,"GEMS_Ultrasound_MovieGroup_001");
   const PrivateTag tseq4(0x7fe1,0x43,"GEMS_Ultrasound_MovieGroup_001");
   const PrivateTag tseq5(0x7fe1,0x60,"GEMS_Ultrasound_MovieGroup_001");
 
-  if( !subds4.FindDataElement( tseq4 ) ) return 1;
-  const DataElement& seq4 = subds4.GetDataElement( tseq4 );
-  if( !subds4.FindDataElement( tseq5 ) ) return 1;
-  const DataElement& seq5 = subds4.GetDataElement( tseq5 );
+  std::vector<char> imbuffer;
+  int dimz = 0;
+  for( int i4 = 1; i4 <= ni4; ++i4 )
+    {
+    Item &item4 = sqi4->GetItem(i4);
+    DataSet &subds4 = item4.GetNestedDataSet();
 
-//    std::cout << seq4 << std::endl;
-//    std::cout << seq5 << std::endl;
+    if( !subds4.FindDataElement( tseq8 ) ) return 1;
+    const DataElement& de8 = subds4.GetDataElement( tseq8 );
+    Element<VR::UL,VM::VM1> ldimz;
+    ldimz.SetFromDataElement( de8 );
+    dimz += ldimz.GetValue();
+    if( !subds4.FindDataElement( tseq4 ) ) return 1;
+    const DataElement& seq4 = subds4.GetDataElement( tseq4 );
+    if( !subds4.FindDataElement( tseq5 ) ) return 1;
+    const DataElement& seq5 = subds4.GetDataElement( tseq5 );
 
-#if 1
-  const ByteValue *bv4 = seq4.GetByteValue();
-{
-  std::ofstream out( "/tmp/mo4" );
-  out.write( bv4->GetPointer(), bv4->GetLength());
-  out.close();
-}
-#endif
+    //    std::cout << seq4 << std::endl;
+    //    std::cout << seq5 << std::endl;
 
+    const ByteValue *bv4 = seq4.GetByteValue();
 #if 0
-  const ByteValue *bv5 = seq5.GetByteValue();
-{
-  std::ofstream out( "/tmp/mo5" );
-  out.write( bv5->GetPointer(), bv5->GetLength());
-  out.close();
-}
+      {
+      std::ofstream out( "/tmp/mo4" );
+      out.write( bv4->GetPointer(), bv4->GetLength());
+      out.close();
+      }
 #endif
+  const ByteValue *bv5 = seq5.GetByteValue();
+#if 0
+    {
+    std::ofstream out( "/tmp/mo5" );
+    out.write( bv5->GetPointer(), bv5->GetLength());
+    out.close();
+    }
+#endif
+
+    std::cout << bv5->GetLength() << std::endl;
+    imbuffer.insert( imbuffer.begin(), bv5->GetPointer(), bv5->GetPointer() + bv5->GetLength() );
+    }
+  DataElement fakedata;
+  fakedata.SetByteValue( &imbuffer[0], imbuffer.size() );
+
 
   gdcm::SmartPointer<gdcm::Image> im = new gdcm::Image;
   im->SetNumberOfDimensions( 3 );
-  // (7fe1,1086) SL 440\129\0\2                              #  16, 4 Unknown Tag & Data
 
-  im->SetDimension(0, 655 );
-  im->SetDimension(1, 202 );
-  im->SetDimension(2, 100 );
+  im->SetDimension(0, dimx );
+  im->SetDimension(1, dimy );
+  im->SetDimension(2, dimz );
+  size_t l1 = imbuffer.size();
+  size_t l2 = im->GetBufferLength();
+  assert( im->GetBufferLength() == imbuffer.size() );
   im->SetPhotometricInterpretation( gdcm::PhotometricInterpretation::MONOCHROME2 );
 
-//  gdcm::DataElement pixeldata( gdcm::Tag(0x7fe0,0x0010) );
-//  pixeldata.SetByteValue( bv5->GetPointer(), l );
-//  im->SetDataElement( pixeldata );
-  im->SetDataElement( seq5 );
+  im->SetDataElement( fakedata );
 
   gdcm::ImageWriter w;
   w.SetImage( *im );
-//  w.SetFile( fd.GetFile() );
   DataSet &dataset = w.GetFile().GetDataSet();
 
   gdcm::UIDGenerator uid;
