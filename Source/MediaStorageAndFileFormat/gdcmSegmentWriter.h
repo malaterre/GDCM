550e34327a1ce201aa43dd5beef0a118426a8b53
Jordi ROMERA <jordiromera@users.sourceforge.net>
2011-07-22 17:21:14 +0200

    Splited implementation with interface

--- Source/MediaStorageAndFileFormat/gdcmSegmentWriter.h
+++ Source/MediaStorageAndFileFormat/gdcmSegmentWriter.h
@@ -20,20 +20,16 @@ public:
   bool Write(); // Set to protected ?
 
   //**        Segment getters/setters     **//
-  const unsigned int GetNumberOfSegments() const { return Segments.size(); }
-  void SetNumberOfSegments(const unsigned int size) { Segments.resize(size); }
+  const unsigned int GetNumberOfSegments() const;
+  void SetNumberOfSegments(const unsigned int size);
 
-  const SegmentVector & GetSegments() const { return Segments; }
-  SegmentVector & GetSegments() { return Segments; }
-  SmartPointer< Segment > GetSegment(const unsigned int idx=0) const
-  {
-    assert( idx < Segments.size() );
-    return Segments[idx];
-  }
-  void AddSegment(SmartPointer< Segment > segment)
-  {
-    Segments.push_back(segment);
-  }
+  const SegmentVector & GetSegments() const;
+  SegmentVector & GetSegments();
+  SmartPointer< Segment > GetSegment(const unsigned int idx = 0) const;
+
+  void AddSegment(SmartPointer< Segment > segment);
+
+  void SetSegments(SegmentVector & segments);
 
 protected:
 
