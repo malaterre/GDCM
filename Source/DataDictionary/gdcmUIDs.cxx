malaterre/GDCM

commit bdeeb3822bc9ce61c05b0ba3751960f0212d7db0

Start adding the minimal information to process WholeSlideImaging

Source/DataDictionary/gdcmUIDs.cxx
@@ -197,6 +197,7 @@ namespace gdcm
 {"1.2.840.10008.5.1.4.1.1.77.1.5.2","Ophthalmic Photography 16 Bit Image Storage"},
 {"1.2.840.10008.5.1.4.1.1.77.1.5.3","Stereometric Relationship Storage"},
 {"1.2.840.10008.5.1.4.1.1.77.1.5.4","Ophthalmic Tomography Image Storage"},
+{"1.2.840.10008.5.1.4.1.1.77.1.6","VL Whole Slide Microscopy Image Storage"},
 {"1.2.840.10008.5.1.4.1.1.88.1","Text SR Storage - Trial"},
 {"1.2.840.10008.5.1.4.1.1.88.2","Audio SR Storage - Trial"},
 {"1.2.840.10008.5.1.4.1.1.88.3","Detail SR Storage - Trial"},

Source/DataDictionary/gdcmUIDs.h
@@ -203,6 +203,7 @@ uid_1_2_840_10008_5_1_4_1_1_77_1_5_1 = 169, // Ophthalmic Photography 8 Bit Imag
 uid_1_2_840_10008_5_1_4_1_1_77_1_5_2 = 170, // Ophthalmic Photography 16 Bit Image Storage
 uid_1_2_840_10008_5_1_4_1_1_77_1_5_3 = 171, // Stereometric Relationship Storage
 uid_1_2_840_10008_5_1_4_1_1_77_1_5_4 = 172, // Ophthalmic Tomography Image Storage
+uid_1_2_840_10008_5_1_4_1_1_77_1_6, // VL Whole Slide Microscopy Image Storage
 uid_1_2_840_10008_5_1_4_1_1_88_1 = 173, // Text SR Storage - Trial
 uid_1_2_840_10008_5_1_4_1_1_88_2 = 174, // Audio SR Storage - Trial
 uid_1_2_840_10008_5_1_4_1_1_88_3 = 175, // Detail SR Storage - Trial

Source/DataDictionary/DICOMV3.xml
@@ -1758,6 +1758,8 @@ This file was generated using the following commands:
   <entry group="0046" element="0145" keyword="ReferencedRefractiveMeasurementsSequence" vr="SQ" vm="1" name="Referenced Refractive Measurements Sequence"/>
   <entry group="0046" element="0146" keyword="SpherePower" vr="FD" vm="1" name="Sphere Power"/>
   <entry group="0046" element="0147" keyword="CylinderPower" vr="FD" vm="1" name="Cylinder Power"/>
+  <entry group="0048" element="0006" keyword="TotalPixelMatrixColumns" vr="UL" vm="1" name="Total Pixel Matrix Columns"/>
+  <entry group="0048" element="0007" keyword="TotalPixelMatrixRows" vr="UL" vm="1" name="Total Pixel Matrix Rows"/>
   <entry group="0050" element="0004" keyword="CalibrationImage" vr="CS" vm="1" name="Calibration Image"/>
   <entry group="0050" element="0010" keyword="DeviceSequence" vr="SQ" vm="1" name="Device Sequence"/>
   <entry group="0050" element="0012" keyword="ContainerComponentTypeCodeSequence" vr="SQ" vm="1" name="Container Component Type Code Sequence"/>

Source/DataDictionary/gdcmTagToType.h
@@ -11980,6 +11980,20 @@ enum { VRType = VR::FD };
 enum { VMType = VM::VM1 };
 static const char* GetVMString() { return "1"; }
 };
+template <> struct TagToType<0x0048,0x0006> {
+static const char* GetVRString() { return "UL"; }
+typedef VRToType<VR::UL>::Type Type;
+enum { VRType = VR::UL };
+enum { VMType = VM::VM1 };
+static const char* GetVMString() { return "1"; }
+};
+template <> struct TagToType<0x0048,0x0007> {
+static const char* GetVRString() { return "UL"; }
+typedef VRToType<VR::UL>::Type Type;
+enum { VRType = VR::UL };
+enum { VMType = VM::VM1 };
+static const char* GetVMString() { return "1"; }
+};
 template <> struct TagToType<0x0050,0x0004> {
 static const char* GetVRString() { return "CS"; }
 typedef VRToType<VR::CS>::Type Type;

Source/DataStructureAndEncodingDefinition/gdcmMediaStorage.cxx
@@ -122,6 +122,7 @@ static const char *MSStrings[] = {
   "1.2.840.10008.5.1.4.1.1.13.1.1", // XRay3DAngiographicImageStorage,
   "1.2.840.10008.5.1.4.1.1.12.1.1", // Enhanced XA Image Storage
   "1.2.840.10008.5.1.4.1.1.481.9", //  RTIonBeamsTreatmentRecordStorage
+  "1.2.840.10008.5.1.4.1.1.77.1.6", // VLWholeSlideMicroscopyImageStorage
   0
 };
 

Source/DataStructureAndEncodingDefinition/gdcmMediaStorage.h
@@ -114,6 +114,7 @@ public:
     XRay3DAngiographicImageStorage, // 1.2.840.10008.5.1.4.1.1.13.1.1
     EnhancedXAImageStorage,
     RTIonBeamsTreatmentRecordStorage, // 1.2.840.10008.5.1.4.1.1.481.9
+    VLWholeSlideMicroscopyImageStorage, // 1.2.840.10008.5.1.4.1.1.77.1.6
     MS_END
   } MSType; // Media Storage Type
 

Source/DataStructureAndEncodingDefinition/gdcmTagToVR.cxx
@@ -1751,6 +1751,8 @@ case 0x00460139:return VR::LO;
 case 0x00460145:return VR::SQ;
 case 0x00460146:return VR::FD;
 case 0x00460147:return VR::FD;
+case 0x00480006:return VR::UL;
+case 0x00480007:return VR::UL;
 case 0x00500004:return VR::CS;
 case 0x00500010:return VR::SQ;
 case 0x00500012:return VR::SQ;

commit 45f377008a979e1da84ad2057f4a967d1d5c112f

Fix regression in CFind
The commit bdeeb3822bc9ce61c05b0ba3751960f0212d7db0 introduced a regression as it shift every enum by one

Source/DataDictionary/gdcmUIDs.cxx
@@ -195,7 +195,6 @@ namespace gdcm
 {"1.2.840.10008.5.1.4.1.1.77.1.5.2","Ophthalmic Photography 16 Bit Image Storage"},
 {"1.2.840.10008.5.1.4.1.1.77.1.5.3","Stereometric Relationship Storage"},
 {"1.2.840.10008.5.1.4.1.1.77.1.5.4","Ophthalmic Tomography Image Storage"},
-{"1.2.840.10008.5.1.4.1.1.77.1.6","VL Whole Slide Microscopy Image Storage"},
 {"1.2.840.10008.5.1.4.1.1.88.1","Text SR Storage - Trial"},
 {"1.2.840.10008.5.1.4.1.1.88.2","Audio SR Storage - Trial"},
 {"1.2.840.10008.5.1.4.1.1.88.3","Detail SR Storage - Trial"},
@@ -292,6 +291,7 @@ namespace gdcm
 {"1.2.840.10008.15.0.4.6","dicomNetworkConnection"},
 {"1.2.840.10008.15.0.4.7","dicomUniqueAETitle"},
 {"1.2.840.10008.15.0.4.8","dicomTransferCapability"},
+{"1.2.840.10008.5.1.4.1.1.77.1.6","VL Whole Slide Microscopy Image Storage"},
 //{"1.2.840.10008.1.4.1.1","Talairach Brain Atlas Frame of Reference"},
 //{"1.2.840.10008.1.4.1.2","SPM2 T1 Frame of Reference"},
 //{"1.2.840.10008.1.4.1.3","SPM2 T2 Frame of Reference"},

Source/DataDictionary/gdcmUIDs.h
@@ -203,7 +203,6 @@ uid_1_2_840_10008_5_1_4_1_1_77_1_5_1 = 169, // Ophthalmic Photography 8 Bit Imag
 uid_1_2_840_10008_5_1_4_1_1_77_1_5_2 = 170, // Ophthalmic Photography 16 Bit Image Storage
 uid_1_2_840_10008_5_1_4_1_1_77_1_5_3 = 171, // Stereometric Relationship Storage
 uid_1_2_840_10008_5_1_4_1_1_77_1_5_4 = 172, // Ophthalmic Tomography Image Storage
-uid_1_2_840_10008_5_1_4_1_1_77_1_6, // VL Whole Slide Microscopy Image Storage
 uid_1_2_840_10008_5_1_4_1_1_88_1 = 173, // Text SR Storage - Trial
 uid_1_2_840_10008_5_1_4_1_1_88_2 = 174, // Audio SR Storage - Trial
 uid_1_2_840_10008_5_1_4_1_1_88_3 = 175, // Detail SR Storage - Trial
@@ -299,7 +298,7 @@ uid_1_2_840_10008_15_0_4_4 = 264, // dicomDevice
 uid_1_2_840_10008_15_0_4_5 = 265, // dicomNetworkAE
 uid_1_2_840_10008_15_0_4_6 = 266, // dicomNetworkConnection
 uid_1_2_840_10008_15_0_4_7 = 267, // dicomUniqueAETitle
-uid_1_2_840_10008_15_0_4_8 = 268 // dicomTransferCapability
+uid_1_2_840_10008_15_0_4_8 = 268, // dicomTransferCapability
 //frameref_1_2_840_10008_1_4_1_1 = 269, // Talairach Brain Atlas Frame of Reference
 //frameref_1_2_840_10008_1_4_1_2 = 270, // SPM2 T1 Frame of Reference
 //frameref_1_2_840_10008_1_4_1_3 = 271, // SPM2 T2 Frame of Reference
@@ -329,6 +328,7 @@ uid_1_2_840_10008_15_0_4_8 = 268 // dicomTransferCapability
 //uid_1_2_840_113543_6_6_1_3_10002 = 506,
 //uid_1_2_392_200036_9116_7_8_1_1_1 = 507,
 //uid_end = 1000
+uid_1_2_840_10008_5_1_4_1_1_77_1_6 // VL Whole Slide Microscopy Image Storage
 } TSType;
   typedef enum {
 VerificationSOPClass = 1, // Verification SOP Class
@@ -598,7 +598,7 @@ dicomDevice = 264, // dicomDevice
 dicomNetworkAE = 265, // dicomNetworkAE
 dicomNetworkConnection = 266, // dicomNetworkConnection
 dicomUniqueAETitle = 267, // dicomUniqueAETitle
-dicomTransferCapability = 268 // dicomTransferCapability
+dicomTransferCapability = 268, // dicomTransferCapability
 //TalairachBrainAtlasFrameofReference = 269, // Talairach Brain Atlas Frame of Reference
 //SPM2T1FrameofReference = 270, // SPM2 T1 Frame of Reference
 //SPM2T2FrameofReference = 271, // SPM2 T2 Frame of Reference
@@ -619,6 +619,7 @@ dicomTransferCapability = 268 // dicomTransferCapability
 //SPM2SINGLESUBJT1FrameofReference = 286, // SPM2 SINGLESUBJT1 Frame of Reference
 //ICBM452T1FrameofReference = 287, // ICBM 452 T1 Frame of Reference
 //ICBMSingleSubjectMRIFrameofReference = 288, // ICBM Single Subject MRI Frame of Reference
+VLWholeSlideMicroscopyImageStorage
 } TSName;
 
 