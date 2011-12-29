f3299bad3b4e32ac327604a6e296ad7a9930bf3c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-12-29 17:15:50 +0100

    Fix bug #3397339
    This is rather important fix. There is a corner case where DICOM file could be encoded in non-lossy form
    So the only way to deduce the stream was at one point in time lossy is to rely on the Lossy Flag Information

--- Source/MediaStorageAndFileFormat/gdcmBitmap.h
+++ Source/MediaStorageAndFileFormat/gdcmBitmap.h
@@ -153,6 +153,8 @@ protected:
 
   friend class PixmapReader;
   friend class ImageChangeTransferSyntax;
+  // Function to compute the lossy flag based only on the image buffer.
+  // Watch out that image can be lossy but in implicit little endian format...
   bool ComputeLossyFlag();
 
 //private:
