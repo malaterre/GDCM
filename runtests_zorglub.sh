a55849f4be0fa21fc2745c8b41fe33b1afdc1480
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2012-03-21 18:55:41 +0100

    Fix config for new zorglub machine

--- runtests_zorglub.sh
+++ runtests_zorglub.sh
@@ -2,8 +2,8 @@
 
 cd $HOME/Dashboards/GDCMScripts && git pull
 
-export PATH=$PATH:$HOME/local/bin
-export JAVA_HOME=/opt/jdk
+#export PATH=$HOME/Software/cmake-2.8.2-Linux-i386/bin:$PATH:$HOME/local/bin
+#export JAVA_HOME=/opt/jdk
 
 echo "Starting dcmqrscp"
 
@@ -25,19 +25,3 @@ echo "Stopping dcmqrscp"
 
 termscu --aetitle GDCMDASH --call GDCM_STORE $HOSTNAME 5678
 
-# Nightly doxygen:
-#cd $HOME/Dashboards/MyTests/gdcm-nightly && touch Utilities/doxygen/Doxyfile
-#cd $HOME/Dashboards/MyTests/gdcm-nightly && make -j8
-#cd $HOME/Dashboards/MyTests/gdcm-nightly && make rebuild_cache && make vtkgdcmDoxygenDoc
-##cd $HOME/Dashboards/MyTests/gdcm-nightly/Utilities/doxygen/vtk && make vtkgdcmDoxygenDoc
-##cd $HOME/Dashboards/MyTests/gdcm-nightly
-#rsync --quiet -av Utilities/doxygen/gdcm-2.1.0-doc.tar.gz malat,gdcm@web.sourceforge.net:htdocs
-#rsync --quiet -av -r Utilities/doxygen/html malat,gdcm@web.sourceforge.net:htdocs
-#
-## Nightly PDF Doxygen
-#cd $HOME/Dashboards/MyTests/gdcm-nightly/Utilities/doxygen/latex
-#rsync --quiet -av gdcm-2.1.0.pdf malat,gdcm@web.sourceforge.net:htdocs
-#
-## mirror on github:
-#cd $HOME/Dashboards/gdcm
-#git push --mirror git@github.com:malaterre/GDCM.git
