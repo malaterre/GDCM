be5969ea3a40c3c417e342be39d8c807a262e09e
Pietro Cerutti <gahr@freebsd.org>
2013-09-13 12:14:26 +0200

    avoids manually looking for OpenJPEG if pkg-config already found it.

--- CMake/FindOpenJPEG.cmake
+++ CMake/FindOpenJPEG.cmake
@@ -21,7 +21,6 @@
 # Try first to locate a cmake config file
 FIND_PACKAGE(OpenJPEG QUIET NO_MODULE)
 
-if( NOT OpenJPEG_DIR )
 SET(OPENJPEG_MAJOR_VERSION 1) # FIXME ?
 FIND_PATH(OPENJPEG_INCLUDE_DIR
   NAMES openjpeg.h #openjpeg-1.0/openjpeg.h
@@ -50,4 +49,3 @@ MARK_AS_ADVANCED(
   OPENJPEG_LIBRARY
   OPENJPEG_INCLUDE_DIR
   )
-endif()
