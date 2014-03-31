f2fc87509011023f8daff8b4d89265da30f799bd
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2014-03-31 11:59:59 +0200

    need to pull in PATH all tools

--- runtests_vista32pro.bat
+++ runtests_vista32pro.bat
@@ -1,5 +1,8 @@
 @rem Update ourselves
-cd c:/Dashboards/GDCMScripts && git pull
+cd C:/Dashboards/GDCMScripts && git pull
+
+@rem Use Visual Studio 2010:
+call "%VS100COMNTOOLS%vsvars32.bat"
 
 @rem ctest -S vbox-vtk-vs10-debug.cmake -V -O vbox-vtk-vs10-debug.log
 ctest -S vbox-gdcm-vs10-debug.cmake -V -O vbox-gdcm-vs10-debug.log
