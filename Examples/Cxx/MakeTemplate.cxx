6659ba16ba6e4a63b3a06ced43abedb1fd5be5a4
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2014-04-29 14:57:11 +0200

    Update example to also hack the DICOM header

--- Examples/Cxx/MakeTemplate.cxx
+++ Examples/Cxx/MakeTemplate.cxx
@@ -11,26 +11,53 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
-
 #include "gdcmFileAnonymizer.h"
+#include "gdcmReader.h"
+#include "gdcmWriter.h"
 
 int main(int argc, char *argv[])
 {
   const char* filename = argv[1];
   const char* outfilename = argv[2];
 
-  gdcm::Trace::DebugOn();
+  //gdcm::Trace::DebugOn();
 
+  // Remove Pixel Data element:
   gdcm::FileAnonymizer fa;
   fa.SetInputFileName( filename );
   fa.SetOutputFileName( outfilename );
 
   fa.Empty( gdcm::Tag(0x7fe0,0x10) );
-  //fa.Empty( gdcm::Tag(0x10,0x10) );
+  // cannot replace in-place DICOM header:
+  //fa.Replace( gdcm::Tag(0x2,0x2), "1.2.840.10008.5.1.4.1.1.7" );
 
   if( !fa.Write() )
     {
     std::cerr << "impossible to remove Pixel Data attribute" << std::endl;
+    return 1;
+    }
+
+  // Update the DICOM Header:
+  gdcm::Reader reader;
+  reader.SetFileName( outfilename );
+  if( !reader.Read() )
+    {
+    std::cerr << "could not read back" << std::endl;
+    return 1;
+    }
+
+  gdcm::File & file = reader.GetFile();
+  gdcm::FileMetaInformation &fmi = file.GetHeader();
+  gdcm::TransferSyntax ts = gdcm::TransferSyntax::ImplicitVRLittleEndian;
+  ts = gdcm::TransferSyntax::ExplicitVRLittleEndian;
+  fmi.SetDataSetTransferSyntax(ts);
+
+  gdcm::Writer writer;
+  writer.SetFile( file );
+  writer.SetFileName( outfilename ); // warning overwrite file !
+  if( !writer.Write() )
+    {
+    std::cerr << "could not write back" << std::endl;
     return 1;
     }
 
