8d2d06624da215c4037097dfc0a10ad3fab4c2f9
Mathieu Malaterre <mathieu.malaterre@voxxl.com>
2014-07-30 17:04:55 +0200

    Add missing tag in RTDOSE image
    As reported by: Nicky Patterson <pattersonnp@gmail.com>
    GDCM would not add a Frame Increment Pointer which is required in the case of RTDOSE
    Update the VTK example to show before/after the change what was needed to fix the output DICOM
    Closes: #334

--- Utilities/VTK/Examples/Cxx/CreateFakeRTDOSE.cxx
+++ Utilities/VTK/Examples/Cxx/CreateFakeRTDOSE.cxx
@@ -20,6 +20,9 @@
 #include "vtkMedicalImageProperties.h"
 
 #include "gdcmTrace.h"
+#include "gdcmReader.h"
+#include "gdcmWriter.h"
+#include "gdcmAttribute.h"
 
 /*
  * Minimal example to create a fake RTDOSE file. The data contains a sphere
@@ -75,12 +78,43 @@ int main(int, char *[])
 #else
   writer->SetInput( image );
 #endif
+  writer->GetMedicalImageProperties()->SetSliceThickness("1.5");
+  writer->GetMedicalImageProperties()->AddUserDefinedValue( "Dose Units", "GY");
+  writer->GetMedicalImageProperties()->AddUserDefinedValue( "Dose Summation Type", "PLAN");
+  writer->GetMedicalImageProperties()->AddUserDefinedValue( "Dose Type", "PHYSICAL");
+  writer->GetMedicalImageProperties()->AddUserDefinedValue( "Frame of Reference UID", "1.3.12.2.1107.5.6.1.68100.30270111041215391275000000001");
   writer->GetMedicalImageProperties()->SetModality( "RTDOSE" );
   writer->SetScale( 0.0042 ); // why not
   writer->Write();
 
   image->Delete();
   writer->Delete();
+
+
+  // BEGIN HACK
+  // In GDCM version 2.4.3 and before, the following tag was missing which caused issue with some RTDose software:
+
+  // Open the DICOM file that was temporarily created. This will allows me to used
+  // GDCM to append specific tags that allows the RTDOSE to be associated with the
+  // relevant CT images.
+  gdcm::Reader reader2;
+  reader2.SetFileName("rtdose.dcm" );
+  reader2.Read();
+  gdcm::File &file = reader2.GetFile();
+  gdcm::DataSet &ds = file.GetDataSet();
+
+  // Required by some software and not automagically added by GDCM in old version
+  gdcm::Attribute<0x0028,0x0009> framePointer;
+  framePointer.SetNumberOfValues(1);
+  framePointer.SetValue( gdcm::Tag(0x3004,0x000C) );
+  ds.Replace( framePointer.GetAsDataElement() );
+
+  gdcm::Writer writer2;
+  writer2.CheckFileMetaInformationOff();
+  writer2.SetFileName("rtdose2.dcm");
+  writer2.SetFile( file );
+  writer2.Write();
+  // END HACK
 
   return 0;
 }
