676c557a58a4e1e289654cbac8eca7e1ef936eff
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-03-25 12:36:48 +0100

    Add Video Photographic Image Storage

--- Source/DataStructureAndEncodingDefinition/gdcmMediaStorage.h
+++ Source/DataStructureAndEncodingDefinition/gdcmMediaStorage.h
@@ -128,6 +128,7 @@ public:
     OphthalmicPhotography8BitImageStorage, // 1.2.840.10008.5.1.4.1.1.77.1.5.1
     OphthalmicTomographyImageStorage, // 1.2.840.10008.5.1.4.1.1.77.1.5.4
     VLMicroscopicImageStorage,
+    VideoPhotographicImageStorage,
     MS_END
   } MSType; // Media Storage Type
 
