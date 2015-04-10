d1d2bb9bb903acbacf56622cd4ce9736d7d31bba
Matt McCormick <matt.mccormick@kitware.com>
2015-04-10 12:04:29 -0400

    DOC: Update KWStyle location.
    It has now moved to GitHub.
    
    Change-Id: If2f28d009022eefa1bfd629b4b95a37236ce06b3

--- CMake/FindKWStyle.cmake
+++ CMake/FindKWStyle.cmake
@@ -1,6 +1,6 @@
 #
 # this module looks for KWStyle
-# http://public.kitware.com/KWStyle
+# https://kitware.github.io/KWStyle/
 #
 #
 #  Copyright (c) 2009-2011 Mathieu Malaterre <mathieu.malaterre@gmail.com>
