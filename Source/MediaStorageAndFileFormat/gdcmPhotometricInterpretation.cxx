81e551fc30ae2da9b9477319cee95d70a27ec9fe
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-04-29 09:12:54 +0200

    Add note about MPEG2

--- Source/MediaStorageAndFileFormat/gdcmPhotometricInterpretation.cxx
+++ Source/MediaStorageAndFileFormat/gdcmPhotometricInterpretation.cxx
@@ -151,6 +151,7 @@ bool PhotometricInterpretation::IsLossless() const
   switch ( PIField )
     {
   case MONOCHROME1:
+  /* technically MPEG-2 is lossy however the PI is set to MONOCHROME2 */
   case MONOCHROME2:
   case PALETTE_COLOR:
   case RGB:
