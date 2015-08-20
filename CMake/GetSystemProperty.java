7597a787f0e11eda130ecc257b78316962206c11
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-08-20 09:47:23 +0200

    Fix compilation issue with GCJ
    Error was:
    1. WARNING in /«PKGBUILDDIR»/CMake/GetSystemProperty.java (at line 27)
      Enumeration keys = p.keys();
      ^^^^^^^^^^^
    Enumeration is a raw type. References to generic type Enumeration<E> should be parameterized

--- CMake/GetSystemProperty.java
+++ CMake/GetSystemProperty.java
@@ -24,7 +24,7 @@ public class GetSystemProperty {
   public static void main(String args[]) {
     if( args.length == 0 ) {
       Properties p = System.getProperties();
-      Enumeration keys = p.keys();
+      Enumeration<Object> keys = p.keys();
       while (keys.hasMoreElements()) {
         String key = (String)keys.nextElement();
         String value = (String)p.get(key);
