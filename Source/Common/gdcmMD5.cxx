65e548d4f80423451f4b2bab3cd1263d8e481eee
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-11-07 13:36:39 +0100

    cleanup some old code

--- Source/Common/gdcmMD5.cxx
+++ Source/Common/gdcmMD5.cxx
@@ -19,10 +19,8 @@
 #elif defined(GDCM_BUILD_TESTING)
 #include "gdcm_md5.h"
 #endif
-
-#include <string.h>//memcmp
-#include <stdlib.h> // malloc
-#include <stdio.h> // fopen
+#include <fstream>
+#include <vector>
 
 /*
  */
@@ -41,22 +39,23 @@ bool MD5::Compute(const char *buffer, size_t buf_len, char digest_str[33])
   MD5_Init(&ctx);
   MD5_Update(&ctx, buffer, buf_len);
   MD5_Final(digest, &ctx);
+  for (int di = 0; di < 16; ++di)
+    sprintf(digest_str+2*di, "%02x", digest[di]);
+  digest_str[2*16] = '\0';
+  return true;
 #elif defined(GDCM_BUILD_TESTING)
   md5_byte_t digest[16];
   md5_state_t state;
   md5_init(&state);
   md5_append(&state, (const md5_byte_t *)buffer, (int)buf_len);
   md5_finish(&state, digest);
-#else
-  unsigned char digest[16] = {};
-  return false;
-#endif
   for (int di = 0; di < 16; ++di)
-    {
     sprintf(digest_str+2*di, "%02x", digest[di]);
-    }
   digest_str[2*16] = '\0';
   return true;
+#else
+  return false;
+#endif
 }
 
 #ifdef GDCM_USE_SYSTEM_OPENSSL
@@ -64,35 +63,20 @@ static bool process_file(const char *filename, unsigned char *digest)
 {
   if( !filename || !digest ) return false;
 
-  FILE *file = fopen(filename, "rb");
-  if(!file)
-    {
-    return false;
-    }
+  std::ifstream file(filename, std::ios::binary);
+  if(!file) return false;
 
-  size_t file_size = System::FileSize(filename);
-  void *buffer = malloc(file_size);
-  if(!buffer)
-    {
-    fclose(file);
-    return false;
-    }
-  size_t read = fread(buffer, 1, file_size, file);
-  if( read != file_size ) return false;
+  const size_t file_size = System::FileSize(filename);
+  std::vector<char> v( file_size );
+
+  char *buffer = &v[0];
+  file.read(buffer, file_size);
 
   MD5_CTX ctx;
   MD5_Init(&ctx);
   MD5_Update(&ctx, buffer, file_size);
   MD5_Final(digest, &ctx);
 
-  /*printf("MD5 (\"%s\") = ", test[i]); */
-  /*for (int di = 0; di < 16; ++di)
-  {
-    printf("%02x", digest[di]);
-  }*/
-  //printf("\t%s\n", filename);
-  free(buffer);
-  fclose(file);
   return true;
 }
 #elif defined(GDCM_BUILD_TESTING)
@@ -100,34 +84,20 @@ inline bool process_file(const char *filename, md5_byte_t *digest)
 {
   if( !filename || !digest ) return false;
 
-  FILE *file = fopen(filename, "rb");
-  if(!file)
-    {
-    return false;
-    }
+  std::ifstream file(filename, std::ios::binary);
+  if(!file) return false;
 
-  size_t file_size = System::FileSize(filename);
-  void *buffer = malloc(file_size);
-  if(!buffer)
-    {
-    fclose(file);
-    return false;
-    }
-  size_t read = fread(buffer, 1, file_size, file);
-  if( read != file_size ) return false;
+  const size_t file_size = System::FileSize(filename);
+  std::vector<char> v( file_size );
+
+  char *buffer = &v[0];
+  file.read(buffer, file_size);
 
   md5_state_t state;
   md5_init(&state);
   md5_append(&state, (const md5_byte_t *)buffer, (int)file_size);
   md5_finish(&state, digest);
-  /*printf("MD5 (\"%s\") = ", test[i]); */
-  /*for (int di = 0; di < 16; ++di)
-  {
-    printf("%02x", digest[di]);
-  }*/
-  //printf("\t%s\n", filename);
-  free(buffer);
-  fclose(file);
+
   return true;
 }
 #else
@@ -139,8 +109,6 @@ inline bool process_file(const char *, unsigned char *)
 
 bool MD5::ComputeFile(const char *filename, char digest_str[33])
 {
-  // If not file exist
-  // return false;
 #ifdef GDCM_USE_SYSTEM_OPENSSL
   unsigned char digest[16];
 #elif defined(GDCM_BUILD_TESTING)
