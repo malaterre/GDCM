be028ad2d9a8ff5eca16f402d8d904916ca6e8f3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-08-24 10:27:07 +0200

    Revert portion from fbf61beb6e18c1a22730b6b9751b6a7f46f81d97

--- Source/MediaStorageAndFileFormat/gdcmImageHelper.cxx
+++ Source/MediaStorageAndFileFormat/gdcmImageHelper.cxx
@@ -1510,13 +1510,13 @@ void ImageHelper::SetSpacingValue(DataSet & ds, const std::vector<double> & spac
 
         // <entry group="0028" element="9110" vr="SQ" vm="1" name="Pixel Measures Sequence"/>
         // do not set a slice thickness since GDCM always recompute it from the IOP/IPP
-        Attribute<0x0018,0x0050> at2;
-        at2.SetValue( fabs(spacing[2]) );
+        //Attribute<0x0018,0x0050> at2;
+        //at2.SetValue( spacing[2] );
         Attribute<0x0028,0x0030> at1;
         at1.SetValue( spacing[1], 0 );
         at1.SetValue( spacing[0], 1 );
         subds2.Replace( at1.GetAsDataElement() );
-        subds2.Replace( at2.GetAsDataElement() );
+        //subds2.Replace( at2.GetAsDataElement() );
       }
     // cleanup per-frame
     {
