a14f2570cb02bc613c4f364b4fc2f1099b853b34
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2017-02-01 10:44:38 +0100

    Remove more openjpeg 1.5.x legacy stuff

--- CMake/FindOpenJPEG.cmake
+++ CMake/FindOpenJPEG.cmake
@@ -20,46 +20,16 @@
 
 # Try with pkg-config first
 find_package(PkgConfig)
-pkg_check_modules(OPENJPEG libopenjpeg1)
-if(OPENJPEG_FOUND)
-    set(OPENJPEG_INCLUDE_DIR ${OPENJPEG_INCLUDE_DIRS})
-    set(OPENJPEG_LIBRARIES ${OPENJPEG_LDFLAGS})
-else()
-# Try to locate a cmake config file
-find_package(OpenJPEG QUIET NO_MODULE)
-mark_as_advanced(OpenJPEG_DIR)
+pkg_check_modules(OPENJPEG libopenjp2)
 
-if( NOT OpenJPEG_DIR )
-set(OPENJPEG_VERSION_MAJOR 2) # FIXME ?
-set(OPENJPEG_VERSION_MINOR 0) # FIXME ?
-set(OPENJPEG_VERSION_PATCH 0) # FIXME ?
-set(OPENJPEG_VERSION "${OPENJPEG_VERSION_MAJOR}.${OPENJPEG_VERSION_MINOR}.${OPENJPEG_VERSION_PATCH}")
-find_path(OPENJPEG_INCLUDE_DIR
-  NAMES openjpeg.h #openjpeg-1.0/openjpeg.h
-  PATHS /usr/local/include
-  /usr/local/include/openjpeg-1.0
-  /usr/include
-  /usr/include/openjpeg-1.0
-  )
-
-find_library(OPENJPEG_LIBRARY
-  NAMES openjpeg
-  )
-endif()
-endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(OpenJPEG REQUIRED_VARS
-  OPENJPEG_LIBRARY
-  OPENJPEG_INCLUDE_DIR
+  OPENJPEG_LIBRARIES
+  OPENJPEG_INCLUDE_DIRS
   VERSION_VAR OPENJPEG_VERSION
 )
 
-if(OPENJPEG_FOUND)
-  set(OPENJPEG_LIBRARIES ${OPENJPEG_LIBRARY})
-  set(OPENJPEG_INCLUDE_DIRS ${OPENJPEG_INCLUDE_DIR})
-endif()
-
 mark_as_advanced(
-  OPENJPEG_LIBRARY
-  OPENJPEG_INCLUDE_DIR
+  OPENJPEG_LIBRARIES
+  OPENJPEG_INCLUDE_DIRS
   )
