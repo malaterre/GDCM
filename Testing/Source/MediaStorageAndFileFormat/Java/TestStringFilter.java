0714e645cb1dc705007f35879a7ec244d3dd4b5d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2017-10-19 07:17:26 -0400

    Update java test

--- Testing/Source/MediaStorageAndFileFormat/Java/TestStringFilter.java
+++ Testing/Source/MediaStorageAndFileFormat/Java/TestStringFilter.java
@@ -38,29 +38,58 @@ public class TestStringFilter
     for( long i = 0; i < nfiles; ++i )
       {
       String filename = Testing.GetFileName( i );
-      //System.out.println("Success reading: " + filename );
-      Reader reader = new Reader();
-      reader.SetFileName( filename );
-      if ( !reader.Read() )
-        {
-        throw new Exception("Could not read: " + filename );
-        }
-      String ref = Testing.GetMediaStorageFromFile(filename);
-      if( ref == null )
-        {
-        throw new Exception("Missing ref for: " + filename );
-        }
-      MediaStorage ms = new MediaStorage();
-      ms.SetFromFile( reader.GetFile() );
-      if( ms.IsUndefined() && !"".equals( ref ) )
-        {
-        // gdcm-CR-DCMTK-16-NonSamplePerPix.dcm is empty
-        throw new Exception("ref is undefined for: " + filename + " should be " + ref );
-        }
-      MediaStorage.MSType ref_mstype = MediaStorage.GetMSType( ref );
-      if( !"".equals( ref ) && ms.GetType() != ref_mstype )
-        {
-        throw new Exception("incompatible type: " + ref + " vs " + ms.GetString() + " for " + filename );
+      if( filename.contains( "MR_Philips_Intera_No_PrivateSequenceImplicitVR.dcm" )) {
+        //System.out.println("Success reading: " + filename );
+        Reader reader = new Reader();
+        reader.SetFileName( filename );
+        if ( !reader.Read() )
+          {
+          throw new Exception("Could not read: " + filename );
+          }
+	File file = reader.GetFile();
+	DataSet ds = file.GetDataSet();
+	Tag t1 = new Tag(0x2005,0xe229);
+	StringFilter sf = new StringFilter();
+	sf.SetFile( file );
+	if( ds.FindDataElement( t1 ) ) {
+	  DataElement de1 = ds.GetDataElement(t1);
+          System.out.println("de1: " + de1 );
+	  String s1 = sf.ToString( de1 );
+          System.out.println("s1: " + s1 );
+	  float array[] = { 5.1f, -4.9f };
+	  de1.SetArray( array, array.length );
+          System.out.println("de1: " + de1 );
+	  s1 = sf.ToString( de1 );
+          System.out.println("s1: " + s1 );
+	}
+	Tag t2 = new Tag(0x18,0x2043);
+	float array[] = { 2.5f, -1.6f };
+	DataElement de = new DataElement(t2);
+	//de.SetVR( new VR(VR.VRType.FL) );
+	de.SetArray( array, array.length );
+	String s = sf.ToString( de );
+        System.out.println("s: " + s );
+	ds.Replace( de );
+	if( ds.FindDataElement( t2 ) ) {
+	  DataElement de2 = ds.GetDataElement(t2);
+          System.out.println("de2: " + de2 );
+	  String s2 = sf.ToString( de2 );
+          System.out.println("s2: " + s2 );
+	  //float array[] = { 51, 49 };
+	  //de1.SetArray( array, array.length );
+          //System.out.println("de1: " + de1 );
+	  //s1 = sf.ToString( de1 );
+          //System.out.println("s1: " + s1 );
+	  //ds.Replace( de1 );
+	}
+
+	Writer writer = new Writer();
+	writer.SetFile( reader.GetFile() );
+	writer.SetFileName( "testsf.dcm" );
+        if ( !writer.Write() )
+          {
+          throw new Exception("Could not write: " + filename );
+          }
         }
       }
     }
