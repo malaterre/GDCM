bb79dc519148a192ca543e5997035407f8a97b2e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2017-12-08 11:18:57 -0500

    Add the Spacing in Between Tag

--- Source/MediaStorageAndFileFormat/gdcmImageHelper.cxx
+++ Source/MediaStorageAndFileFormat/gdcmImageHelper.cxx
@@ -1537,13 +1537,13 @@ void ImageHelper::SetSpacingValue(DataSet & ds, const std::vector<double> & spac
 
         // <entry group="0028" element="9110" vr="SQ" vm="1" name="Pixel Measures Sequence"/>
         // do not set spacing between slices since GDCM always recompute it from the IOP/IPP
-        //Attribute<0x0018,0x0088> at2;
-        //at2.SetValue( fabs(spacing[2]) );
+        Attribute<0x0018,0x0088> at2;
+        at2.SetValue( fabs(spacing[2]) );
         Attribute<0x0028,0x0030> at1;
         at1.SetValue( spacing[1], 0 );
         at1.SetValue( spacing[0], 1 );
         subds2.Replace( at1.GetAsDataElement() );
-        //subds2.Replace( at2.GetAsDataElement() );
+        subds2.Replace( at2.GetAsDataElement() );
       }
     // cleanup per-frame
     {
