malaterre/GDCM

commit f287cb817cea68f07ee7d267ccef46f0e089dac2

Hande case where width is not multiple of 8

Source/MediaStorageAndFileFormat/gdcmBitmap.cxx
@@ -285,9 +285,10 @@ unsigned long Bitmap::GetBufferLength() const
   else if( PF == PixelFormat::SINGLEBIT )
     {
     assert( PF.GetSamplesPerPixel() == 1 );
-    unsigned int save = mul;
-    save /= 8;
-    assert( save * 8 == mul );
+    const size_t bytesPerRow = Dimensions[0] / 8 + (Dimensions[0] % 8 != 0 ? 1 : 0);
+    const unsigned int save = bytesPerRow * Dimensions[1];
+    if(Dimensions[0] % 8 == 0 )
+      assert( save * 8 == mul );
     mul = save;
     }
   else if( PF.GetBitsAllocated() % 8 != 0 )

Source/MediaStorageAndFileFormat/gdcmPNMCodec.cxx
@@ -272,8 +272,10 @@ bool PNMCodec::GetHeaderInfo(std::istream &is, TransferSyntax &ts)
   //assert(pos < INT_MAX);
   size_t m = ((size_t)len - (size_t)pos ) / ( dims[0]*dims[1] );
   bool cond;
-  if( type == "P4" )
-    cond = dims[0] * dims[1] != ((size_t)len - (size_t)pos) * 8;
+  if( type == "P4" ) {
+    const size_t bytesPerRow = dims[0] / 8 + (dims[0] % 8 != 0 ? 1 : 0);
+    cond = bytesPerRow * dims[1] != ((size_t)len - (size_t)pos);
+  }
   else
     cond = m * dims[0] * dims[1] != (size_t)len - (size_t)pos;
   if( cond )

commit 3fb24e60f84f3df1e7df7e1cbb92894395b6b9bc

Properly handle 3D data
Fix a bug introduced in f287cb817cea68f07ee7d267ccef46f0e089dac2

Thanks to M.Isakov for bug report

Source/MediaStorageAndFileFormat/gdcmBitmap.cxx
@@ -286,7 +286,9 @@ unsigned long Bitmap::GetBufferLength() const
     {
     assert( PF.GetSamplesPerPixel() == 1 );
     const size_t bytesPerRow = Dimensions[0] / 8 + (Dimensions[0] % 8 != 0 ? 1 : 0);
-    const unsigned int save = bytesPerRow * Dimensions[1];
+    unsigned int save = bytesPerRow * Dimensions[1];
+    if( NumberOfDimensions > 2 )
+      save *= Dimensions[2];
     if(Dimensions[0] % 8 == 0 )
       assert( save * 8 == mul );
     mul = save;