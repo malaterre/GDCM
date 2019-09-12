64c6ae61c9494217710adfedd5bcd0c63d9cd4a9
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2019-09-12 10:42:08 +0200

    Prefer size_t

--- Source/MediaStorageAndFileFormat/gdcmBitmap.cxx
+++ Source/MediaStorageAndFileFormat/gdcmBitmap.cxx
@@ -286,7 +286,7 @@ unsigned long Bitmap::GetBufferLength() const
     {
     assert( PF.GetSamplesPerPixel() == 1 );
     const size_t bytesPerRow = Dimensions[0] / 8 + (Dimensions[0] % 8 != 0 ? 1 : 0);
-    unsigned int save = bytesPerRow * Dimensions[1];
+    size_t save = bytesPerRow * Dimensions[1];
     if( NumberOfDimensions > 2 )
       save *= Dimensions[2];
     if(Dimensions[0] % 8 == 0 )
