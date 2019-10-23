fcacc1593a6638e1252545fe60dd240cbcddb15e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2019-10-23 11:12:45 +0200

    Fix regression for lossy flag

--- Source/MediaStorageAndFileFormat/gdcmPixmapReader.cxx
+++ Source/MediaStorageAndFileFormat/gdcmPixmapReader.cxx
@@ -1095,7 +1095,8 @@ bool PixmapReader::ReadImageInternal(MediaStorage const &ms, bool handlepixeldat
   // execute computation of lossy flag eny time the TS is encapsulated so as to
   // update the correct PixelFormat as early as possible and not during
   // decompression in case of mismatch:
-  if( PixelData->GetTransferSyntax().IsEncapsulated() )
+  if( (!haslossyflag || !lossyflag)
+   || PixelData->GetTransferSyntax().IsEncapsulated() )
     {
     PixelData->ComputeLossyFlag();
     if( PixelData->IsLossy() && (!lossyflag && haslossyflag ) )
@@ -1103,6 +1104,8 @@ bool PixmapReader::ReadImageInternal(MediaStorage const &ms, bool handlepixeldat
       // We always prefer the setting from the stream...
       gdcmWarningMacro( "DataSet set LossyFlag to 0, while Codec made the stream lossy" );
       }
+    // Make sure to combine DICOM info + pixel data bitstream:
+    PixelData->SetLossyFlag( PixelData->IsLossy() || lossyflag);
     }
 
   return true;
