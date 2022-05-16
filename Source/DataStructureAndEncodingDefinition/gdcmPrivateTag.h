cf88327578de828b9e2bc09c3f3d825ae8d85550
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-05-16 12:47:39 +0200

    Add missing operators

--- Source/DataStructureAndEncodingDefinition/gdcmPrivateTag.h
+++ Source/DataStructureAndEncodingDefinition/gdcmPrivateTag.h
@@ -51,6 +51,22 @@ public:
   const char *GetOwner() const { return Owner.c_str(); }
   void SetOwner(const char *owner) { if(owner) Owner = LOComp::Trim(owner); }
 
+  PrivateTag &operator=(const PrivateTag &_val)
+    {
+    SetElementTag( _val.GetElementTag() );
+    Owner = _val.Owner;
+    return *this;
+    }
+
+  bool operator==(const PrivateTag &_val) const
+    {
+    return GetElementTag() == _val.GetElementTag() && Owner == _val.Owner;
+    }
+  bool operator!=(const PrivateTag &_val) const
+    {
+    return GetElementTag() != _val.GetElementTag() || Owner != _val.Owner;
+    }
+
   bool operator<(const PrivateTag &_val) const;
 
   /// Read PrivateTag from a string. Element number will be truncated
