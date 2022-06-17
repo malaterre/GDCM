09b53c0a4ba6b355c05e16c7f98727b35dbcfc08
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-06-17 08:27:46 +0200

    Backport correct poppler fix

--- Applications/Cxx/gdcmpdf.cxx
+++ Applications/Cxx/gdcmpdf.cxx
@@ -52,7 +52,7 @@ static std::string getInfoDate(Dict *infoDict, const char *key)
   if (infoDict->lookup((char*)key, &obj)->isString())
 #endif
     {
-#ifdef LIBPOPPLER_OBJECT_HAS_CONST_GETSTRING
+#ifdef LIBPOPPLER_GOOSTRING_HAS_CONST_GETCSTRING
     const
 #endif
     GooString* gs = obj.getString();
