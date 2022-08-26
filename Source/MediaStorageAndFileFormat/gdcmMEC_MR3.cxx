d47a6aa7a3e62cf1deec6e9101ed330799e64e92
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-08-26 16:24:55 +0200

    add export stuff for mec_mr3

---
+++ Source/MediaStorageAndFileFormat/gdcmMEC_MR3.cxx
@@ -0,0 +1,21 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+
+  Copyright (c) 2006-2011 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmMEC_MR3.h"
+
+namespace gdcm
+{
+  void MEC_MR3::Print(const char *src, size_t srclen)
+  {
+  }
+} // end namespace gdcm
