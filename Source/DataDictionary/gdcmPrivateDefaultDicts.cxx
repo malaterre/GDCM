malaterre/GDCM

commit 4e328a6d54fdf06b5715c528e1fcf5f9e16d737d

Backport from master onto release branch

Source/DataDictionary/gdcmPrivateDefaultDicts.cxx
@@ -1988,7 +1988,7 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0009,0x0042,"GEMS_GENIE_1",VR::DA,VM::VM1,"Patient Creation Date",false },
   {0x0009,0x0043,"GEMS_GENIE_1",VR::TM,VM::VM1,"Patient Creation Time",false },
   {0x0009,0x0044,"GEMS_GENIE_1",VR::SL,VM::VM1,"Num Views Acquired (retired)",false },
-  {0x0009,0x0045,"GEMS_GENIE_1",VR::LT,VM::VM1,"?",false },
+  {0x0009,0x0045,"GEMS_GENIE_1",VR::LT,VM::VM1,"Dataset UID List",false },
   {0x0011,0x000a,"GEMS_GENIE_1",VR::SL,VM::VM1,"Series Type",false },
   {0x0011,0x000b,"GEMS_GENIE_1",VR::SL,VM::VM1,"Effective Series Duration",false },
   {0x0011,0x000c,"GEMS_GENIE_1",VR::SL,VM::VM1,"Num Beats",false },
@@ -2123,7 +2123,7 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0015,0x0015,"GEMS_GENIE_1",VR::SL,VM::VM1,"WB Scan Num",false },
   {0x0015,0x0016,"GEMS_GENIE_1",VR::SL,VM::VM1,"Comb Head Number",false },
   {0x0015,0x0017,"GEMS_GENIE_1",VR::UL,VM::VM1,"Preceding Beat",false },
-  {0x0019,0x005f,"GEMS_GENIE_1",VR::SQ,VM::VM1,"?",false },
+  {0x0019,0x005f,"GEMS_GENIE_1",VR::SQ,VM::VM1,"Annotation Sequence",false },
   {0x0033,0x0007,"GEMS_GENIE_1",VR::LO,VM::VM1,"Orig SOP Instance UID",false },
   {0x0033,0x0008,"GEMS_GENIE_1",VR::CS,VM::VM1,"Xeleris Data Object Type",false },
   {0x0033,0x0010,"GEMS_GENIE_1",VR::SL,VM::VM1,"Modified",false },
@@ -2156,7 +2156,7 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0055,0x0062,"GEMS_GENIE_1",VR::SQ,VM::VM1,"Xeleris Gated Information Sequence",false },
   {0x0055,0x0063,"GEMS_GENIE_1",VR::SQ,VM::VM1,"Xeleris Data Information Sequence",false },
   {0x0055,0x0064,"GEMS_GENIE_1",VR::SQ,VM::VM1,"SDODoubleData SQ",false },
-  {0x0055,0x0065,"GEMS_GENIE_1",VR::SQ,VM::VM1,"?",false },
+  {0x0055,0x0065,"GEMS_GENIE_1",VR::SQ,VM::VM1,"Xeleris Frame Sequence",false },
   {0x5001,0x0001,"GEMS_GENIE_1",VR::SL,VM::VM1,"Modified",false },
   {0x5001,0x0002,"GEMS_GENIE_1",VR::LO,VM::VM1,"Name",false },
   {0x5001,0x0003,"GEMS_GENIE_1",VR::SL,VM::VM1,"Cid",false },
@@ -4429,16 +4429,16 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x2005,0x00b1,"Philips MR Imaging DD 001",VR::FL,VM::VM1,"Diffusion Direction AP",false },
   {0x2005,0x00b2,"Philips MR Imaging DD 001",VR::FL,VM::VM1,"Diffusion Direction FH",false },
   {0x2005,0x00c0,"Philips MR Imaging DD 001",VR::CS,VM::VM1,"?",false },
-  {0x2005,0x0032,"Philips MR Imaging DD 002",VR::SQ,VM::VM1,"?",false },
+  {0x2005,0x0032,"Philips MR Imaging DD 002",VR::SQ,VM::VM1,"Protocol Data Sequence",false },
   {0x2005,0x0034,"Philips MR Imaging DD 002",VR::LT,VM::VM1,"?",false },
-  {0x2005,0x0037,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?",false },
-  {0x2005,0x0038,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?",false },
-  {0x2005,0x0039,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?",false },
-  {0x2005,0x0040,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?",false },
-  {0x2005,0x0041,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?",false },
-  {0x2005,0x0043,"Philips MR Imaging DD 002",VR::SL,VM::VM1,"?",false },
-  {0x2005,0x0044,"Philips MR Imaging DD 002",VR::OW,VM::VM1,"?",false },
-  {0x2005,0x0047,"Philips MR Imaging DD 002",VR::CS,VM::VM1,"?",false },
+  {0x2005,0x0037,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"Protocol Data Name",false },
+  {0x2005,0x0038,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"?",false },
+  {0x2005,0x0039,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"Protocol Data Type",false },
+  {0x2005,0x0040,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"?",false },
+  {0x2005,0x0041,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"?",false },
+  {0x2005,0x0043,"Philips MR Imaging DD 002",VR::SL,VM::VM1,"Protocol Data Block Length (non-padded)",false },
+  {0x2005,0x0044,"Philips MR Imaging DD 002",VR::OW,VM::VM1,"Protocol Data Block",false },
+  {0x2005,0x0047,"Philips MR Imaging DD 002",VR::CS,VM::VM1,"Protocol Data Boolean",false },
   {0x2005,0x0099,"Philips MR Imaging DD 002",VR::UL,VM::VM1,"?",false },
   {0x2005,0x0000,"Philips MR Imaging DD 003",VR::UL,VM::VM1,"?",false },
   {0x2005,0x0001,"Philips MR Imaging DD 003",VR::UL,VM::VM1,"?",false },
@@ -7302,14 +7302,17 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0009,0x0016,"SPI-P Release 1",VR::LO,VM::VM1,"?",false },
   {0x0009,0x0018,"SPI-P Release 1",VR::LO,VM::VM1,"?",false },
   {0x0009,0x0021,"SPI-P Release 1",VR::LT,VM::VM1,"?",false },
-  {0x0009,0x0031,"SPI-P Release 1",VR::LT,VM::VM1,"PACS Unique Identifier",false },
-  {0x0009,0x0034,"SPI-P Release 1",VR::LT,VM::VM1,"Cluster Unique Identifier",false },
-  {0x0009,0x0038,"SPI-P Release 1",VR::LT,VM::VM1,"System Unique Identifier",false },
-  {0x0009,0x0039,"SPI-P Release 1",VR::LT,VM::VM1,"?",false },
-  {0x0009,0x0051,"SPI-P Release 1",VR::LT,VM::VM1,"Study Unique Identifier",false },
-  {0x0009,0x0061,"SPI-P Release 1",VR::LT,VM::VM1,"Series Unique Identifier",false },
+  {0x0009,0x0022,"SPI-P Release 1",VR::LO,VM::VM1,"?",false },
+  {0x0009,0x0031,"SPI-P Release 1",VR::LO,VM::VM1,"PACS Unique Identifier",false },
+  {0x0009,0x0034,"SPI-P Release 1",VR::LO,VM::VM1,"Cluster Unique Identifier",false },
+  {0x0009,0x0038,"SPI-P Release 1",VR::LO,VM::VM1,"System Unique Identifier",false },
+  {0x0009,0x0039,"SPI-P Release 1",VR::LO,VM::VM1,"?",false },
+  {0x0009,0x0040,"SPI-P Release 1",VR::LO,VM::VM1,"?",false },
+  {0x0009,0x0041,"SPI-P Release 1",VR::LO,VM::VM1,"?",false },
+  {0x0009,0x0051,"SPI-P Release 1",VR::LO,VM::VM1,"Study Unique Identifier",false },
+  {0x0009,0x0061,"SPI-P Release 1",VR::LO,VM::VM1,"Series Unique Identifier",false },
   {0x0009,0x0091,"SPI-P Release 1",VR::LT,VM::VM1,"?",false },
-  {0x0009,0x00a0,"SPI-P Release 1",VR::LT,VM::VM1,"?",false },
+  {0x0009,0x00a0,"SPI-P Release 1",VR::UN,VM::VM1,"?",false },
   {0x0009,0x00f2,"SPI-P Release 1",VR::LT,VM::VM1,"?",false },
   {0x0009,0x00f3,"SPI-P Release 1",VR::UN,VM::VM1,"?",false },
   {0x0009,0x00f4,"SPI-P Release 1",VR::LT,VM::VM1,"?",false },
@@ -7408,6 +7411,23 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0019,0x0018,"SPI-P Release 1;1",VR::UN,VM::VM1,"Number Of Samples 2",false },
   {0x0019,0x0019,"SPI-P Release 1;1",VR::UN,VM::VM1,"Sample Data 2",false },
   {0x0019,0x001a,"SPI-P Release 1;1",VR::UN,VM::VM1,"Sample Rate 2",false },
+  {0x0021,0x0041,"SPI-P Release 1;1",VR::LO,VM::VM1,"?",false },
+  {0x0021,0x0042,"SPI-P Release 1;1",VR::US,VM::VM1,"?",false },
+  {0x0021,0x0043,"SPI-P Release 1;1",VR::US,VM::VM1,"?",false },
+  {0x0021,0x0044,"SPI-P Release 1;1",VR::DS,VM::VM1,"?",false },
+  {0x0021,0x0045,"SPI-P Release 1;1",VR::LO,VM::VM1,"?",false },
+  {0x0021,0x0050,"SPI-P Release 1;1",VR::US,VM::VM1,"?",false },
+  {0x0021,0x0051,"SPI-P Release 1;1",VR::US,VM::VM1,"?",false },
+  {0x0021,0x0052,"SPI-P Release 1;1",VR::US,VM::VM1,"?",false },
+  {0x0021,0x0060,"SPI-P Release 1;1",VR::IS,VM::VM1_n,"?",false },
+  {0x0021,0x0061,"SPI-P Release 1;1",VR::IS,VM::VM1_n,"?",false },
+  {0x0021,0x0067,"SPI-P Release 1;1",VR::LO,VM::VM1_n,"?",false },
+  {0x0021,0x006a,"SPI-P Release 1;1",VR::DS,VM::VM1_n,"?",false },
+  {0x0021,0x006b,"SPI-P Release 1;1",VR::DS,VM::VM1_n,"?",false },
+  {0x0021,0x006c,"SPI-P Release 1;1",VR::US,VM::VM1_n,"?",false },
+  {0x0021,0x006d,"SPI-P Release 1;1",VR::DS,VM::VM1_n,"?",false },
+  {0x0021,0x006e,"SPI-P Release 1;1",VR::US,VM::VM1_n,"?",false },
+  {0x0021,0x006f,"SPI-P Release 1;1",VR::DS,VM::VM1_n,"?",false },
   {0x0029,0x0000,"SPI-P Release 1;1",VR::LT,VM::VM1,"Zoom ID",false },
   {0x0029,0x0001,"SPI-P Release 1;1",VR::DS,VM::VM1_n,"Zoom Rectangle",false },
   {0x0029,0x0003,"SPI-P Release 1;1",VR::DS,VM::VM1,"Zoom Factor",false },
@@ -7574,13 +7594,18 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0029,0x0010,"SPI-P-Private_CDS Release 1",VR::OB,VM::VM1,"?",false },
   {0x0019,0x0030,"SPI-P-Private_ICS Release 1",VR::DS,VM::VM1,"?",false },
   {0x0019,0x0031,"SPI-P-Private_ICS Release 1",VR::LO,VM::VM1,"?",false },
+  {0x0029,0x0007,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
+  {0x0029,0x0008,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
+  {0x0029,0x000c,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x000d,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x000e,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x000f,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x0010,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
+  {0x0029,0x0018,"SPI-P-Private_ICS Release 1",VR::FD,VM::VM1,"?",false },
   {0x0029,0x001b,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x001c,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x001d,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
+  {0x0029,0x001e,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x0020,"SPI-P-Private_ICS Release 1",VR::UN,VM::VM1,"?",false },
   {0x0029,0x0021,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x004c,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
@@ -7589,19 +7614,30 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0029,0x004f,"SPI-P-Private_ICS Release 1",VR::UN,VM::VM1,"?",false },
   {0x0029,0x0050,"SPI-P-Private_ICS Release 1",VR::UN,VM::VM1,"?",false },
   {0x0029,0x0051,"SPI-P-Private_ICS Release 1",VR::UN,VM::VM1,"?",false },
+  {0x0029,0x0066,"SPI-P-Private_ICS Release 1",VR::DS,VM::VM4,"?",false },
   {0x0029,0x0067,"SPI-P-Private_ICS Release 1",VR::LO,VM::VM1,"?",false },
   {0x0029,0x0068,"SPI-P-Private_ICS Release 1",VR::US,VM::VM1,"?",false },
   {0x0029,0x006a,"SPI-P-Private_ICS Release 1",VR::LO,VM::VM1,"?",false },
   {0x0029,0x006b,"SPI-P-Private_ICS Release 1",VR::US,VM::VM1,"?",false },
   {0x0029,0x0072,"SPI-P-Private_ICS Release 1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x0091,"SPI-P-Private_ICS Release 1",VR::IS,VM::VM1,"?",false },
+  {0x0029,0x0093,"SPI-P-Private_ICS Release 1",VR::LO,VM::VM1_n,"?",false },
+  {0x0029,0x0094,"SPI-P-Private_ICS Release 1",VR::LO,VM::VM1_n,"?",false },
   {0x0029,0x0000,"SPI-P-Private_ICS Release 1;1",VR::SL,VM::VM1,"?",false },
   {0x0029,0x0005,"SPI-P-Private_ICS Release 1;1",VR::FL,VM::VM1,"?",false },
   {0x0029,0x0006,"SPI-P-Private_ICS Release 1;1",VR::FL,VM::VM1,"?",false },
   {0x0029,0x0020,"SPI-P-Private_ICS Release 1;1",VR::FL,VM::VM1,"?",false },
   {0x0029,0x0021,"SPI-P-Private_ICS Release 1;1",VR::FL,VM::VM1,"?",false },
+  {0x0029,0x0022,"SPI-P-Private_ICS Release 1;1",VR::US,VM::VM1,"?",false },
+  {0x0029,0x0023,"SPI-P-Private_ICS Release 1;1",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x0024,"SPI-P-Private_ICS Release 1;1",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x0025,"SPI-P-Private_ICS Release 1;1",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x0026,"SPI-P-Private_ICS Release 1;1",VR::FD,VM::VM1,"?",false },
   {0x0029,0x0030,"SPI-P-Private_ICS Release 1;1",VR::ST,VM::VM1,"?",false },
   {0x0029,0x006b,"SPI-P-Private_ICS Release 1;1",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x00c1,"SPI-P-Private_ICS Release 1;1",VR::US,VM::VM1,"?",false },
+  {0x0029,0x00cb,"SPI-P-Private_ICS Release 1;1",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x00cc,"SPI-P-Private_ICS Release 1;1",VR::FD,VM::VM1,"?",false },
   {0x0029,0x00cd,"SPI-P-Private_ICS Release 1;1",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x00d4,"SPI-P-Private_ICS Release 1;1",VR::FL,VM::VM1,"?",false },
   {0x0029,0x00d5,"SPI-P-Private_ICS Release 1;1",VR::FL,VM::VM1,"?",false },
@@ -7612,13 +7648,13 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0029,0x0003,"SPI-P-Private_ICS Release 1;2",VR::SL,VM::VM1,"?",false },
   {0x0029,0x0004,"SPI-P-Private_ICS Release 1;2",VR::SL,VM::VM1,"?",false },
   {0x0029,0x0005,"SPI-P-Private_ICS Release 1;2",VR::SL,VM::VM1,"?",false },
-  {0x0029,0x0030,"SPI-P-Private_ICS Release 1;2",VR::LO,VM::VM1,"?",false },
-  {0x0029,0x00a0,"SPI-P-Private_ICS Release 1;2",VR::UN,VM::VM1,"?",false },
-  {0x0029,0x00a1,"SPI-P-Private_ICS Release 1;2",VR::UN,VM::VM1,"?",false },
-  {0x0029,0x00a2,"SPI-P-Private_ICS Release 1;2",VR::UN,VM::VM1,"?",false },
-  {0x0029,0x00a3,"SPI-P-Private_ICS Release 1;2",VR::UN,VM::VM1,"?",false },
+  {0x0029,0x0030,"SPI-P-Private_ICS Release 1;2",VR::LT,VM::VM1,"?",false },
+  {0x0029,0x00a0,"SPI-P-Private_ICS Release 1;2",VR::SL,VM::VM1,"?",false },
+  {0x0029,0x00a1,"SPI-P-Private_ICS Release 1;2",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x00a2,"SPI-P-Private_ICS Release 1;2",VR::SL,VM::VM1,"?",false },
+  {0x0029,0x00a3,"SPI-P-Private_ICS Release 1;2",VR::SL,VM::VM1,"?",false },
   {0x0029,0x00a5,"SPI-P-Private_ICS Release 1;2",VR::SQ,VM::VM1,"?",false },
-  {0x0029,0x00a6,"SPI-P-Private_ICS Release 1;2",VR::UN,VM::VM1,"?",false },
+  {0x0029,0x00a6,"SPI-P-Private_ICS Release 1;2",VR::SL,VM::VM1,"?",false },
   {0x0029,0x00d9,"SPI-P-Private_ICS Release 1;2",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x0000,"SPI-P-Private_ICS Release 1;3",VR::ST,VM::VM1,"?",false },
   {0x0029,0x0001,"SPI-P-Private_ICS Release 1;3",VR::ST,VM::VM1,"?",false },
@@ -7644,22 +7680,29 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   {0x0029,0x000a,"SPI-P-Private_ICS Release 1;4",VR::FL,VM::VM1,"?",false },
   {0x0029,0x000b,"SPI-P-Private_ICS Release 1;4",VR::FL,VM::VM1,"?",false },
   {0x0029,0x000c,"SPI-P-Private_ICS Release 1;4",VR::FL,VM::VM1,"?",false },
+  {0x0029,0x000e,"SPI-P-Private_ICS Release 1;4",VR::US,VM::VM1,"?",false },
+  {0x0029,0x000f,"SPI-P-Private_ICS Release 1;4",VR::US,VM::VM1,"?",false },
+  {0x0029,0x0010,"SPI-P-Private_ICS Release 1;4",VR::SL,VM::VM1,"?",false },
+  {0x0029,0x0090,"SPI-P-Private_ICS Release 1;4",VR::SL,VM::VM1,"?",false },
+  {0x0029,0x009a,"SPI-P-Private_ICS Release 1;4",VR::SQ,VM::VM1,"?",false },
+  {0x0029,0x009c,"SPI-P-Private_ICS Release 1;4",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x00a0,"SPI-P-Private_ICS Release 1;4",VR::US,VM::VM1,"?",false },
   {0x0029,0x00a1,"SPI-P-Private_ICS Release 1;4",VR::US,VM::VM1,"?",false },
   {0x0029,0x00b1,"SPI-P-Private_ICS Release 1;4",VR::US,VM::VM1,"?",false },
   {0x0029,0x00b2,"SPI-P-Private_ICS Release 1;4",VR::US,VM::VM1,"?",false },
   {0x0029,0x00b3,"SPI-P-Private_ICS Release 1;4",VR::SL,VM::VM1,"?",false },
   {0x0029,0x00b5,"SPI-P-Private_ICS Release 1;4",VR::FL,VM::VM1,"?",false },
+  {0x0029,0x00b6,"SPI-P-Private_ICS Release 1;4",VR::SL,VM::VM1,"?",false },
   {0x0029,0x00bc,"SPI-P-Private_ICS Release 1;4",VR::SL,VM::VM1,"?",false },
   {0x0029,0x00bd,"SPI-P-Private_ICS Release 1;4",VR::SL,VM::VM1,"?",false },
   {0x0029,0x00be,"SPI-P-Private_ICS Release 1;4",VR::US,VM::VM1,"?",false },
-  {0x0029,0x00d6,"SPI-P-Private_ICS Release 1;4",VR::UN,VM::VM1,"?",false },
-  {0x0029,0x00d7,"SPI-P-Private_ICS Release 1;4",VR::UN,VM::VM1,"?",false },
+  {0x0029,0x00d6,"SPI-P-Private_ICS Release 1;4",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x00d7,"SPI-P-Private_ICS Release 1;4",VR::FD,VM::VM1,"?",false },
   {0x0029,0x00d8,"SPI-P-Private_ICS Release 1;4",VR::UN,VM::VM1,"?",false },
   {0x0029,0x00d9,"SPI-P-Private_ICS Release 1;4",VR::UN,VM::VM1,"?",false },
   {0x0029,0x00da,"SPI-P-Private_ICS Release 1;4",VR::UN,VM::VM1,"?",false },
-  {0x0029,0x00dc,"SPI-P-Private_ICS Release 1;4",VR::UN,VM::VM1,"?",false },
-  {0x0029,0x00dd,"SPI-P-Private_ICS Release 1;4",VR::UN,VM::VM1,"?",false },
+  {0x0029,0x00dc,"SPI-P-Private_ICS Release 1;4",VR::FD,VM::VM1,"?",false },
+  {0x0029,0x00dd,"SPI-P-Private_ICS Release 1;4",VR::FD,VM::VM1,"?",false },
   {0x0029,0x00e0,"SPI-P-Private_ICS Release 1;4",VR::SQ,VM::VM1,"?",false },
   {0x0029,0x0050,"SPI-P-Private_ICS Release 1;5",VR::CS,VM::VM1,"?",false },
   {0x0029,0x0055,"SPI-P-Private_ICS Release 1;5",VR::CS,VM::VM1,"?",false },

.gitignore
@@ -1,0 +1,12 @@
+# back-up
+*~
+*.bak
+
+# files when there are conflicts
+*.orig
+
+# qtcreator files
+CMakeLists.txt.user*
+
+# kdevelop files
+*.kdev*

Applications/Cxx/gdcmanon.cxx
@@ -71,20 +71,21 @@ static bool AnonymizeOneFileDumb(gdcm::Anonymizer &anon, const char *filename, c
     }
 
   std::vector<gdcm::Tag>::const_iterator it = empty_tags.begin();
+  bool success = true;
   for(; it != empty_tags.end(); ++it)
     {
-    anon.Empty( *it );
+    success = success && anon.Empty( *it );
     }
   it = remove_tags.begin();
   for(; it != remove_tags.end(); ++it)
     {
-    anon.Remove( *it );
+    success = success && anon.Remove( *it );
     }
 
   std::vector< std::pair<gdcm::Tag, std::string> >::const_iterator it2 = replace_tags.begin();
   for(; it2 != replace_tags.end(); ++it2)
     {
-    anon.Replace( it2->first, it2->second.c_str() );
+    success = success && anon.Replace( it2->first, it2->second.c_str() );
     }
 
   gdcm::Writer writer;
@@ -104,7 +105,7 @@ static bool AnonymizeOneFileDumb(gdcm::Anonymizer &anon, const char *filename, c
 
     return false;
     }
-  return true;
+  return success;
 }
 
 static bool AnonymizeOneFile(gdcm::Anonymizer &anon, const char *filename, const char *outfilename, bool continuemode = false)

Applications/Cxx/gdcmdump.cxx
@@ -62,6 +62,162 @@ int ignoreerrors = 0;
 
 namespace cleanup
 {
+// {"1.3.46.670589.11.0.0.12.2"       ,"Philips Private MR Series Data Storage"},
+enum {
+  TYPE_FLOAT  = 0, // float
+  TYPE_INT32  = 1, // int32
+  TYPE_STRING = 2, // 80 bytes string (+1)
+  TYPE_UINT32 = 4, // uint32
+};
+
+template <typename T>
+void printvaluet(std::istream & is, uint32_t numels)
+{
+  T buffer;
+  for( uint32_t i = 0; i < numels; ++i )
+    {
+    if( i ) std::cout << "\\";
+    is.read( (char*)&buffer, sizeof(T) );
+    std::cout << buffer;
+    }
+}
+
+void printvalue(std::istream &is, uint32_t type, uint32_t numels, uint32_t pos)
+{
+  assert( numels > 0 );
+  std::streampos start = is.tellg();
+  is.seekg( pos );
+  std::cout << "[";
+  typedef char (string81)[81]; // 80'th byte == 0
+  assert( sizeof( string81 ) == 81 );
+  switch( type )
+    {
+  case TYPE_FLOAT:
+    printvaluet<float>(is, numels);
+    break;
+  case TYPE_INT32:
+    printvaluet<int32_t>(is, numels);
+    break;
+  case TYPE_STRING:
+    printvaluet<string81>(is, numels);
+    break;
+  case TYPE_UINT32:
+    printvaluet<uint32_t>(is, numels);
+    break;
+  default:
+    assert( 0 );
+    }
+  std::cout << "]";
+  std::cout << " # " << numels;
+  is.seekg( start );
+}
+
+struct PDFElement
+{
+  const char *getname() const { return name; }
+  uint32_t gettype() const { return getvalue(0); }
+  uint32_t getnumelems() const { return getvalue(1); }
+  uint32_t getdummy() const { return getvalue(2); }
+  uint32_t getoffset() const { return getvalue(3); }
+private:
+  char name[50];
+  // type , numel and offset needs to be read starting from the end
+  // the data in between name and those value can contains garbage stuff
+  uint32_t getvalue(int n) const {
+    uint32_t val = 0;
+    memcpy( (char*)&val, name + 50 - 16 + n * 4, sizeof( val ) );
+    return val;
+  }
+};
+
+void printbinary(std::istream &is, PDFElement const & pdfel )
+{
+  const char *bufferref = pdfel.getname();
+  std::cout << "  " << bufferref << " ";
+  uint32_t type = pdfel.gettype();
+  uint32_t numels = pdfel.getnumelems();
+  uint32_t dummy = pdfel.getdummy();
+  assert( dummy == 0 );
+  uint32_t offset = pdfel.getoffset();
+  uint32_t pos = offset + is.tellg() - 4;
+  printvalue(is, type, numels, pos);
+}
+
+void ProcessSDSData( std::istream & is )
+{
+  // havent been able to figure out what was the begin meant for
+  is.seekg( 0x20 - 8 );
+  uint32_t version = 0;
+  is.read( (char*)&version, sizeof(version) );
+  assert( version == 8 );
+  uint32_t numel = 0;
+  is.read( (char*)&numel, sizeof(numel) );
+  for( uint32_t el = 0; el < numel; ++el )
+    {
+    PDFElement pdfel;
+    assert( sizeof(pdfel) == 50 );
+    is.read( (char*)&pdfel, 50 );
+    if( *pdfel.getname() )
+      {
+      printbinary( is, pdfel );
+      std::cout << std::endl;
+      }
+    }
+
+}
+// PMS MR Series Data Storage
+int DumpPMS_MRSDS(const gdcm::DataSet & ds)
+{
+  const gdcm::PrivateTag tdata(0x2005,0x32,"Philips MR Imaging DD 002");
+  if( !ds.FindDataElement( tdata ) ) return 1;
+  const gdcm::DataElement &data = ds.GetDataElement( tdata );
+  gdcm::SmartPointer<gdcm::SequenceOfItems> sqi = data.GetValueAsSQ();
+  if( !sqi ) return 1;
+  std::cout << "PMS Dumping info from tag " << tdata << std::endl;
+  gdcm::SequenceOfItems::ConstIterator it = sqi->Begin();
+  for( ; it != sqi->End(); ++it )
+    {
+    const gdcm::Item & item = *it;
+    const gdcm::DataSet & nestedds = item.GetNestedDataSet();
+    const gdcm::PrivateTag tprotocoldataname(0x2005,0x37,"Philips MR Imaging DD 002");
+    const gdcm::DataElement & protocoldataname = nestedds.GetDataElement( tprotocoldataname );
+    const gdcm::ByteValue *bv1 = protocoldataname.GetByteValue();
+    const gdcm::PrivateTag tprotocoldatatype(0x2005,0x39,"Philips MR Imaging DD 002");
+    const gdcm::DataElement & protocoldatatype = nestedds.GetDataElement( tprotocoldatatype );
+    const gdcm::ByteValue *bv2 = protocoldatatype.GetByteValue();
+    const gdcm::PrivateTag tprotocoldatablock(0x2005,0x44,"Philips MR Imaging DD 002");
+    const gdcm::DataElement & protocoldatablock = nestedds.GetDataElement( tprotocoldatablock );
+    const gdcm::ByteValue *bv3 = protocoldatablock.GetByteValue();
+    const gdcm::PrivateTag tprotocoldatabool(0x2005,0x47,"Philips MR Imaging DD 002");
+    const gdcm::DataElement & protocoldatabool = nestedds.GetDataElement( tprotocoldatabool );
+    const gdcm::ByteValue *bv4 = protocoldatabool.GetByteValue();
+    std::string s1;
+    if( bv1 )
+      {
+      s1 = std::string( bv1->GetPointer(), bv1->GetLength() );
+      }
+    std::string s2;
+    if( bv2 )
+      {
+      s2 = std::string( bv2->GetPointer(), bv2->GetLength() );
+      }
+    std::string s3;
+    if( bv3 )
+      {
+      s3 = std::string( bv3->GetPointer(), bv3->GetLength() );
+      }
+    std::string s4;
+    if( bv4 )
+      {
+      s4 = std::string( bv4->GetPointer(), bv4->GetLength() );
+      }
+    std::istringstream is( s3 );
+    std::cout << "PMS/Item name: [" << s1 << "/" << s2 << "/" << s4 << "]" << std::endl;
+    ProcessSDSData( is );
+    }
+  return 0;
+}
+
 // VEPRO
 /*
 [VIMDATA2]
@@ -465,6 +621,24 @@ int PrintVEPRO(const std::string & filename, bool verbose)
   return ret;
 }
 
+int PrintSDS(const std::string & filename, bool verbose)
+{
+  (void)verbose;
+  gdcm::Reader reader;
+  reader.SetFileName( filename.c_str() );
+  if( !reader.Read() )
+    {
+    std::cerr << "Failed to read: " << filename << std::endl;
+    return 1;
+    }
+
+  const gdcm::DataSet& ds = reader.GetFile().GetDataSet();
+  int ret = cleanup::DumpPMS_MRSDS( ds );
+
+  return ret;
+}
+
+
 int PrintPDB(const std::string & filename, bool verbose)
 {
   (void)verbose;
@@ -614,6 +788,7 @@ void PrintHelp()
   std::cout << "  -P --pdb            print GEMS Protocol Data Block (0025,1b,GEMS_SERS_01)." << std::endl;
   std::cout << "     --elscint        print ELSCINT Protocol Information (01f7,26,ELSCINT1)." << std::endl;
   std::cout << "     --vepro          print VEPRO Protocol Information (0055,20,VEPRO VIM 5.0 DATA)." << std::endl;
+  std::cout << "     --sds            print Philips MR Series Data Storage (1.3.46.670589.11.0.0.12.2) Information (2005,32,Philips MR Imaging DD 002)." << std::endl;
   std::cout << "  -A --asn1           print encapsulated ASN1 structure >(0400,0520)." << std::endl;
   std::cout << "     --map-uid-names  map UID to names." << std::endl;
   std::cout << "General Options:" << std::endl;
@@ -641,6 +816,7 @@ int main (int argc, char *argv[])
   int printpdb = 0;
   int printelscint = 0;
   int printvepro = 0;
+  int printsds = 0; // MR Series Data Storage
   int verbose = 0;
   int warning = 0;
   int debug = 0;
@@ -681,6 +857,7 @@ int main (int argc, char *argv[])
         {"map-uid-names", 0, &mapuidnames, 1},
         {"elscint", 0, &printelscint, 1},
         {"vepro", 0, &printvepro, 1},
+        {"sds", 0, &printsds, 1},
         {0, 0, 0, 0} // required
     };
     static const char short_options[] = "i:xrpdcCPAVWDEhvI";
@@ -878,6 +1055,10 @@ int main (int argc, char *argv[])
         {
         res += PrintVEPRO(*it, verbose!= 0);
         }
+      else if( printsds )
+        {
+        res += PrintSDS(*it, verbose!= 0);
+        }
       else if( printelscint )
         {
         res += PrintELSCINT(*it, verbose!= 0);
@@ -916,6 +1097,10 @@ int main (int argc, char *argv[])
     else if( printvepro )
       {
       res += PrintVEPRO(filename, verbose!= 0);
+      }
+    else if( printsds )
+      {
+      res += PrintSDS(filename, verbose!= 0);
       }
     else if( printelscint )
       {

Applications/Cxx/gdcmimg.cxx
@@ -282,6 +282,7 @@ bool PopulateSingeFile( gdcm::PixmapWriter & writer, gdcm::SequenceOfFragments *
   bool b = jpeg.GetHeaderInfo( is, ts );
   if( !b )
     {
+    std::cerr << "Could not read: " << filename << std::endl;
     return false;
     }
 

Applications/Cxx/gdcmpdf.cxx
@@ -536,10 +536,10 @@ http://msdn.microsoft.com/en-us/library/078sfkak(VS.80).aspx
   gdcm::Attribute<0x0008, 0x0050> at;
   ds.Insert( at.GetAsDataElement() );
 }
-//(0008,0060) CS [OT]                                     #   2, 1 Modality
+//(0008,0060) CS [DOC]                                     #   2, 1 Modality
 {
   gdcm::Attribute<0x0008, 0x0060> at;
-  at.SetValue( "OT" );
+  at.SetValue( "DOC " );
   ds.Insert( at.GetAsDataElement() );
 }
 //(0008,0064) CS [WSD]                                    #   4, 1 ConversionType
@@ -559,6 +559,14 @@ http://msdn.microsoft.com/en-us/library/078sfkak(VS.80).aspx
   gdcm::Attribute<0x0008, 0x0090> at;
   ds.Insert( at.GetAsDataElement() );
 }
+
+// In past DICOM implementation there used to be those neat tags:
+// (0088,0904) Topic Title TopicTitle LO 1 RET
+// (0088,0906) Topic Subject TopicSubject ST 1 RET
+// (0088,0910) Topic Author TopicAuthor LO 1 RET
+// (0088,0912) Topic Keywords TopicKeywords LO 1-32 RET
+// However they are now deprecated...
+
 //(0010,0010) PN (no value available)                     #   0, 0 PatientsName
 {
   gdcm::Attribute<0x0010, 0x0010> at;

CMake/FindCharLS.cmake
@@ -1,0 +1,40 @@
+#
+#  Copyright (c) 2006-2011 Mathieu Malaterre <mathieu.malaterre@gmail.com>
+#
+#  Redistribution and use is allowed according to the terms of the New
+#  BSD license.
+#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+FIND_PATH(CHARLS_INCLUDE_DIR CharLS/interface.h
+/usr/local/include
+/usr/include
+)
+
+FIND_LIBRARY(CHARLS_LIBRARY
+  NAMES CharLS
+  PATHS /usr/lib /usr/local/lib
+  )
+
+IF (CHARLS_LIBRARY AND CHARLS_INCLUDE_DIR)
+    SET(CHARLS_LIBRARIES    ${CHARLS_LIBRARY})
+    SET(CHARLS_INCLUDE_DIRS ${CHARLS_INCLUDE_DIR})
+    SET(CHARLS_FOUND "YES")
+ELSE (CHARLS_LIBRARY AND CHARLS_INCLUDE_DIR)
+  SET(CHARLS_FOUND "NO")
+ENDIF (CHARLS_LIBRARY AND CHARLS_INCLUDE_DIR)
+
+IF (CHARLS_FOUND)
+   IF (NOT CHARLS_FIND_QUIETLY)
+      MESSAGE(STATUS "Found CHARLS: ${CHARLS_LIBRARIES}")
+   ENDIF (NOT CHARLS_FIND_QUIETLY)
+ELSE (CHARLS_FOUND)
+   IF (CHARLS_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find CHARLS library")
+   ENDIF (CHARLS_FIND_REQUIRED)
+ENDIF (CHARLS_FOUND)
+
+MARK_AS_ADVANCED(
+  CHARLS_LIBRARIES
+  CHARLS_INCLUDE_DIR
+  )

CMake/InstallMacros.cmake
@@ -40,7 +40,7 @@ MACRO (install_pdb library)
         get_target_property(library_dll ${library} LOCATION_${cfg})
         string(REPLACE .dll .pdb library_pdb ${library_dll})
         string(TOLOWER ${cfg} lcfg)
-        if(lcfg STREQUAL "debug" or lcfg STREQUAL "relwithdebinfo")
+        if(lcfg STREQUAL "debug" OR lcfg STREQUAL "relwithdebinfo")
           install (FILES ${library_pdb}
             DESTINATION ${GDCM_INSTALL_BIN_DIR}
             COMPONENT Development
@@ -55,7 +55,7 @@ MACRO (install_pdb library)
       get_target_property(library_dll ${library} LOCATION_${CMAKE_BUILD_TYPE})
       string(REPLACE .dll .pdb library_pdb ${library_dll})
       string(TOLOWER ${CMAKE_BUILD_TYPE} lcfg)
-      if(lcfg STREQUAL "debug" or lcfg STREQUAL "relwithdebinfo")
+      if(lcfg STREQUAL "debug" OR lcfg STREQUAL "relwithdebinfo")
         install (FILES ${library_pdb}
           DESTINATION ${GDCM_INSTALL_BIN_DIR}
           COMPONENT Development

commit 137039ddccba001637f6a78997eaaa8a7fb55336

Fix typos introduced in 4e328a6d54

Source/DataDictionary/gdcmPrivateDefaultDicts.cxx
@@ -7418,11 +7418,11 @@ static const DICT_ENTRY DICOMV3DataDict [] = {
   /* end of dups */
   {0x2005,0x0032,"Philips MR Imaging DD 002",VR::SQ,VM::VM1,"Protocol Data Sequence",false },
   {0x2005,0x0034,"Philips MR Imaging DD 002",VR::LT,VM::VM1,"?SeriesTransactionUID?",false },
-  {0x2005,0x0037,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"Protocol Data Name",false },
-  {0x2005,0x0038,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"?MRApplicationName",false },
-  {0x2005,0x0039,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"Protocol Data Type",false },
-  {0x2005,0x0040,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"?MRVersionStr",false },
-  {0x2005,0x0041,"Philips MR Imaging DD 002",VR::PN,VM::VM1,"?MRCommentStr",false },
+  {0x2005,0x0037,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"Protocol Data Name",false },
+  {0x2005,0x0038,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?MRApplicationName",false },
+  {0x2005,0x0039,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"Protocol Data Type",false },
+  {0x2005,0x0040,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?MRVersionStr",false },
+  {0x2005,0x0041,"Philips MR Imaging DD 002",VR::LO,VM::VM1,"?MRCommentStr",false },
   {0x2005,0x0043,"Philips MR Imaging DD 002",VR::SL,VM::VM1,"Protocol Data Block Length (non-padded)",false },
   {0x2005,0x0044,"Philips MR Imaging DD 002",VR::OW,VM::VM1,"Protocol Data Block",false },
   {0x2005,0x0047,"Philips MR Imaging DD 002",VR::CS,VM::VM1,"Protocol Data Boolean",false },