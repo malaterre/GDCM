68498f00db7b2d5e8d062a1812315e9ddd720d98
Sean McBride <sean@rogue-research.com>
2025-06-10 16:11:44 -0400

    Increase cmake_minimum_required for CMake 4 compatibility

--- gdcm_common.cmake
+++ gdcm_common.cmake
@@ -88,7 +88,7 @@
 #
 #==========================================================================*/
 
-cmake_minimum_required(VERSION 2.8.2 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
 
 set(dashboard_user_home "$ENV{HOME}")
 
