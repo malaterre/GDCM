1b8149fd16b5ecfa57484e3aa5574665eb64593d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2026-05-20 17:27:39 +0000

    ENH: Add encoding tests for JPEGTurboCodec
    Add TestImageChangeTransferSyntax8 (JPEG lossless via turbo),
    TestImageChangeTransferSyntax9 (JPEG baseline lossy 8-bit), and
    TestImageChangeTransferSyntax10 (JPEG extended lossy 12-bit).
    Tests are conditional on GDCM_USE_JPEGTURBO.

---
+++ Testing/Source/MediaStorageAndFileFormat/Cxx/TestImageChangeTransferSyntax10.cxx
@@ -0,0 +1,153 @@
+/*=========================================================================
+
+  Program: GDCM (Grassroots DICOM). A DICOM library
+
+  Copyright (c) 2006-2011 Mathieu Malaterre
+  All rights reserved.
+  See Copyright.txt or http://gdcm.sourceforge.net/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "gdcmImageChangeTransferSyntax.h"
+#include "gdcmImageReader.h"
+#include "gdcmImageWriter.h"
+#include "gdcmFilename.h"
+#include "gdcmSystem.h"
+#include "gdcmFileMetaInformation.h"
+#include "gdcmTesting.h"
+#include "gdcmByteSwap.h"
+
+namespace gdcm
+{
+
+int TestImageChangeTransferSyntaxJPEGTurboExtended(const char *filename, bool verbose = false)
+{
+  ImageReader reader;
+  reader.SetFileName( filename );
+  if ( !reader.Read() )
+    {
+    const FileMetaInformation &header = reader.GetFile().GetHeader();
+    MediaStorage ms = header.GetMediaStorage();
+    bool isImage = MediaStorage::IsImage( ms );
+    bool pixeldata = reader.GetFile().GetDataSet().FindDataElement( Tag(0x7fe0,0x0010) );
+    if( isImage && pixeldata )
+      {
+      std::cerr << "Failed to read: " << filename << std::endl;
+      return 1;
+      }
+    else
+      {
+      // not an image give up...
+      std::cerr << "Problem with: " << filename << " but that's ok" << std::endl;
+      return 0;
+      }
+    }
+
+  const gdcm::Image &image = reader.GetImage();
+
+  // JPEGExtendedProcess2_4 supports 8 or 12-bit data
+  unsigned short ba = image.GetPixelFormat().GetBitsAllocated();
+  if( ba != 8 && ba != 12 && ba != 16 )
+    {
+    return 0;
+    }
+
+  gdcm::ImageChangeTransferSyntax change;
+  change.SetTransferSyntax( gdcm::TransferSyntax::JPEGExtendedProcess2_4 );
+  change.SetInput( image );
+  bool b = change.Change();
+  if( !b )
+    {
+    // Some images may legitimately fail (e.g. 32-bit, palette color)
+    std::cerr << "Could not change the Transfer Syntax: " << filename
+              << " but that may be ok" << std::endl;
+    return 0;
+    }
+
+  // Create directory first:
+  const char subdir[] = "TestImageChangeTransferSyntax10";
+  std::string tmpdir = Testing::GetTempDirectory( subdir );
+  if( !System::FileIsDirectory( tmpdir.c_str() ) )
+    {
+    System::MakeDirectory( tmpdir.c_str() );
+    }
+  std::string outfilename = Testing::GetTempFilename( filename, subdir );
+
+  ImageWriter writer;
+  writer.SetFileName( outfilename.c_str() );
+  writer.SetFile( reader.GetFile() );
+  writer.SetImage( change.GetOutput() );
+  if( !writer.Write() )
+    {
+    std::cerr << "Failed to write: " << outfilename << std::endl;
+    return 1;
+    }
+
+  // Let's read that file back in !
+  ImageReader reader2;
+  reader2.SetFileName( outfilename.c_str() );
+  if ( !reader2.Read() )
+    {
+    std::cerr << "Could not even reread our generated file : " << outfilename << std::endl;
+    return 1;
+    }
+
+  // For lossy compression, we cannot compare MD5.
+  // Instead verify the image is readable and dimensions match.
+  const gdcm::Image &img2 = reader2.GetImage();
+  unsigned long len = img2.GetBufferLength();
+  char* buffer = new char[len];
+  bool res2 = img2.GetBuffer(buffer);
+  if( !res2 )
+    {
+    std::cerr << "could not get buffer: " << outfilename << std::endl;
+    delete[] buffer;
+    return 1;
+    }
+
+  // Verify dimensions match
+  const unsigned int *dims1 = image.GetDimensions();
+  const unsigned int *dims2 = img2.GetDimensions();
+  if( dims1[0] != dims2[0] || dims1[1] != dims2[1] )
+    {
+    std::cerr << "Dimension mismatch for: " << filename << std::endl;
+    delete[] buffer;
+    return 1;
+    }
+
+  if( verbose )
+    {
+    std::cout << "file was written in: " << outfilename << std::endl;
+    }
+
+  delete[] buffer;
+  return 0;
+}
+
+} // end namespace gdcm
+
+int TestImageChangeTransferSyntax10(int argc, char *argv[])
+{
+  if( argc == 2 )
+    {
+    const char *filename = argv[1];
+    return gdcm::TestImageChangeTransferSyntaxJPEGTurboExtended(filename, true);
+    }
+
+  // else
+  gdcm::Trace::DebugOff();
+  gdcm::Trace::WarningOff();
+  int r = 0, i = 0;
+  const char *filename;
+  const char * const *filenames = gdcm::Testing::GetFileNames();
+  while( (filename = filenames[i]) )
+    {
+    r += gdcm::TestImageChangeTransferSyntaxJPEGTurboExtended( filename );
+    ++i;
+    }
+
+  return r;
+}
