5abc23fbd56ccda13cd775e88b180ad048ea0a60
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2026-06-18 16:10:36 +0200

    UIH/VFRAME is actually never inverted

--- Source/MediaStorageAndFileFormat/gdcmSplitGridFilter.cxx
+++ Source/MediaStorageAndFileFormat/gdcmSplitGridFilter.cxx
@@ -189,6 +189,7 @@ bool SplitGridFilter::ComputeGRIDSliceNormal(double slicenormalvector[3],
       inverted = false;
     } else if (fabs(-1. - snv_dot) < 1e-6) {
       gdcmDebugMacro("SliceNormalVector is opposite direction");
+      // FIXME SNV is always opposite dir but actually not used in code
       inverted = true;
     } else {
       gdcmErrorMacro(
@@ -374,7 +375,7 @@ bool SplitGridFilter::Split() {
   outbuf.resize(l);
 
   bool b;
-  if (inverted) {
+  if (inverted && false) {
     b = details::reorganize_grid_invert(
       (unsigned short*)(void*)buf.data(), inputimage.GetDimensions(), div, dims,
       (unsigned short*)(void*)outbuf.data());
